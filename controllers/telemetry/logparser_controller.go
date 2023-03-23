8feb20367cf96806b3e925f59acc7ec8404bf8f9
Rakesh Garimella <rakesh.garimella@sap.com>
2023-03-23 17:08:48 +0100

    fix

--- controllers/telemetry/logparser_controller.go
+++ controllers/telemetry/logparser_controller.go
@@ -18,15 +18,10 @@ limitations under the License.
 
 import (
 	"context"
-	"fmt"
-
 	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
 	"github.com/kyma-project/telemetry-manager/internal/reconciler/logparser"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // LogParserReconciler reconciles a Logparser object
@@ -53,25 +48,4 @@ func (r *LogParserReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&telemetryv1alpha1.LogParser{}).
 		Complete(r)
-}
-
-func (r *LogParserReconciler) mapDaemonSets(object client.Object) []reconcile.Request {
-	daemonSet := object.(*appsv1.DaemonSet)
-
-	var requests []reconcile.Request
-	if daemonSet.Name != r.config.DaemonSet.Name || daemonSet.Namespace != r.config.DaemonSet.Namespace {
-		return requests
-	}
-
-	var allParsers telemetryv1alpha1.LogParserList
-	if err := r.List(context.Background(), &allParsers); err != nil {
-		ctrl.Log.Error(err, "DamonSet UpdateEvent: fetching LogParserList failed!", err.Error())
-		return requests
-	}
-
-	for _, parser := range allParsers.Items {
-		requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{Name: parser.Name}})
-	}
-	ctrl.Log.V(1).Info(fmt.Sprintf("DaemonSet changed event handling done: Created %d new reconciliation requests.\n", len(requests)))
-	return requests
 }
