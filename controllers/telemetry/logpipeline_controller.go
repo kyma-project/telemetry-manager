4bf0c6c426404f36baeba6766461bec36daf7806
Aleksei Chernyshov <aleksei.chernyshov@sap.com>
2023-04-19 17:03:02 +0200

    Added custom reconciliation request handler

--- controllers/telemetry/logpipeline_controller.go
+++ controllers/telemetry/logpipeline_controller.go
@@ -23,14 +23,18 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
+	"github.com/go-logr/logr"
 	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
 	"github.com/kyma-project/telemetry-manager/internal/reconciler/logpipeline"
 	"github.com/kyma-project/telemetry-manager/internal/secretref"
@@ -63,40 +67,102 @@ func (r *LogPipelineReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&telemetryv1alpha1.LogPipeline{}).
 		Watches(
 			&source.Kind{Type: &appsv1.DaemonSet{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &corev1.Service{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &corev1.ConfigMap{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &corev1.ServiceAccount{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &rbacv1.ClusterRole{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &rbacv1.ClusterRoleBinding{}},
-			&handler.EnqueueRequestForOwner{
-				OwnerType:    &telemetryv1alpha1.LogPipeline{},
-				IsController: false}).
+			enqueueRequestForOwnerFuncs(ctrl.Log),
+			builder.WithPredicates(setup.DeleteOrUpdate()),
+		).
 		Watches(
 			&source.Kind{Type: &corev1.Secret{}},
 			handler.EnqueueRequestsFromMapFunc(r.mapSecret),
 			builder.WithPredicates(setup.CreateOrUpdate()),
 		).
 		Complete(r)
+}
+
+func enqueueRequestForOwnerFuncs(log logr.Logger) handler.EventHandler {
+	return &handler.Funcs{
+		CreateFunc: func(evt event.CreateEvent, q workqueue.RateLimitingInterface) {
+			enqueueRequestsForOwners(log, evt.Object, q)
+		},
+		DeleteFunc: func(evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
+			enqueueRequestsForOwners(log, evt.Object, q)
+		},
+		UpdateFunc: func(e event.UpdateEvent, q workqueue.RateLimitingInterface) {
+			oldOwners := getOwnersFromObject(e.ObjectOld)
+			newOwners := getOwnersFromObject(e.ObjectNew)
+
+			for _, newOwner := range newOwners {
+				if ownerSliceContains(oldOwners, newOwner) {
+					continue
+				}
+
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Namespace: "kyma-system",
+						Name:      newOwner.Name,
+					},
+				}
+				q.Add(req)
+				log.V(1).Info("Enqueued reconcile request for owner", "owner", req.NamespacedName)
+			}
+		},
+	}
+}
+
+func enqueueRequestsForOwners(log logr.Logger, obj metav1.Object, q workqueue.RateLimitingInterface) {
+	owners := getOwnersFromObject(obj)
+
+	for _, owner := range owners {
+		req := reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: "kyma-system",
+				Name:      owner.Name,
+			},
+		}
+		q.Add(req)
+		log.V(1).Info("Enqueued reconcile request for owner", "owner", req.NamespacedName)
+	}
+}
+
+func getOwnersFromObject(obj metav1.Object) []metav1.OwnerReference {
+	if obj == nil {
+		return nil
+	}
+
+	return obj.GetOwnerReferences()
+}
+
+func ownerSliceContains(owners []metav1.OwnerReference, owner metav1.OwnerReference) bool {
+	for _, o := range owners {
+		if o.UID == owner.UID {
+			return true
+		}
+	}
+	return false
 }
 
 func (r *LogPipelineReconciler) mapSecret(object client.Object) []reconcile.Request {
