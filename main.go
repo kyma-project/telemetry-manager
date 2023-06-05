f3bed8c4f0bea00def4d96130713b519da159422
Stanislav Khalash <stanislav.khalash@sap.com>
2023-06-05 09:29:08 +0200

    Fix race condition

--- main.go
+++ main.go
@@ -364,8 +364,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		ctx := context.Background()
-		if err = webhookcert.EnsureCertificate(ctx, k8sClient, webhookConfig.CertConfig); err != nil {
+		if err = webhookcert.EnsureCertificate(context.TODO(), k8sClient, webhookConfig.CertConfig); err != nil {
 			setupLog.Error(err, "Failed to ensure webhook cert")
 			os.Exit(1)
 		}
@@ -374,12 +373,9 @@ func main() {
 		// Temporary solution for non-modularized telemetry operator
 		go func() {
 			for range time.Tick(1 * time.Hour) {
-				ctx := context.Background()
-				if err = webhookcert.EnsureCertificate(ctx, k8sClient, webhookConfig.CertConfig); err != nil {
-					setupLog.Error(err, "Failed to ensure webhook cert")
-					os.Exit(1)
+				if ensureErr := webhookcert.EnsureCertificate(context.TODO(), k8sClient, webhookConfig.CertConfig); ensureErr != nil {
+					setupLog.Error(ensureErr, "Failed to ensure webhook cert")
 				}
-				setupLog.Info("Ensured webhook cert")
 			}
 		}()
 	}
