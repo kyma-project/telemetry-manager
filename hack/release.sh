e6facebea9154a3947c6e307b9fc430c7e4d8f22
Mostafa Shorim <mostafa.shorim@sap.com>
2023-06-19 17:14:56 +0200

    locally test doc-url annotation

--- hack/release.sh
+++ hack/release.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-readonly MODULE_REGISTRY="europe-docker.pkg.dev/kyma-project/prod/unsigned"
+# readonly MODULE_REGISTRY="europe-docker.pkg.dev/kyma-project/prod/unsigned"
+readonly MODULE_REGISTRY="${MODULE_REGISTRY:-localhost:5001}"
 readonly GCP_ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
 
 function create_module() {
     cd config/manager && ${KUSTOMIZE} edit set image controller=${IMG} && cd ../..
-    ${KYMA} alpha create module --name kyma-project.io/module/${MODULE_NAME} --version ${MODULE_VERSION} --channel ${MODULE_CHANNEL} --default-cr ${MODULE_CR_PATH} --registry ${MODULE_REGISTRY} -c oauth2accesstoken:${GCP_ACCESS_TOKEN} --ci
+    # ${KYMA} alpha create module --name kyma-project.io/module/${MODULE_NAME} --version ${MODULE_VERSION} --channel ${MODULE_CHANNEL} --default-cr ${MODULE_CR_PATH} --registry ${MODULE_REGISTRY} -c oauth2accesstoken:${GCP_ACCESS_TOKEN} --ci
+    ${KYMA} alpha create module --name kyma-project.io/module/${MODULE_NAME} --version ${MODULE_VERSION} --channel ${MODULE_CHANNEL} --default-cr ${MODULE_CR_PATH} --registry ${MODULE_REGISTRY} --insecure --ci
+}
+
+function apply_doc_url_annotation() {
+    kubectl annotate --local=true -f ./template.yaml operator.kyma-project.io/doc-url=https://github.com/kyma-project/telemetry-manager/tree/main/docs/user -o yaml > temporary-template.yaml
+    mv temporary-template.yaml template.yaml
 }
 
 function create_github_release() {
     git remote add origin git@github.com:kyma-project/telemetry-manager.git
 	git reset --hard
-	curl -sL https://git.io/goreleaser | VERSION=${GORELEASER_VERSION} bash
+	curl -sL https://git.io/goreleaser | VERSION=${GORELEASER_VERSION} bash -s --  --skip-publish
 }
 
 function main() {
+    # Create the module and push its image to the prod registry defined in MODULE_REGISTRY
     create_module
+
+    # Apply doc-url annotation
+    apply_doc_url_annotation
+
     # Create github release entry using goreleaser
     create_github_release
 }
