9f86e4fdd97488a8ca4da802a323af367bd297a1
Stanislav Khalash <stanislav.khalash@sap.com>
2023-07-06 11:05:14 +0200

    Fix build

--- internal/resources/otelcollector/agent/resources_test.go
+++ internal/resources/otelcollector/agent/resources_test.go
@@ -36,7 +36,7 @@ func TestMakeClusterRole(t *testing.T) {
 }
 
 func TestMakeDaemonSet(t *testing.T) {
-	daemonSet := MakeDaemonSet(config, "123", "MY_POD_IP", "MY_NODE_NAME"t )
+	daemonSet := MakeDaemonSet(config, "123", "MY_POD_IP", "MY_NODE_NAME")
 
 	require.NotNil(t, daemonSet)
 	require.Equal(t, daemonSet.Name, config.BaseName)
