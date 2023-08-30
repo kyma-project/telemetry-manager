049cdb820a26aa90a5aae747906166a40e5421a7
Stanislav Khalash <stanislav.khalash@sap.com>
2023-08-30 09:18:34 +0200

    Add Istio status checker

---
+++ internal/reconciler/metricpipeline/istio_status_checker_test.go
@@ -0,0 +1,56 @@
+package metricpipeline
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestIsIstioActive(t *testing.T) {
+	scheme := scheme.Scheme
+	_ = apiextensionsv1.AddToScheme(scheme)
+
+	tests := []struct {
+		name string
+		crds []*apiextensionsv1.CustomResourceDefinition
+		want bool
+	}{
+		{
+			name: "should return true if peerauthentication crd found",
+			crds: []*apiextensionsv1.CustomResourceDefinition{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "peerauthentications.security.istio.io",
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "should return false if peerauthentication not crd found",
+			crds: []*apiextensionsv1.CustomResourceDefinition{},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakeClientBuilder := fake.NewClientBuilder().WithScheme(scheme)
+			for _, crd := range tt.crds {
+				fakeClientBuilder.WithObjects(crd)
+			}
+			fakeClient := fakeClientBuilder.Build()
+
+			isc := &istioStatusChecker{client: fakeClient}
+
+			got := isc.isIstioActive(context.Background())
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
