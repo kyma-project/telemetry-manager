62e5c10c2376e8f354246a681946a3a3744342bf
Stanislav Khalash <stanislav.khalash@sap.com>
2023-09-13 10:02:18 +0200

    Comment unused func

--- test/testkit/matchers/metric/metric_matchers_test.go
+++ test/testkit/matchers/metric/metric_matchers_test.go
@@ -1,14 +1,10 @@
 package metric
 
-import (
-	"go.opentelemetry.io/collector/pdata/pmetric"
-)
-
-func mustMarshalMetrics(md pmetric.Metrics) []byte {
-	var marshaler pmetric.JSONMarshaler
-	bytes, err := marshaler.MarshalMetrics(md)
-	if err != nil {
-		panic(err)
-	}
-	return bytes
-}
+//func mustMarshalMetrics(md pmetric.Metrics) []byte {
+//	var marshaler pmetric.JSONMarshaler
+//	bytes, err := marshaler.MarshalMetrics(md)
+//	if err != nil {
+//		panic(err)
+//	}
+//	return bytes
+//}
