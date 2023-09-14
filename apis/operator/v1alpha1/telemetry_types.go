2d02f91fc45fd82f63d164e67d687941dad47613
Mostafa Shorim <mostafa.shorim@sap.com>
2023-09-14 16:55:56 +0200

    clean up Telemetry possible states

--- apis/operator/v1alpha1/telemetry_types.go
+++ apis/operator/v1alpha1/telemetry_types.go
@@ -20,20 +20,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 type State string
 
 // Valid Module CR States.
 const (
 	// StateReady signifies Module CR is Ready and has been installed successfully.
 	StateReady State = "Ready"
-
-	// StateError signifies an error for Module CR. This signifies that the Installation
-	// process encountered an error.
-	// Contrary to Processing, it can be expected that this state should change on the next retry.
-	StateError State = "Error"
 
 	// StateDeleting signifies Module CR is being deleted. This is the state that is used
 	// when a deletionTimestamp was detected and Finalizers are picked up.
@@ -106,8 +98,8 @@ func init() {
 // Status defines the observed state of Module CR.
 type Status struct {
 	// State signifies current state of Module CR.
-	// Value can be one of ("Ready", "Processing", "Error", "Deleting", "Warning").
+	// Value can be one of ("Ready", "Deleting", "Warning").
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Enum=Processing;Deleting;Ready;Error;Warning
+	// +kubebuilder:validation:Enum=Deleting;Ready;Warning
 	State State `json:"state"`
 }
