2368bcdde25760a458ea05365e4cc7fd736091a2
Andreas Thaler <andreas.thaler01@sap.com>
2023-09-21 12:27:02 +0200

    feat: Update to fluent-bit 2.1.9 and otel-collector 0.85.0

--- main.go
+++ main.go
@@ -132,9 +132,9 @@ var (
 )
 
 const (
-	otelImage              = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.83.0-da21e9f9"
+	otelImage              = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.85.0-6130ed2c"
 	overridesConfigMapName = "telemetry-override-config"
-	fluentBitImage         = "europe-docker.pkg.dev/kyma-project/prod/tpi/fluent-bit:2.1.8-da21e9f9"
+	fluentBitImage         = "europe-docker.pkg.dev/kyma-project/prod/tpi/fluent-bit:2.1.9-6130ed2c"
 	fluentBitExporterImage = "europe-docker.pkg.dev/kyma-project/prod/directory-size-exporter:v20230824-2d68935f"
 
 	fluentBitDaemonSet = "telemetry-fluent-bit"
