457be4319898466aa045348235a2c7298f8c32d6
Stanislav Khalash <stanislav.khalash@sap.com>
2023-09-21 17:39:23 +0200

    Fix unit tests

--- internal/reconciler/telemetry/status_utils.go
+++ internal/reconciler/telemetry/status_utils.go
@@ -17,10 +17,10 @@ func generateDeletionBlockedMessage(resources ...blockingResources) string {
 	for _, res := range resources {
 		if len(res.resourceNames) > 0 {
 			slices.Sort(res.resourceNames)
-			resourcesDesc = append(resourcesDesc, fmt.Sprintf("%s: (%s)", res.resourceType, strings.Join(res.resourceNames, ",")))
+			resourcesDesc = append(resourcesDesc, fmt.Sprintf("%s (%s)", res.resourceType, strings.Join(res.resourceNames, ",")))
 		}
 	}
 
-	return fmt.Sprintf("The deletion of the module is blocked. To unblock the deletion, delete the following resources %s",
+	return fmt.Sprintf("The deletion of the module is blocked. To unblock the deletion, delete the following resources: %s",
 		strings.Join(resourcesDesc, ", "))
 }
