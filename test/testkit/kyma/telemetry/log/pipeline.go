43cbe2784b69a5f512d4b654705b4a8c2fe47bf2
Dennis Geiselmann <dennis.geiselmann@sap.com>
2023-10-05 13:09:11 +0200

    fix assignment

--- test/testkit/kyma/telemetry/log/pipeline.go
+++ test/testkit/kyma/telemetry/log/pipeline.go
@@ -74,7 +74,7 @@ func (p *Pipeline) WithHTTPOutput() *Pipeline {
 			Port:   "9880",
 			URI:    "/",
 			Format: "json",
-			TLSConfig: telemetry.TLSConfig{
+			TLSConfig: &telemetry.TLSConfig{
 				Disabled:                  true,
 				SkipCertificateValidation: true,
 			},
@@ -88,7 +88,7 @@ func (p *Pipeline) WithTLS(certs tls.Certs) *Pipeline {
 		return p
 	}
 
-	p.output.HTTP.TLSConfig = telemetry.TLSConfig{
+	p.output.HTTP.TLSConfig = &telemetry.TLSConfig{
 		Disabled:                  false,
 		SkipCertificateValidation: false,
 		CA: telemetry.ValueType{
