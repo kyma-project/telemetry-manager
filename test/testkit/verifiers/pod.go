79cf7cc3ed14cc309a96a7cfe2b9765b88132433
Stanislav Khalash <stanislav.khalash@sap.com>
2023-10-05 19:54:12 +0200

    Improve error handling

--- test/testkit/verifiers/pod.go
+++ test/testkit/verifiers/pod.go
@@ -2,6 +2,7 @@ package verifiers
 
 import (
 	"context"
+	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -11,12 +12,12 @@ func IsPodReady(ctx context.Context, k8sClient client.Client, listOptions client
 	var pods corev1.PodList
 	err := k8sClient.List(ctx, &pods, &listOptions)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("failed to list pods: %w", err)
 	}
 	for _, pod := range pods.Items {
 		for _, containerStatus := range pod.Status.ContainerStatuses {
 			if containerStatus.State.Running == nil {
-				return false, nil
+				return false, fmt.Errorf("pod %s has a container %s that is not running", pod.Name, containerStatus.Name)
 			}
 		}
 	}
@@ -27,7 +28,7 @@ func HasContainer(ctx context.Context, k8sClient client.Client, listOptions clie
 	var pods corev1.PodList
 	err := k8sClient.List(ctx, &pods, &listOptions)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("failed to list pods: %w", err)
 	}
 	for _, pod := range pods.Items {
 		for _, container := range pod.Spec.Containers {
