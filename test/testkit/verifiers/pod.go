ed50ae588f53e1d6dfb074e1368c05dfc3d8500a
Stanislav Khalash <stanislav.khalash@sap.com>
2023-10-06 09:10:41 +0200

    Print more info

--- test/testkit/verifiers/pod.go
+++ test/testkit/verifiers/pod.go
@@ -17,11 +17,21 @@ func IsPodReady(ctx context.Context, k8sClient client.Client, listOptions client
 	for _, pod := range pods.Items {
 		for _, containerStatus := range pod.Status.ContainerStatuses {
 			if containerStatus.State.Running == nil {
-				return false, fmt.Errorf("pod %s has a container %s that is not running", pod.Name, containerStatus.Name)
+				return false, generateContainerError(pod.Name, containerStatus
 			}
 		}
 	}
 	return true, nil
+}
+
+func generateContainerError(podName string, containerStatus corev1.ContainerStatus) error {
+	var additionalInfo string
+	if containerStatus.State.Waiting != nil {
+		additionalInfo = fmt.Sprintf("Waiting reason: %s, message: %s", containerStatus.State.Waiting.Reason, containerStatus.State.Waiting.Message)
+	} else if containerStatus.State.Terminated != nil {
+		additionalInfo = fmt.Sprintf("Terminated reason: %s, message: %s", containerStatus.State.Terminated.Reason, containerStatus.State.Terminated.Message)
+	}
+	return fmt.Errorf("pod %s has a container %s that is not running. Additional info: %s", podName, containerStatus.Name, additionalInfo)
 }
 
 func HasContainer(ctx context.Context, k8sClient client.Client, listOptions client.ListOptions, containerName string) (bool, error) {
