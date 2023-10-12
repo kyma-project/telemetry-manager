91a5aab7d027b7155318ae422565842801fe4dd7
Andreas Thaler <andreas.thaler01@sap.com>
2023-10-12 12:32:00 +0200

    fix

--- internal/otelcollector/config/metric/agent/receivers_test.go
+++ internal/otelcollector/config/metric/agent/receivers_test.go
@@ -31,7 +31,6 @@ func TestReceivers(t *testing.T) {
 		require.Equal(t, "serviceAccount", collectorConfig.Receivers.KubeletStats.AuthType)
 		require.Equal(t, true, collectorConfig.Receivers.KubeletStats.InsecureSkipVerify)
 		require.Equal(t, "https://${env:MY_NODE_NAME}:10250", collectorConfig.Receivers.KubeletStats.Endpoint)
-		require.Equal(t, false, collectorConfig.Receivers.KubeletStats.InsecureSkipVerify)
 
 		require.Nil(t, collectorConfig.Receivers.PrometheusSelf)
 		require.Nil(t, collectorConfig.Receivers.PrometheusAppPods)
