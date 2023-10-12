0a4cd038060ff1c4dcb8660ebadb25841d231348
Andreas Thaler <andreas.thaler01@sap.com>
2023-10-12 11:17:22 +0200

    fix

--- test/e2e/metrics_runtime_input_test.go
+++ test/e2e/metrics_runtime_input_test.go
@@ -20,8 +20,38 @@ import (
 )
 
 var (
-	kubeletMetricNames      = []string{"container.cpu.time", "container.cpu.utilization", "container.filesystem.available", "container.filesystem.capacity", "container.filesystem.usage", "container.memory.available", "container.memory.major_page_faults", "container.memory.page_faults", "container.memory.rss", "container.memory.usage", "container.memory.working_set", "k8s.pod.cpu.time", "k8s.pod.cpu.utilization", "k8s.pod.filesystem.available", "k8s.pod.filesystem.capacity", "k8s.pod.filesystem.usage", "k8s.pod.memory.available", "k8s.pod.memory.major_page_faults", "k8s.pod.memory.page_faults", "k8s.pod.memory.rss", "k8s.pod.memory.usage", "k8s.pod.memory.working_set", "k8s.pod.network.errors", "k8s.pod.network.io"}
-	kubeletMetricAttributes = []string{"k8s.cluster.name", "k8s.container.name", "k8s.namespace.name", "k8s.node.name", "k8s.pod.name", "k8s.pod.uid"}
+	kubeletMetricNames = []string{
+		"container.cpu.time",
+		"container.cpu.utilization",
+		"container.filesystem.available",
+		"container.filesystem.capacity",
+		"container.filesystem.usage",
+		"container.memory.available",
+		"container.memory.major_page_faults",
+		"container.memory.page_faults",
+		"container.memory.rss",
+		"container.memory.usage",
+		"container.memory.working_set",
+		"k8s.pod.cpu.time",
+		"k8s.pod.cpu.utilization",
+		"k8s.pod.filesystem.available",
+		"k8s.pod.filesystem.capacity",
+		"k8s.pod.filesystem.usage",
+		"k8s.pod.memory.available",
+		"k8s.pod.memory.major_page_faults",
+		"k8s.pod.memory.page_faults",
+		"k8s.pod.memory.rss",
+		"k8s.pod.memory.usage",
+		"k8s.pod.memory.working_set",
+		"k8s.pod.network.errors",
+		"k8s.pod.network.io"}
+	kubeletMetricResourceAttributes = []string{
+		"k8s.cluster.name",
+		"k8s.container.name",
+		"k8s.namespace.name",
+		"k8s.node.name",
+		"k8s.pod.name",
+		"k8s.pod.uid"}
 )
 
 var _ = Describe("Metrics Runtime Input", Label("metrics"), func() {
@@ -90,7 +120,7 @@ var _ = Describe("Metrics Runtime Input", Label("metrics"), func() {
 			}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
 		})
 
-		It("Should verify kubelet metric attributes", func() {
+		It("Should verify kubelet metric resource attributes", func() {
 			Eventually(func(g Gomega) {
 				resp, err := proxyClient.Get(telemetryExportURL)
 				g.Expect(err).NotTo(HaveOccurred())
