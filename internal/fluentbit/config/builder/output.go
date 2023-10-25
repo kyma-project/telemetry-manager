1654c91295a429a952d26ceabef6b276edd34410
Mostafa Shorim <mostafa.shorim@sap.com>
2023-10-25 23:45:14 +0200

    remove generation of loki output section

--- internal/fluentbit/config/builder/output.go
+++ internal/fluentbit/config/builder/output.go
@@ -25,10 +25,6 @@ func createOutputSection(pipeline *telemetryv1alpha1.LogPipeline, defaults Pipel
 		return generateHTTPOutput(output.HTTP, defaults.FsBufferLimit, pipeline.Name)
 	}
 
-	if output.IsLokiDefined() {
-		return generateLokiOutput(output.Loki, defaults.FsBufferLimit, pipeline.Name)
-	}
-
 	return ""
 }
 
@@ -99,27 +95,6 @@ func generateHTTPOutput(httpOutput *telemetryv1alpha1.HTTPOutput, fsBufferLimit 
 		sb.AddConfigParam("tls.key_file", fmt.Sprintf("/fluent-bit/etc/output-tls-config/%s-key.key", name))
 	}
 
-	return sb.Build()
-}
-
-func generateLokiOutput(lokiOutput *telemetryv1alpha1.LokiOutput, fsBufferLimit string, name string) string {
-	sb := NewOutputSectionBuilder()
-	sb.AddConfigParam("labelMapPath", "/fluent-bit/etc/loki-labelmap.json")
-	sb.AddConfigParam("loglevel", "warn")
-	sb.AddConfigParam("lineformat", "json")
-	sb.AddConfigParam("match", fmt.Sprintf("%s.*", name))
-	sb.AddConfigParam("storage.total_limit_size", fsBufferLimit)
-	sb.AddConfigParam("name", "grafana-loki")
-	sb.AddConfigParam("alias", fmt.Sprintf("%s-grafana-loki", name))
-	sb.AddConfigParam("url", resolveValue(lokiOutput.URL, name))
-	if len(lokiOutput.Labels) != 0 {
-		value := concatenateLabels(lokiOutput.Labels)
-		sb.AddConfigParam("labels", value)
-	}
-	if len(lokiOutput.RemoveKeys) != 0 {
-		str := strings.Join(lokiOutput.RemoveKeys, ", ")
-		sb.AddConfigParam("removeKeys", str)
-	}
 	return sb.Build()
 }
 
