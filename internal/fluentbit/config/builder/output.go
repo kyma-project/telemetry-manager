d2bfc5187532841085ffa991f5ed5eb430c3fa72
Mostafa Shorim <mostafa.shorim@sap.com>
2023-10-26 08:24:59 +0200

    fix linting

--- internal/fluentbit/config/builder/output.go
+++ internal/fluentbit/config/builder/output.go
@@ -2,6 +2,7 @@ package builder
 
 import (
 	"fmt"
+
 	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
 	"github.com/kyma-project/telemetry-manager/internal/utils/envvar"
 )
