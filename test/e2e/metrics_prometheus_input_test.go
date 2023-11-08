e5b91079e15dcddeaca9cd20a581308885ebdadf
Mostafa Shorim <mostafa.shorim@sap.com>
2023-11-08 18:10:07 +0100

    remove dummy test label

--- test/e2e/metrics_prometheus_input_test.go
+++ test/e2e/metrics_prometheus_input_test.go
@@ -20,7 +20,7 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/verifiers"
 )
 
-var _ = Describe("Metrics Prometheus Input", Label("metrics", "my-test"), func() {
+var _ = Describe("Metrics Prometheus Input", Label("metrics"), func() {
 	const (
 		mockNs          = "metric-prometheus-input"
 		mockBackendName = "metric-agent-receiver"
