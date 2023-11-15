27dd31c8b00668ae335cb38485542e07c124f8a6
Hisar Balik <hisar.balik@sap.com>
2023-11-15 14:01:57 +0100

    roll back timeout changes

--- test/testkit/periodic/periodic.go
+++ test/testkit/periodic/periodic.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	// EventuallyTimeout is used when asserting an event with Eventually.
-	EventuallyTimeout = time.Second * 100
+	EventuallyTimeout = time.Second * 60
 
 	// ConsistentlyTimeout is used when asserting the absence of an event with Consistently.
 	ConsistentlyTimeout = time.Second * 10
@@ -20,7 +20,7 @@ const (
 	TelemetryConsistentlyTimeout = time.Second * 20
 
 	// DefaultInterval is the default interval duration used when no specialized interval is applicable.
-	DefaultInterval = time.Second
+	DefaultInterval = time.Millisecond * 250
 
 	// TelemetryInterval is used for asynchronous checks when polling Telemetry data from a mock backend via the export URL.
 	TelemetryInterval = time.Second
