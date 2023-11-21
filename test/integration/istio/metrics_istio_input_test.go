c3a60bc7571f13ad69e347b7f4d4fad49cd0b114
Stanislav Khalash <stanislav.khalash@sap.com>
2023-11-21 16:19:17 +0100

    Test metric names

--- test/integration/istio/metrics_istio_input_test.go
+++ test/integration/istio/metrics_istio_input_test.go
@@ -3,28 +3,41 @@
 package istio
 
 import (
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"net/http"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
 	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
 	kitmetric "github.com/kyma-project/telemetry-manager/test/testkit/kyma/telemetry/metric"
+	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/metric"
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
-	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/metricproducer"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/verifiers"
 )
 
-var _ = Describe("Metrics Istio Input", Label("metrics"), func() {
+var _ = Describe("Metrics Istio Input", Label("new"), func() {
 	const (
-		mockNs             = "metric-istio-input"
-		mockBackendName    = "metric-agent-receiver"
-		metricProducerName = "metric-producer-with-proxy"
+		mockNs          = "metric-istio-input"
+		mockBackendName = "metric-agent-receiver"
 	)
-	//var telemetryExportURL string
+	var (
+		istioProxyMetricNames = []string{
+			"istio_requests_total",
+			"istio_request_duration_milliseconds",
+			"istio_request_bytes",
+			"istio_response_bytes",
+			"istio_request_messages_total",
+			"istio_response_messages_total",
+		}
+		telemetryExportURL string
+	)
 
+	// Given that the metric agent/gateway components are already in the service mesh,
+	// there is no need to deploy any additional workloads for this purpose.
 	makeResources := func() []client.Object {
 		var objs []client.Object
 
@@ -33,12 +46,7 @@ var _ = Describe("Metrics Istio Input", Label("metrics"), func() {
 		// Mocks namespace objects
 		mockBackend := backend.New(mockBackendName, mockNs, backend.SignalTypeMetrics)
 		objs = append(objs, mockBackend.K8sObjects()...)
-		//telemetryExportURL = mockBackend.TelemetryExportURL(proxyClient)
-
-		metricProducer := metricproducer.New(mockNs, metricproducer.WithName(metricProducerName))
-		objs = append(objs, []client.Object{
-			metricProducer.Pod().WithSidecarInjection().K8sObject(),
-		}...)
+		telemetryExportURL = mockBackend.TelemetryExportURL(proxyClient)
 
 		metricPipeline := kitmetric.NewPipeline("pipeline-with-istio-input-enabled").
 			WithOutputEndpointFromSecret(mockBackend.HostSecretRef()).
@@ -71,33 +79,15 @@ var _ = Describe("Metrics Istio Input", Label("metrics"), func() {
 			verifiers.DaemonSetShouldBeReady(ctx, k8sClient, kitkyma.MetricAgentName)
 		})
 
-		//It("Should verify istio proxy metric scraping", func() {
-		//	Eventually(func(g Gomega) {
-		//		resp, err := proxyClient.Get(telemetryExportURL)
-		//		g.Expect(err).NotTo(HaveOccurred())
-		//		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
-		//		// here we are discovering the same metric-producer workload twice: once via the annotated service and once via the annotated pod
-		//		// targets discovered via annotated pods must have no service label
-		//		g.Expect(resp).To(HaveHTTPBody(SatisfyAll(
-		//			ContainMd(ContainMetric(SatisfyAll(
-		//				WithName(Equal(metricproducer.MetricCPUTemperature.Name)),
-		//				WithType(Equal(metricproducer.MetricCPUTemperature.Type)),
-		//			))),
-		//			ContainMd(ContainMetric(SatisfyAll(
-		//				WithName(Equal(metricproducer.MetricCPUEnergyHistogram.Name)),
-		//				WithType(Equal(metricproducer.MetricCPUEnergyHistogram.Type)),
-		//			))),
-		//			ContainMd(ContainMetric(SatisfyAll(
-		//				WithName(Equal(metricproducer.MetricHardwareHumidity.Name)),
-		//				WithType(Equal(metricproducer.MetricHardwareHumidity.Type)),
-		//			))),
-		//			ContainMd(ContainMetric(SatisfyAll(
-		//				WithName(Equal(metricproducer.MetricHardDiskErrorsTotal.Name)),
-		//				WithType(Equal(metricproducer.MetricHardDiskErrorsTotal.Type)),
-		//			))),
-		//		),
-		//		))
-		//	}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
-		//})
+		It("Should verify istio proxy metric scraping", func() {
+			Eventually(func(g Gomega) {
+				resp, err := proxyClient.Get(telemetryExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+				g.Expect(resp).To(HaveHTTPBody(
+					ContainMd(ContainMetric(WithName(BeElementOf(istioProxyMetricNames)))),
+				))
+			}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
 	})
 })
