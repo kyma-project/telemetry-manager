4c438f8122da14c02420fc95bdaee9827b303a35
Rakesh Garimella <rakesh.garimella@sap.com>
2023-11-22 15:42:44 +0100

    fix

--- internal/resources/otelcollector/gateway_test.go
+++ internal/resources/otelcollector/gateway_test.go
@@ -311,7 +311,7 @@ func TestApplyGatewayResourcesWithIstioEnabled(t *testing.T) {
 
 	t.Run("It should have permissive peer authentication created", func(t *testing.T) {
 		var peerAuth istiosecv1beta1.PeerAuthentication
-		require.NoError(t, client.Get(ctx, types.NamespacedName{Namespace: namespace, Name: "name"}, &peerAuth))
+		require.NoError(t, client.Get(ctx, types.NamespacedName{Namespace: namespace, Name: name}, &peerAuth))
 
 		require.Equal(t, name, peerAuth.Name)
 		require.Equal(t, v1beta1.PeerAuthentication_MutualTLS_PERMISSIVE, peerAuth.Spec.Mtls.Mode)
