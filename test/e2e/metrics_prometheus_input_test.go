fee174fe8737fd5c2531f3e4ebefa81972f809fe
Stanislav Khalash <stanislav.khalash@sap.com>
2023-11-24 18:02:48 +0100

    Fix

--- test/e2e/metrics_prometheus_input_test.go
+++ test/e2e/metrics_prometheus_input_test.go
@@ -16,6 +16,7 @@ import (
 	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/metric"
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/metricproducer"
+	"github.com/kyma-project/telemetry-manager/test/testkit/otlp/kubeletstats"
 	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/verifiers"
 )
@@ -149,7 +150,7 @@ var _ = Describe("Metrics Prometheus Input", Label("metrics"), func() {
 				g.Expect(err).NotTo(HaveOccurred())
 				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
 				g.Expect(resp).To(HaveHTTPBody(
-					Not(ContainMd(ContainMetric(WithName(BeElementOf(kubeletMetricNames))))),
+					Not(ContainMd(ContainMetric(WithName(BeElementOf(kubeletstats.MetricNames))))),
 				))
 			}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
 		})
