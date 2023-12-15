4bf2da1f2a147076631d8834c7463feaea9b1160
Stanislav Khalash <stanislav.khalash@sap.com>
2023-12-15 14:45:33 +0100

    Add more tests

--- internal/reconciler/metricpipeline/status.go
+++ internal/reconciler/metricpipeline/status.go
@@ -99,6 +99,7 @@ func (r *Reconciler) setGatewayReadyCondition(ctx context.Context, pipeline *tel
 func (r *Reconciler) setGatewayConfigGeneratedCondition(ctx context.Context, pipeline *telemetryv1alpha1.MetricPipeline, lockAcquired bool) {
 	status := metav1.ConditionTrue
 	reason := conditions.ReasonMetricConfigurationGenerated
+
 	if !lockAcquired {
 		status = metav1.ConditionFalse
 		reason = conditions.ReasonWaitingForLock
--- internal/reconciler/metricpipeline/status_test.go
+++ internal/reconciler/metricpipeline/status_test.go
@@ -169,7 +169,8 @@ func TestUpdateStatus(t *testing.T) {
 			},
 			Data: map[string][]byte{"user": {}, "password": {}},
 		}
-		pipeline := testutils.NewMetricPipelineBuilder().WithBasicAuthFromSecret(secret.Name, secret.Namespace, "user", "password").Build()
+		pipeline := testutils.NewMetricPipelineBuilder().WithBasicAuthFromSecret(
+			secret.Name, secret.Namespace, "user", "password").Build()
 		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(&pipeline, secret).WithStatusSubresource(&pipeline).Build()
 
 		gatewayProberStub := &mocks.DeploymentProber{}
@@ -177,7 +178,6 @@ func TestUpdateStatus(t *testing.T) {
 
 		sut := Reconciler{
 			Client:        fakeClient,
-			config:        Config{Gateway: otelcollector.GatewayConfig{}},
 			gatewayProber: gatewayProberStub,
 		}
 
@@ -194,7 +194,8 @@ func TestUpdateStatus(t *testing.T) {
 	})
 
 	t.Run("referenced secret missing", func(t *testing.T) {
-		pipeline := testutils.NewMetricPipelineBuilder().WithBasicAuthFromSecret("some-secret", "some-namespace", "user", "password").Build()
+		pipeline := testutils.NewMetricPipelineBuilder().WithBasicAuthFromSecret(
+			"some-secret", "some-namespace", "user", "password").Build()
 		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(&pipeline).WithStatusSubresource(&pipeline).Build()
 
 		gatewayProberStub := &mocks.DeploymentProber{}
@@ -202,7 +203,6 @@ func TestUpdateStatus(t *testing.T) {
 
 		sut := Reconciler{
 			Client:        fakeClient,
-			config:        Config{Gateway: otelcollector.GatewayConfig{}},
 			gatewayProber: gatewayProberStub,
 		}
 
@@ -218,125 +218,26 @@ func TestUpdateStatus(t *testing.T) {
 		require.Equal(t, conditions.ReasonReferencedSecretMissing, cond.Reason)
 	})
 
-	//t.Run("should add pending condition if waiting for lock", func(t *testing.T) {
-	//	pipelineName := "pipeline"
-	//	pipeline := &telemetryv1alpha1.MetricPipeline{
-	//		ObjectMeta: metav1.ObjectMeta{
-	//			Name: pipelineName,
-	//		},
-	//		Spec: telemetryv1alpha1.MetricPipelineSpec{
-	//			Output: telemetryv1alpha1.MetricPipelineOutput{
-	//				Otlp: &telemetryv1alpha1.OtlpOutput{
-	//					Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
-	//				},
-	//			}},
-	//	}
-	//	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).WithStatusSubresource(pipeline).Build()
-	//
-	//	proberStub := &mocks.DeploymentProber{}
-	//	proberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
-	//
-	//	sut := Reconciler{
-	//		Client: fakeClient,
-	//		config: Config{Gateway: otelcollector.GatewayConfig{
-	//			Config: otelcollector.Config{BaseName: "metric-gateway"},
-	//		}},
-	//		gatewayProber: proberStub,
-	//	}
-	//	err := sut.updateStatus(context.Background(), pipeline.Name, false)
-	//	require.NoError(t, err)
-	//
-	//	var updatedPipeline telemetryv1alpha1.MetricPipeline
-	//	_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
-	//	require.Len(t, updatedPipeline.Status.Conditions, 1)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.MetricPipelinePending)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, conditions.ReasonWaitingForLock)
-	//})
+	t.Run("waiting for lock", func(t *testing.T) {
+		pipeline := testutils.NewMetricPipelineBuilder().Build()
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(&pipeline).WithStatusSubresource(&pipeline).Build()
 
-	//t.Run("should add pending condition if acquired lock but metric gateway is not ready", func(t *testing.T) {
-	//	pipelineName := "pipeline"
-	//	pipeline := &telemetryv1alpha1.MetricPipeline{
-	//		ObjectMeta: metav1.ObjectMeta{
-	//			Name: pipelineName,
-	//		},
-	//		Spec: telemetryv1alpha1.MetricPipelineSpec{
-	//			Output: telemetryv1alpha1.MetricPipelineOutput{
-	//				Otlp: &telemetryv1alpha1.OtlpOutput{
-	//					Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
-	//				},
-	//			}},
-	//	}
-	//	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).WithStatusSubresource(pipeline).Build()
-	//
-	//	proberStub := &mocks.DeploymentProber{}
-	//	proberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
-	//
-	//	sut := Reconciler{
-	//		Client: fakeClient,
-	//		config: Config{Gateway: otelcollector.GatewayConfig{
-	//			Config: otelcollector.Config{BaseName: "metric-gateway"},
-	//		}},
-	//		gatewayProber: proberStub,
-	//	}
-	//	err := sut.updateStatus(context.Background(), pipeline.Name, false)
-	//	require.NoError(t, err)
-	//	err = sut.updateStatus(context.Background(), pipeline.Name, true)
-	//	require.NoError(t, err)
-	//
-	//	var updatedPipeline telemetryv1alpha1.MetricPipeline
-	//	_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
-	//	require.Len(t, updatedPipeline.Status.Conditions, 1)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.MetricPipelinePending)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, conditions.ReasonMetricGatewayDeploymentNotReady)
-	//})
-	//
-	//t.Run("should add pending condition if metric gateway deployment is not ready but metric agent is ready", func(t *testing.T) {
-	//	pipelineName := "pipeline"
-	//	pipeline := &telemetryv1alpha1.MetricPipeline{
-	//		ObjectMeta: metav1.ObjectMeta{
-	//			Name: pipelineName,
-	//		},
-	//		Spec: telemetryv1alpha1.MetricPipelineSpec{
-	//			Output: telemetryv1alpha1.MetricPipelineOutput{
-	//				Otlp: &telemetryv1alpha1.OtlpOutput{
-	//					Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
-	//				},
-	//			},
-	//			Input: telemetryv1alpha1.MetricPipelineInput{
-	//				Runtime: telemetryv1alpha1.MetricPipelineRuntimeInput{
-	//					Enabled: pointer.Bool(true),
-	//				},
-	//				Prometheus: telemetryv1alpha1.MetricPipelinePrometheusInput{
-	//					Enabled: pointer.Bool(false),
-	//				},
-	//				Istio: telemetryv1alpha1.MetricPipelineIstioInput{
-	//					Enabled: pointer.Bool(false),
-	//				},
-	//			},
-	//		},
-	//	}
-	//	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).WithStatusSubresource(pipeline).Build()
-	//
-	//	gatewayProberStub := &mocks.DeploymentProber{}
-	//	gatewayProberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
-	//
-	//	agentProberStub := &mocks.DaemonSetProber{}
-	//	agentProberStub.On("IsReady", mock.Anything, mock.Anything).Return(true, nil)
-	//	sut := Reconciler{
-	//		Client: fakeClient,
-	//		config: Config{Gateway: otelcollector.GatewayConfig{
-	//			Config: otelcollector.Config{BaseName: "metric-gateway"},
-	//		}},
-	//		gatewayProber: gatewayProberStub,
-	//		agentProber:   agentProberStub,
-	//	}
-	//	err := sut.updateStatus(context.Background(), pipeline.Name, true)
-	//	require.NoError(t, err)
-	//
-	//	var updatedPipeline telemetryv1alpha1.MetricPipeline
-	//	_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
-	//	require.Len(t, updatedPipeline.Status.Conditions, 1)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.MetricPipelinePending)
-	//	require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, conditions.ReasonMetricGatewayDeploymentNotReady)
-	//})
+		gatewayProberStub := &mocks.DeploymentProber{}
+		gatewayProberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
+
+		sut := Reconciler{
+			Client:        fakeClient,
+			gatewayProber: gatewayProberStub,
+		}
+		err := sut.updateStatus(context.Background(), pipeline.Name, false)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.MetricPipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipeline.Name}, &updatedPipeline)
+
+		cond := meta.FindStatusCondition(updatedPipeline.Status.Conditions, conditions.TypeConfigurationGenerated)
+		require.NotNil(t, cond, "could not find condition of type %s", conditions.TypeConfigurationGenerated)
+		require.Equal(t, metav1.ConditionFalse, cond.Status)
+		require.Equal(t, conditions.ReasonWaitingForLock, cond.Reason)
+	})
 }
