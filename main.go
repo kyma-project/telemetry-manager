78de94fcf5778f12715b86a99a66e35b8f3194fd
Stanislav Khalash <stanislav.khalash@gmail.com>
2023-12-20 11:44:08 +0100

    chore: Bump images and module version 1.6.0 (#655)

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ const (
 	overridesConfigMapName = "telemetry-override-config"
 	overridesConfigMapKey  = "override-config"
 	fluentBitImage         = "europe-docker.pkg.dev/kyma-project/prod/tpi/fluent-bit:2.1.10-a5234020"
-	fluentBitExporterImage = "europe-docker.pkg.dev/kyma-project/prod/directory-size-exporter:v20231206-af68e692"
+	fluentBitExporterImage = "europe-docker.pkg.dev/kyma-project/prod/directory-size-exporter:v20231214-4555a23b"
 
 	fluentBitDaemonSet = "telemetry-fluent-bit"
 	webhookServiceName = "telemetry-operator-webhook"
