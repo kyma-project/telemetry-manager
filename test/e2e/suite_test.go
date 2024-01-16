fec8535aac2e99799ee85e56bff7b48ba84408dd
Mostafa Shorim <mostafa.shorim@sap.com>
2024-01-16 15:48:49 +0100

    fix shadowing of k8sObjects declaration

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -71,7 +71,7 @@ var _ = BeforeSuite(func() {
 
 	telemetryK8sObject := kitk8s.NewTelemetry("default", "kyma-system").Persistent(isOperational()).K8sObject()
 	denyAllNetworkPolicyK8sObject := kitk8s.NewNetworkPolicy("deny-all-ingress-and-egress", kitkyma.SystemNamespaceName).K8sObject()
-	k8sObjects := []client.Object{
+	k8sObjects = []client.Object{
 		telemetryK8sObject,
 		denyAllNetworkPolicyK8sObject,
 	}
