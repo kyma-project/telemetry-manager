9ff8014ab3c6dc077cbf2f85d6eef5f83d284691
Hisar Balik <hisar.balik@sap.com>
2024-02-18 23:47:57 +0100

    feat: Update OTEL image version to 0.94.0 (#796)

--- test/testkit/mocks/backend/deployment.go
+++ test/testkit/mocks/backend/deployment.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	replicas           int32 = 1
-	otelCollectorImage       = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.93.0-fdc9e6c4"
+	otelCollectorImage       = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.94.0-fc04f979"
 	nginxImage               = "europe-docker.pkg.dev/kyma-project/prod/external/nginx:1.23.3"
 	fluentDImage             = "europe-docker.pkg.dev/kyma-project/prod/external/fluent/fluentd:v1.16-debian-1"
 )
