be6bd1318c033fcaecb2333d060d38320ab5a8ce
Mostafa Shorim <mostafa.shorim@sap.com>
2024-02-28 18:54:20 +0100

    fix linting

--- internal/reconciler/tracepipeline/status_test.go
+++ internal/reconciler/tracepipeline/status_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
+
 	//corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
