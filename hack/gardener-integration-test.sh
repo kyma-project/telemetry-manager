e1f5d68de07bae65a166052630ceb3c576b33337
Rakesh Garimella <rakesh.garimella@sap.com>
2024-03-07 15:29:06 +0100

    fix: Get the Ginkgo path properly

--- hack/gardener-integration-test.sh
+++ hack/gardener-integration-test.sh
@@ -7,7 +7,7 @@ set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 readonly LOCALBIN=${LOCALBIN:-$(pwd)/bin}
-readonly GINKGO=${GINKGO:-$(LOCALBIN)/ginkgo}
+readonly GINKGO=${GINKGO:-$LOCALBIN/ginkgo}
 GIT_COMMIT_SHA=$(git rev-parse --short=8 HEAD)
 GIT_COMMIT_DATE=$(git show -s --format=%cd --date=format:'v%Y%m%d' ${GIT_COMMIT_SHA})
 IMG="europe-docker.pkg.dev/kyma-project/prod/telemetry-manager:${GIT_COMMIT_DATE}-${GIT_COMMIT_SHA} make deploy-dev"
