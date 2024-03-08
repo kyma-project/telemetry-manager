e1af876d9511e693b2169b8614683cb764748727
Rakesh Garimella <rakesh.garimella@sap.com>
2024-03-08 12:28:10 +0100

    some fixes

--- docs/contributor/telemetry-load-test/assets/run-load-test.sh
+++ docs/contributor/telemetry-load-test/assets/run-load-test.sh
@@ -16,8 +16,6 @@ BACKPRESSURE_TEST="false"
 TEST_TARGET="traces"
 TEST_NAME="No Name"
 TEST_DURATION=1200
-TELEMETRY_MANAGER_DEPLOYMENT_NAME="telemetry-manager"
-TELEMETRY_MANAGER_NAMESPACE="kyma-system"
 
 while getopts m:b:n:t:d: flag; do
     case "$flag" in
@@ -28,41 +26,6 @@ while getopts m:b:n:t:d: flag; do
         d) TEST_DURATION=${OPTARG} ;;
     esac
 done
-
-# shellcheck disable=SC2112
-function check_deployment_ready() {
-    DESIRED=$(kubectl get deployment "$TELEMETRY_MANAGER_DEPLOYMENT_NAME" -n "$TELEMETRY_MANAGER_NAMESPACE" -o jsonpath='{.spec.replicas}')
-    CURRENT=$(kubectl get deployment "$TELEMETRY_MANAGER_DEPLOYMENT_NAME"  -n "$TELEMETRY_MANAGER_NAMESPACE" -o jsonpath='{.status.readyReplicas}')
-    if [ "$CURRENT" == "$DESIRED" ]; then
-        echo "ready"
-    else
-        echo "not ready"
-    fi
-}
-
-# shellcheck disable=SC2112
-function check_telemetry_manager_is_ready() {
-  MAX_ATTEMPTS=10
-  DELAY_SECONDS=30
-
-  for ((attempts=1; attempts<=MAX_ATTEMPTS; attempts++)); do
-    echo "Checking deployment status"
-    check=$(check_deployment_ready)
-    echo "$check"
-
-    if [ "$check" == "ready" ]; then
-      echo "Telemetry manager running successfully!"
-      return
-    else
-      kubectl get pods -n kyma-system
-      echo "Telemetry manager is not ready. Checking again in $DELAY_SECONDS seconds..."
-      sleep $DELAY_SECONDS
-    fi
-  done
-
-  echo "Maximum attempts reached. Telemetry manager is not ready!"
-  exit 1
-}
 
 # shellcheck disable=SC2112
 function setup() {
