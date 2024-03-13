cbbf53d1bffabfc64345c3cf7ef5fdec1afb868c
Rakesh Garimella <Rakesh.garimella@sap.com>
2024-03-13 18:03:47 +0100

    feat: Add scrape config for self-monitor (#876)

--- internal/resources/otelcollector/agent.go
+++ internal/resources/otelcollector/agent.go
@@ -27,7 +27,7 @@ const istioCertVolumeName = "istio-certs"
 func ApplyAgentResources(ctx context.Context, c client.Client, cfg *AgentConfig) error {
 	name := types.NamespacedName{Namespace: cfg.Namespace, Name: cfg.BaseName}
 
-	if err := applyCommonResources(ctx, c, name, makeAgentClusterRole(name), cfg.allowedPorts); err != nil {
+	if err := applyCommonResources(ctx, c, name, makeAgentClusterRole(name), cfg.allowedPorts, cfg.SelfMonitorLabel); err != nil {
 		return fmt.Errorf("failed to create common resource: %w", err)
 	}
 
