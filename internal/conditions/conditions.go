4eb82e2df03be5cbf43baceb6795cccdcb3f90a8
Rakesh Garimella <Rakesh.garimella@sap.com>
2024-03-18 14:24:27 +0100

    feat: Add default status message to the self-monitor conditions (#902)

--- internal/conditions/conditions.go
+++ internal/conditions/conditions.go
@@ -71,6 +71,11 @@ var MetricsMessage = map[string]string{
 	ReasonDaemonSetNotReady:       "Metric agent DaemonSet is not ready",
 	ReasonDaemonSetReady:          "Metric agent DaemonSet is ready",
 	ReasonMetricComponentsRunning: "All metric components are running",
+	ReasonAllDataDropped:          "All metrics dropped: backend unreachable or rejecting",
+	ReasonSomeDataDropped:         "Some metrics dropped: backend unreachable or rejecting",
+	ReasonBufferFillingUp:         "Buffer nearing capacity: incoming metric rate exceeds export rate",
+	ReasonGatewayThrottling:       "Metric gateway experiencing high influx: Unable to receive metrics at current rate",
+	ReasonFlowHealthy:             "Metrics are flowing normally to backend",
 }
 
 var TracesMessage = map[string]string{
@@ -79,6 +84,11 @@ var TracesMessage = map[string]string{
 	ReasonTraceGatewayDeploymentNotReady: "Trace gateway Deployment is not ready",
 	ReasonTraceGatewayDeploymentReady:    "Trace gateway Deployment is ready",
 	ReasonTraceComponentsRunning:         "All trace components are running",
+	ReasonAllDataDropped:                 "All traces dropped: backend unreachable or rejecting",
+	ReasonSomeDataDropped:                "Some traces dropped: backend unreachable or rejecting",
+	ReasonBufferFillingUp:                "Buffer nearing capacity: incoming trace rate exceeds export rate",
+	ReasonGatewayThrottling:              "Trace collector experiencing high influx: Unable to receive metrics at current rate",
+	ReasonFlowHealthy:                    "Traces are flowing normally to backend",
 }
 
 var LogsMessage = map[string]string{
