431766245f3a0634176c870fbde12f3574d5567a
Andreas Thaler <andreas.thaler01@sap.com>
2024-03-21 15:32:31 +0100

    reset mock backend

--- test/testkit/mocks/backend/deployment.go
+++ test/testkit/mocks/backend/deployment.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	replicas           int32 = 1
-	otelCollectorImage       = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.96.0-79b139cb"
+	otelCollectorImage       = "europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.96.0-7cd3d3a3"
 	nginxImage               = "europe-docker.pkg.dev/kyma-project/prod/external/nginx:1.23.3"
 	fluentDImage             = "europe-docker.pkg.dev/kyma-project/prod/external/fluent/fluentd:v1.16-debian-1"
 )
