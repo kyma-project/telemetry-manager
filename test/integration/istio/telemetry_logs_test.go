ae1abb57b9bf3e00dae8a7559fdb0db2eab84f33
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2024-03-27 15:22:16 +0100

    Working version. Whitelisting WIP

--- test/integration/istio/telemetry_logs_test.go
+++ test/integration/istio/telemetry_logs_test.go
@@ -13,10 +13,13 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/kyma-project/telemetry-manager/internal/otelcollector/ports"
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
 	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
 	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/log"
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
+	kittraces "github.com/kyma-project/telemetry-manager/test/testkit/otel/traces"
 	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/verifiers"
 )
@@ -24,20 +27,23 @@ import (
 // TODO: Change to logs label
 var _ = Describe("Telemetry Components Error/Warning Logs", Label("wip"), Ordered, func() {
 	const (
-		mockNs            = "tlogs-http"
-		logBackendName    = "tlogs-log"
-		metricBackendName = "tlogs-metric"
-		traceBackendName  = "tlogs-trace"
-		nginxImage        = "europe-docker.pkg.dev/kyma-project/prod/external/nginx:1.23.3"
-		curlImage         = "europe-docker.pkg.dev/kyma-project/prod/external/curlimages/curl:7.78.0"
+		mockNs             = "tlogs-http"
+		logBackendName     = "tlogs-log"
+		metricBackendName  = "tlogs-metric"
+		traceBackendName   = "tlogs-trace"
+		nginxImage         = "europe-docker.pkg.dev/kyma-project/prod/external/nginx:1.23.3"
+		curlImage          = "europe-docker.pkg.dev/kyma-project/prod/external/curlimages/curl:7.78.0"
+		pushMetricsDepName = "push-metrics-istiofied"
 	)
 
 	var (
-		logPipelineName       string
-		metricPipelineName    string
-		tracePipelineName     string
-		logTelemetryExportURL string
-		now                   time.Time
+		logPipelineName          string
+		metricPipelineName       string
+		tracePipelineName        string
+		logTelemetryExportURL    string
+		metricTelemetryExportURL string
+		traceTelemetryExportURL  string
+		now                      time.Time
 	)
 
 	sourcePodSpec := func() corev1.PodSpec {
@@ -82,16 +88,11 @@ var _ = Describe("Telemetry Components Error/Warning Logs", Label("wip"), Ordere
 		objs = append(objs, logBackend.K8sObjects()...)
 		logTelemetryExportURL = logBackend.TelemetryExportURL(proxyClient)
 		metricBackend := backend.New(metricBackendName, mockNs, backend.SignalTypeMetrics)
+		metricTelemetryExportURL = metricBackend.TelemetryExportURL(proxyClient)
 		objs = append(objs, metricBackend.K8sObjects()...)
 		traceBackend := backend.New(traceBackendName, mockNs, backend.SignalTypeTraces)
+		traceTelemetryExportURL = traceBackend.TelemetryExportURL(proxyClient)
 		objs = append(objs, traceBackend.K8sObjects()...)
-		// TODO: Generate some traces (see other traces test-cases)
-
-		// metric istio set-up
-		source := kitk8s.NewPod("source", mockNs).WithPodSpec(sourcePodSpec())
-		destination := kitk8s.NewPod("destination", mockNs).WithPodSpec(destinationPodSpec()).WithLabel("app", "destination")
-		service := kitk8s.NewService("destination", mockNs).WithPort("http", 80)
-		objs = append(objs, source.K8sObject(), destination.K8sObject(), service.K8sObject(kitk8s.WithLabel("app", "destination")))
 
 		// components
 		logPipeline := kitk8s.NewLogPipelineV1Alpha1(fmt.Sprintf("%s-pipeline", logBackend.Name())).
@@ -104,6 +105,7 @@ var _ = Describe("Telemetry Components Error/Warning Logs", Label("wip"), Ordere
 			WithOutputEndpointFromSecret(metricBackend.HostSecretRefV1Alpha1()).
 			PrometheusInput(true, kitk8s.IncludeNamespacesV1Alpha1(mockNs)).
 			IstioInput(true, kitk8s.IncludeNamespacesV1Alpha1(mockNs)).
+			OtlpInput(true).
 			RuntimeInput(true, kitk8s.IncludeNamespacesV1Alpha1(mockNs))
 		metricPipelineName = metricPipeline.Name()
 		objs = append(objs, metricPipeline.K8sObject())
@@ -111,6 +113,18 @@ var _ = Describe("Telemetry Components Error/Warning Logs", Label("wip"), Ordere
 			WithOutputEndpointFromSecret(traceBackend.HostSecretRefV1Alpha1())
 		tracePipelineName = tracePipeline.Name()
 		objs = append(objs, tracePipeline.K8sObject())
+
+		// metrics istio set-up (pods)
+		source := kitk8s.NewPod("source", mockNs).WithPodSpec(sourcePodSpec())
+		destination := kitk8s.NewPod("destination", mockNs).WithPodSpec(destinationPodSpec()).WithLabel("app", "destination")
+		service := kitk8s.NewService("destination", mockNs).WithPort("http", 80)
+		objs = append(objs, source.K8sObject(), destination.K8sObject(), service.K8sObject(kitk8s.WithLabel("app", "destination")))
+
+		// metric istio set-up (telemetrygen)
+		podSpec := telemetrygen.PodSpec(telemetrygen.SignalTypeMetrics, "")
+		objs = append(objs,
+			kitk8s.NewDeployment(pushMetricsDepName, mockNs).WithPodSpec(podSpec).K8sObject(),
+		)
 
 		return objs
 	}
@@ -146,11 +160,22 @@ var _ = Describe("Telemetry Components Error/Warning Logs", Label("wip"), Ordere
 			verifiers.DaemonSetShouldBeReady(ctx, k8sClient, kitkyma.MetricAgentName)
 		})
 
+		It("Should push metrics successfully", func() {
+			verifiers.MetricsFromNamespaceShouldBeDelivered(proxyClient, metricTelemetryExportURL, mockNs, telemetrygen.MetricNames)
+		})
+
+		It("Should verify end-to-end trace delivery", func() {
+			gatewayPushURL := proxyClient.ProxyURLForService(kitkyma.SystemNamespaceName, "telemetry-otlp-traces", "v1/traces/", ports.OTLPHTTP)
+			traceID, spanIDs, attrs := kittraces.MakeAndSendTraces(proxyClient, gatewayPushURL)
+			verifiers.TracesShouldBeDelivered(proxyClient, traceTelemetryExportURL, traceID, spanIDs, attrs)
+		})
+
 		// TODO: Whitelist possible (flaky/expected) errors
 		// excludeWhitelistedLogs := func() string {
 		// }
 
 		It("Should not have any ERROR/WARNING level logs in the components", func() {
+			// TODO: Remove time constraint
 			now = time.Now().UTC()
 			Consistently(func(g Gomega) {
 				resp, err := proxyClient.Get(logTelemetryExportURL)
