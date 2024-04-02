9bb7525d280242853fa0f63d17327a6ad6f290d2
Stanislav Khalash <stanislav.khalash@sap.com>
2024-04-02 19:29:20 +0200

    WIP: Parse request and reconcile only the target resource

--- internal/selfmonitor/webhook/handler.go
+++ internal/selfmonitor/webhook/handler.go
@@ -1,16 +1,22 @@
 package webhook
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/go-logr/logr"
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+	"github.com/kyma-project/telemetry-manager/internal/otelcollector/config/otlpexporter"
 	"io"
 	"net/http"
-
-	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"slices"
+	"strings"
 )
 
 type Handler struct {
+	c           client.Reader
 	subscribers []chan<- event.GenericEvent
 	logger      logr.Logger
 }
@@ -29,8 +35,9 @@ func WithSubscriber(subscriber chan<- event.GenericEvent) Option {
 	}
 }
 
-func NewHandler(opts ...Option) *Handler {
+func NewHandler(c client.Reader, opts ...Option) *Handler {
 	h := &Handler{
+		c:      c,
 		logger: logr.New(logf.NullLogSink{}),
 	}
 
@@ -40,8 +47,6 @@ func NewHandler(opts ...Option) *Handler {
 
 	return h
 }
-
-type Request []Alert
 
 type Alert struct {
 	Labels map[string]string `json:"labels"`
@@ -54,15 +59,15 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	reqYAML, err := io.ReadAll(r.Body)
+	alertsYAML, err := io.ReadAll(r.Body)
 	if err != nil {
 		h.logger.Error(err, "Failed to read request body")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	var req Request
-	if unmarshallErr := json.Unmarshal(reqYAML, &req); unmarshallErr != nil {
+	var alerts []Alert
+	if unmarshallErr := json.Unmarshal(alertsYAML, &alerts); unmarshallErr != nil {
 		h.logger.Error(err, "Failed to unmarshal request body")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -70,11 +75,52 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
-	h.logger.V(1).Info("Webhook called. Notifying the subscribers.", "request", req)
+	h.logger.V(1).Info("Webhook called. Notifying the subscribers.", "request", alerts)
 
-	for _, sub := range h.subscribers {
-		sub <- event.GenericEvent{}
+	for _, ev := range h.metricPipelineReconcileEvents(alerts) {
+		for _, sub := range h.subscribers {
+			sub <- ev
+		}
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) metricPipelineReconcileEvents(alerts []Alert) []event.GenericEvent {
+	var events []event.GenericEvent
+
+	var allPipelines telemetryv1alpha1.MetricPipelineList
+	if err := h.c.List(context.TODO(), &allPipelines); err != nil {
+		h.logger.Error(err, "Failed to list MetricPipelines")
+		return nil
+	}
+
+	pipelinesToReconcile := slices.DeleteFunc(allPipelines.Items, func(mp telemetryv1alpha1.MetricPipeline) bool {
+		return matchesNoAlert(&mp, alerts)
+	})
+
+	for _, mp := range pipelinesToReconcile {
+		events = append(events, event.GenericEvent{Object: &mp})
+	}
+
+	return events
+}
+
+func matchesNoAlert(mp *telemetryv1alpha1.MetricPipeline, alerts []Alert) bool {
+	for _, alert := range alerts {
+		if strings.HasPrefix(alert.Labels["alertname"], "Metric") && matchesPipeline(alert.Labels, mp.Name) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func matchesPipeline(labels map[string]string, pipelineName string) bool {
+	exportedID, ok := labels["exporter"]
+	if !ok {
+		return false
+	}
+
+	return otlpexporter.ExporterID(telemetryv1alpha1.OtlpProtocolHTTP, pipelineName) == exportedID || otlpexporter.ExporterID(telemetryv1alpha1.OtlpProtocolGRPC, pipelineName) == exportedID
 }
--- internal/selfmonitor/webhook/handler_test.go
+++ internal/selfmonitor/webhook/handler_test.go
@@ -8,8 +8,15 @@ import (
 	"testing"
 
 	"github.com/go-logr/logr"
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+	"github.com/kyma-project/telemetry-manager/internal/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -22,17 +29,42 @@ func (errReader) Read(p []byte) (n int, err error) {
 
 func TestHandler(t *testing.T) {
 	tests := []struct {
-		name           string
-		requestMethod  string
-		requestBody    io.Reader
-		expectedStatus int
-		expectEvent    bool
+		name                 string
+		requestMethod        string
+		requestBody          io.Reader
+		resources            []client.Object
+		expectedStatus       int
+		expectedResourceName string
+		expectedResourceType any
 	}{
 		{
-			name:           "valid",
-			requestMethod:  http.MethodPost,
+			name:          "alert matches pipeline with same name",
+			requestMethod: http.MethodPost,
+			requestBody:   bytes.NewBuffer([]byte(`[{"labels":{"alertname":"MetricGatewayExporterDroppedData","exporter":"otlp/cls"}}]`)),
+			resources: []client.Object{
+				ptr.To(testutils.NewMetricPipelineBuilder().WithName("cls").Build()),
+			},
+			expectedStatus:       http.StatusOK,
+			expectedResourceName: "cls",
+			expectedResourceType: &telemetryv1alpha1.MetricPipeline{},
+		},
+		{
+			name:          "alert does not match pipeline with other name",
+			requestMethod: http.MethodPost,
+			requestBody:   bytes.NewBuffer([]byte(`[{"labels":{"alertname":"MetricGatewayExporterDroppedData","exporter":"otlp/dynatrace"}}]`)),
+			resources: []client.Object{
+				ptr.To(testutils.NewTracePipelineBuilder().WithName("cls").Build()),
+			},
 			expectedStatus: http.StatusOK,
-			expectEvent:    true,
+		},
+		{
+			name:          "alert does not match pipeline of other type",
+			requestMethod: http.MethodPost,
+			requestBody:   bytes.NewBuffer([]byte(`[{"labels":{"alertname":"MetricGatewayExporterDroppedData","exporter":"otlp/cls"}}]`)),
+			resources: []client.Object{
+				ptr.To(testutils.NewTracePipelineBuilder().WithName("cls").Build()),
+			},
+			expectedStatus: http.StatusOK,
 		},
 		{
 			name:           "invalid method",
@@ -52,10 +84,12 @@ func TestHandler(t *testing.T) {
 			ch := make(chan event.GenericEvent, 1)
 
 			noopLogger := logr.New(logf.NullLogSink{})
-			handler := NewHandler(WithSubscriber(ch), WithLogger(noopLogger))
-			if tc.requestBody == nil {
-				tc.requestBody = bytes.NewBuffer([]byte(`{"key":"value"}`))
-			}
+
+			scheme := runtime.NewScheme()
+			_ = clientgoscheme.AddToScheme(scheme)
+			_ = telemetryv1alpha1.AddToScheme(scheme)
+			fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tc.resources...).Build()
+			handler := NewHandler(fakeClient, WithSubscriber(ch), WithLogger(noopLogger))
 
 			req, err := http.NewRequest(tc.requestMethod, "/", tc.requestBody)
 			require.NoError(t, err)
@@ -64,8 +98,14 @@ func TestHandler(t *testing.T) {
 			handler.ServeHTTP(rr, req)
 
 			require.Equal(t, tc.expectedStatus, rr.Code)
-			if tc.expectEvent {
+			if tc.expectedResourceName != "" {
 				require.NotEmpty(t, ch)
+				event := <-ch
+				require.NotNil(t, event.Object)
+				require.Equal(t, tc.expectedResourceName, event.Object.GetName())
+				require.IsType(t, tc.expectedResourceType, event.Object)
+			} else {
+				require.Empty(t, ch)
 			}
 		})
 	}
