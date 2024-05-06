741cdff8052c6f47424cdd790656990e0df720b1
Hisar Balik <hisar.balik@sap.com>
2024-05-06 15:17:20 +0200

    fix flaky log multi pipeline test

--- test/testkit/kyma/common_names.go
+++ test/testkit/kyma/common_names.go
@@ -18,8 +18,6 @@ const (
 	SelfMonitorBaseName   = "telemetry-self-monitor"
 	DefaultTelemetryName  = "default"
 	WebhookName           = "validation.webhook.telemetry.kyma-project.io"
-
-	LogFluentBitAgentBaseName = "telemetry-fluent-bit"
 )
 
 var (
@@ -46,6 +44,4 @@ var (
 	TelemetryName = types.NamespacedName{Name: DefaultTelemetryName, Namespace: SystemNamespaceName}
 
 	WebhookCertSecret = types.NamespacedName{Name: "telemetry-webhook-cert", Namespace: SystemNamespaceName}
-
-	LogFluentBitAgentPipelineConfigMapName = types.NamespacedName{Name: LogFluentBitAgentBaseName + "-sections", Namespace: SystemNamespaceName}
 )
