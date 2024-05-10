19720e0586e53c2d09798255ed34737f7b991f6f
Hisar Balik <hisar.balik@sap.com>
2024-05-10 13:02:36 +0200

    fix merge issues

--- hack/load-tests/run-load-test.sh
+++ hack/load-tests/run-load-test.sh
@@ -4,7 +4,7 @@
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
-# set -o pipefail # prevents errors in a pipeline from being masked
+set -o pipefail # prevents errors in a pipeline from being masked
 
 PROMETHEUS_NAMESPACE="prometheus"
 HELM_PROM_RELEASE="prometheus"
