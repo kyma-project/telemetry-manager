394efe7ebc1a35385c867666dc2078f5c0003bd0
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2024-05-17 13:02:57 +0200

    add missing test file

---
+++ test/integration/istio/traces_self_monitor_backpressure_test.go
@@ -0,0 +1,94 @@
+//go:build istio
+
+package istio
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kyma-project/telemetry-manager/internal/conditions"
+	"github.com/kyma-project/telemetry-manager/internal/testutils"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+)
+
+var _ = Describe(suite.ID(), Label(suite.LabelSelfMonitoringTracesBackpressure), Ordered, func() {
+	var (
+		mockNs       = "istio-permissive-mtls"
+		pipelineName = suite.ID()
+	)
+
+	makeResources := func() []client.Object {
+		var objs []client.Object
+
+		backend := backend.New(mockNs, backend.SignalTypeTraces, backend.WithAbortFaultInjection(95))
+		objs = append(objs, backend.K8sObjects()...)
+
+		tracePipeline := testutils.NewTracePipelineBuilder().
+			WithName(pipelineName).
+			WithOTLPOutput(testutils.OTLPEndpoint(backend.Endpoint())).
+			Build()
+
+		objs = append(objs,
+			&tracePipeline,
+			telemetrygen.New(mockNs, telemetrygen.SignalTypeTraces,
+				telemetrygen.WithRate(800),
+				telemetrygen.WithWorkers(5)).K8sObject(),
+		)
+
+		return objs
+	}
+
+	Context("Before deploying a tracepipeline", func() {
+		It("Should have a healthy webhook", func() {
+			assert.WebhookHealthy(ctx, k8sClient)
+		})
+	})
+
+	Context("When a tracepipeline exists", Ordered, func() {
+		BeforeAll(func() {
+			k8sObjects := makeResources()
+			DeferCleanup(func() {
+				Expect(kitk8s.DeleteObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+			})
+			Expect(kitk8s.CreateObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+		})
+
+		It("Should have a running tracepipeline", func() {
+			assert.TracePipelineHealthy(ctx, k8sClient, pipelineName)
+		})
+
+		It("Should have a running self-monitor", func() {
+			assert.DeploymentReady(ctx, k8sClient, kitkyma.SelfMonitorName)
+		})
+
+		It("Should have a trace backend running", func() {
+			assert.DeploymentReady(ctx, k8sClient, types.NamespacedName{Namespace: mockNs, Name: backend.DefaultName})
+		})
+
+		It("Should have a telemetrygen running", func() {
+			listOptions := client.ListOptions{
+				LabelSelector: labels.SelectorFromSet(map[string]string{"app.kubernetes.io/name": telemetrygen.DefaultName}),
+				Namespace:     mockNs,
+			}
+
+			// types.NamespacedName{Namespace: mockNs, Name: telemetrygen.DefaultName})
+			assert.PodReady(ctx, k8sClient, listOptions)
+		})
+
+		It("Should wait for the trace flow to gradually become unhealthy", func() {
+			assert.TracePipelineConditionReasonsTransition(ctx, k8sClient, pipelineName, conditions.TypeFlowHealthy, []string{
+				conditions.ReasonFlowHealthy,
+				conditions.ReasonBufferFillingUp,
+				conditions.ReasonSomeDataDropped,
+			})
+		})
+	})
+})
