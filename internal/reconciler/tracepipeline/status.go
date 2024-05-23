739d783c0419643deadae3a679416ea45a56f981
Teodor-Adrian Mihaescu <103431261+TeodorSAP@users.noreply.github.com>
2024-05-23 10:28:28 +0200

    chore: Rename ProbingNotReachable reason to ProbingFailed (#1112)

--- internal/reconciler/tracepipeline/status.go
+++ internal/reconciler/tracepipeline/status.go
@@ -124,7 +124,7 @@ func (r *Reconciler) setFlowHealthCondition(ctx context.Context, pipeline *telem
 	} else {
 		logf.FromContext(ctx).Error(err, "Failed to probe flow health")
 
-		reason = conditions.ReasonSelfMonProbingNotReachable
+		reason = conditions.ReasonSelfMonProbingFailed
 		status = metav1.ConditionUnknown
 	}
 
