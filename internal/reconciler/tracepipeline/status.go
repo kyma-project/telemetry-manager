02d24aea9c6660e44e19a323de27a1108f32e87f
Teodor-Adrian Mihaescu <103431261+TeodorSAP@users.noreply.github.com>
2024-05-28 14:05:54 +0200

    Update internal/reconciler/tracepipeline/status.go
    Co-authored-by: Stanislav Khalash <stanislav.khalash@gmail.com>

--- internal/reconciler/tracepipeline/status.go
+++ internal/reconciler/tracepipeline/status.go
@@ -156,8 +156,7 @@ func flowHealthReasonFor(probeResult prober.OTelPipelineProbeResult) string {
 }
 
 func (r *Reconciler) setLegacyConditions(ctx context.Context, pipeline *telemetryv1alpha1.TracePipeline) {
-	evaluatedCondition := meta.FindStatusCondition(pipeline.Status.Conditions, conditions.TypeConfigurationGenerated)
-	if evaluatedCondition.Status == metav1.ConditionFalse {
+	if meta.IsStatusConditionFalse(pipeline.Status.Conditions, conditions.TypeConfigurationGenerated) {
 		conditions.HandlePendingCondition(&pipeline.Status.Conditions, pipeline.Generation,
 			evaluatedCondition.Reason,
 			evaluatedCondition.Message)
