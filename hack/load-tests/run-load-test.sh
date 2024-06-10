e03d4887664712169bb8b76a94a8ecba2f3f9605
Andreas Thaler <andreas.thaler01@sap.com>
2024-06-10 15:40:38 +0200

    feat: Update to otel-collector 0.102.1 (#1158)

--- hack/load-tests/run-load-test.sh
+++ hack/load-tests/run-load-test.sh
@@ -16,7 +16,7 @@ BACKPRESSURE_TEST="false"
 TEST_TARGET="traces"
 TEST_NAME="No Name"
 TEST_DURATION=1200
-OTEL_IMAGE="europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.99.0-41265c69"
+OTEL_IMAGE="europe-docker.pkg.dev/kyma-project/prod/tpi/otel-collector:0.102.1-fbfb6cdc"
 
 while getopts m:b:n:t:d: flag; do
     case "$flag" in
