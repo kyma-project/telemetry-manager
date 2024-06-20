93253b56b0035d014e71ae708aedc5103a0b35eb
Mostafa Shorim <mostafa.shorim@sap.com>
2024-06-20 15:18:05 +0200

    fix existing unit test for referencing missing secret

--- internal/reconciler/metricpipeline/reconciler_test.go
+++ internal/reconciler/metricpipeline/reconciler_test.go
@@ -421,14 +421,15 @@ func TestReconcile(t *testing.T) {
 		flowHealthProberStub.On("Probe", mock.Anything, pipeline.Name).Return(prober.OTelPipelineProbeResult{}, nil)
 
 		sut := Reconciler{
-			Client:               fakeClient,
-			config:               testConfig,
-			gatewayConfigBuilder: gatewayConfigBuilderMock,
-			pipelineLock:         pipelineLockStub,
-			gatewayProber:        gatewayProberStub,
-			flowHealthProber:     flowHealthProberStub,
-			overridesHandler:     overridesHandlerStub,
-			istioStatusChecker:   istioStatusCheckerStub,
+			Client:                  fakeClient,
+			config:                  testConfig,
+			gatewayConfigBuilder:    gatewayConfigBuilderMock,
+			gatewayResourcesHandler: &otelcollector.GatewayResourcesHandler{Config: testConfig.Gateway},
+			pipelineLock:            pipelineLockStub,
+			gatewayProber:           gatewayProberStub,
+			flowHealthProber:        flowHealthProberStub,
+			overridesHandler:        overridesHandlerStub,
+			istioStatusChecker:      istioStatusCheckerStub,
 		}
 		_, err := sut.Reconcile(context.Background(), ctrl.Request{NamespacedName: types.NamespacedName{Name: pipeline.Name}})
 		require.NoError(t, err)
