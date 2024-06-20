1c9a0404549e861bd359d29374c755e79500d392
Mostafa Shorim <mostafa.shorim@sap.com>
2024-06-21 00:12:16 +0200

    extend metrics resources e2e test

--- test/e2e/metrics_resources_test.go
+++ test/e2e/metrics_resources_test.go
@@ -14,72 +14,226 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
 	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
-var _ = Describe(suite.ID(), Label(suite.LabelMetrics), func() {
+var _ = Describe(suite.ID(), Label(suite.LabelMetrics), Ordered, func() {
 	var pipelineName = suite.ID()
 	const ownerReferenceKind = "MetricPipeline"
 
 	Context("When a MetricPipeline exists", Ordered, func() {
+		endpointKey := "metrics-endpoint"
+		secret := kitk8s.NewOpaqueSecret("metrics-resources", kitkyma.DefaultNamespaceName, kitk8s.WithStringData(endpointKey, "http://localhost:4317"))
+		metricPipeline := testutils.NewMetricPipelineBuilder().
+			WithName(pipelineName).
+			WithOTLPOutput(testutils.OTLPEndpointFromSecret(secret.Name(), secret.Namespace(), endpointKey)).
+			WithRuntimeInput(true).
+			Build()
 
 		BeforeAll(func() {
-			pipeline := testutils.NewMetricPipelineBuilder().WithName(pipelineName).Build()
-
 			DeferCleanup(func() {
-				Expect(kitk8s.DeleteObjects(ctx, k8sClient, &pipeline)).Should(Succeed())
+				Expect(kitk8s.DeleteObjects(ctx, k8sClient, &metricPipeline)).Should(Succeed())
 			})
-			Expect(kitk8s.CreateObjects(ctx, k8sClient, &pipeline)).Should(Succeed())
+			Expect(kitk8s.CreateObjects(ctx, k8sClient, &metricPipeline, secret.K8sObject())).Should(Succeed())
 		})
 
-		It("Should have a ServiceAccount owned by the MetricPipeline", func() {
-			var serviceAccount corev1.ServiceAccount
-			assert.HasOwnerReference(ctx, k8sClient, &serviceAccount, kitkyma.MetricGatewayServiceAccount, ownerReferenceKind, pipelineName)
+		Context("Should have gateway resources", Ordered, func() {
+			It("Should have a gateway ServiceAccount owned by the MetricPipeline", func() {
+				var serviceAccount corev1.ServiceAccount
+				assert.HasOwnerReference(ctx, k8sClient, &serviceAccount, kitkyma.MetricGatewayServiceAccount, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway ClusterRole owned by the MetricPipeline", func() {
+				var clusterRole rbacv1.ClusterRole
+				assert.HasOwnerReference(ctx, k8sClient, &clusterRole, kitkyma.MetricGatewayClusterRole, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway ClusterRoleBinding owned by the MetricPipeline", func() {
+				var clusterRoleBinding rbacv1.ClusterRoleBinding
+				assert.HasOwnerReference(ctx, k8sClient, &clusterRoleBinding, kitkyma.MetricGatewayClusterRoleBinding, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway Metrics service owned by the MetricPipeline", func() {
+				var service corev1.Service
+				assert.HasOwnerReference(ctx, k8sClient, &service, kitkyma.MetricGatewayMetricsService, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway Network Policy owned by the MetricPipeline", func() {
+				var networkPolicy networkingv1.NetworkPolicy
+				assert.HasOwnerReference(ctx, k8sClient, &networkPolicy, kitkyma.MetricGatewayNetworkPolicy, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway Secret owned by the MetricPipeline", func() {
+				var secret corev1.Secret
+				assert.HasOwnerReference(ctx, k8sClient, &secret, kitkyma.MetricGatewaySecretName, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway ConfigMap owned by the MetricPipeline", func() {
+				var configMap corev1.ConfigMap
+				assert.HasOwnerReference(ctx, k8sClient, &configMap, kitkyma.MetricGatewayConfigMap, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway Deployment owned by the MetricPipeline", func() {
+				var deployment appsv1.Deployment
+				assert.HasOwnerReference(ctx, k8sClient, &deployment, kitkyma.MetricGatewayName, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway OTLP Service owned by the MetricPipeline", func() {
+				var service corev1.Service
+				assert.HasOwnerReference(ctx, k8sClient, &service, kitkyma.MetricGatewayOTLPService, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have a gateway Deployment with correct pod priority class", func() {
+				assert.DeploymentHasPriorityClass(ctx, k8sClient, kitkyma.MetricGatewayName, "telemetry-priority-class")
+			})
 		})
 
-		It("Should have a ClusterRole owned by the MetricPipeline", func() {
-			var clusterRole rbacv1.ClusterRole
-			assert.HasOwnerReference(ctx, k8sClient, &clusterRole, kitkyma.MetricGatewayClusterRole, ownerReferenceKind, pipelineName)
+		Context("Should have agent resources", Ordered, func() {
+			It("Should have an agent ServiceAccount owned by the MetricPipeline", func() {
+				var serviceAccount corev1.ServiceAccount
+				assert.HasOwnerReference(ctx, k8sClient, &serviceAccount, kitkyma.MetricAgentServiceAccount, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent ClusterRole owned by the MetricPipeline", func() {
+				var clusterRole rbacv1.ClusterRole
+				assert.HasOwnerReference(ctx, k8sClient, &clusterRole, kitkyma.MetricAgentClusterRole, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent ClusterRoleBinding owned by the MetricPipeline", func() {
+				var clusterRoleBinding rbacv1.ClusterRoleBinding
+				assert.HasOwnerReference(ctx, k8sClient, &clusterRoleBinding, kitkyma.MetricAgentClusterRoleBinding, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent Metrics service owned by the MetricPipeline", func() {
+				var service corev1.Service
+				assert.HasOwnerReference(ctx, k8sClient, &service, kitkyma.MetricAgentMetricsService, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent Network Policy owned by the MetricPipeline", func() {
+				var networkPolicy networkingv1.NetworkPolicy
+				assert.HasOwnerReference(ctx, k8sClient, &networkPolicy, kitkyma.MetricAgentNetworkPolicy, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent ConfigMap owned by the MetricPipeline", func() {
+				var configMap corev1.ConfigMap
+				assert.HasOwnerReference(ctx, k8sClient, &configMap, kitkyma.MetricAgentConfigMap, ownerReferenceKind, pipelineName)
+			})
+
+			It("Should have an agent DaemonSet owned by the MetricPipeline", func() {
+				var daemonSet appsv1.DaemonSet
+				assert.HasOwnerReference(ctx, k8sClient, &daemonSet, kitkyma.MetricAgentName, ownerReferenceKind, pipelineName)
+			})
 		})
 
-		It("Should have a ClusterRoleBinding owned by the MetricPipeline", func() {
-			var clusterRoleBinding rbacv1.ClusterRoleBinding
-			assert.HasOwnerReference(ctx, k8sClient, &clusterRoleBinding, kitkyma.MetricGatewayClusterRoleBinding, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a Metrics service owned by the MetricPipeline", func() {
-			var service corev1.Service
-			assert.HasOwnerReference(ctx, k8sClient, &service, kitkyma.MetricGatewayMetricsService, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a Network Policy owned by the MetricPipeline", func() {
-			var networkPolicy networkingv1.NetworkPolicy
-			assert.HasOwnerReference(ctx, k8sClient, &networkPolicy, kitkyma.MetricGatewayNetworkPolicy, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a Secret owned by the MetricPipeline", func() {
-			var secret corev1.Secret
-			assert.HasOwnerReference(ctx, k8sClient, &secret, kitkyma.MetricGatewaySecretName, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a ConfigMap owned by the MetricPipeline", func() {
-			var configMap corev1.ConfigMap
-			assert.HasOwnerReference(ctx, k8sClient, &configMap, kitkyma.MetricGatewayConfigMap, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a Deployment owned by the MetricPipeline", func() {
-			var deployment appsv1.Deployment
-			assert.HasOwnerReference(ctx, k8sClient, &deployment, kitkyma.MetricGatewayName, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have an OTLP Service owned by the MetricPipeline", func() {
-			var service corev1.Service
-			assert.HasOwnerReference(ctx, k8sClient, &service, kitkyma.MetricGatewayOTLPService, ownerReferenceKind, pipelineName)
-		})
-
-		It("Should have a Deployment with correct pod priority class", func() {
-			assert.DeploymentHasPriorityClass(ctx, k8sClient, kitkyma.MetricGatewayName, "telemetry-priority-class")
+		It("Should clean up gateway and agent resources when pipeline becomes non-reconcilable", func() {
+			By("Deleting referenced secret", func() {
+				Expect(k8sClient.Delete(ctx, secret.K8sObject())).Should(Succeed())
+			})
+			gatewayResourcesAreDeleted()
+			agentResourcesAreDeleted()
 		})
 
 	})
 })
+
+func gatewayResourcesAreDeleted() {
+	Eventually(func(g Gomega) bool {
+		var serviceAccount corev1.ServiceAccount
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayServiceAccount, &serviceAccount)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var clusterRole rbacv1.ClusterRole
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayClusterRole, &clusterRole)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var clusterRoleBinding rbacv1.ClusterRoleBinding
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayClusterRoleBinding, &clusterRoleBinding)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var service corev1.Service
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayMetricsService, &service)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var networkPolicy networkingv1.NetworkPolicy
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayNetworkPolicy, &networkPolicy)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var secret corev1.Secret
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewaySecretName, &secret)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var configMap corev1.ConfigMap
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayConfigMap, &configMap)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var service corev1.Service
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayOTLPService, &service)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var deployment appsv1.Deployment
+		err := k8sClient.Get(ctx, kitkyma.MetricGatewayName, &deployment)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+}
+
+func agentResourcesAreDeleted() {
+	Eventually(func(g Gomega) bool {
+		var serviceAccount corev1.ServiceAccount
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentServiceAccount, &serviceAccount)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var clusterRole rbacv1.ClusterRole
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentClusterRole, &clusterRole)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var clusterRoleBinding rbacv1.ClusterRoleBinding
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentClusterRoleBinding, &clusterRoleBinding)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var service corev1.Service
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentMetricsService, &service)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var networkPolicy networkingv1.NetworkPolicy
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentNetworkPolicy, &networkPolicy)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var configMap corev1.ConfigMap
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentConfigMap, &configMap)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+
+	Eventually(func(g Gomega) bool {
+		var daemonSet appsv1.DaemonSet
+		err := k8sClient.Get(ctx, kitkyma.MetricAgentName, &daemonSet)
+		return apierrors.IsNotFound(err)
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+}
