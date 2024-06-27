4ec63ce3a7839e8bbb1dbc68088daa3ce308c461
Stanislav Khalash <stanislav.khalash@sap.com>
2024-06-27 11:13:18 +0200

    Remove unneeded targets and merge helper mk files

--- hack/make/provision.mk
+++ hack/make/provision.mk
@@ -1,18 +1,34 @@
+## Location to install dependencies to
+LOCALBIN ?= $(shell pwd)/bin
+$(LOCALBIN):
+	mkdir -p $(LOCALBIN)
+
+## Tool Binaries
+K3D ?= $(LOCALBIN)/k3d
+KYMA ?= $(LOCALBIN)/kyma-$(KYMA_STABILITY)
+
+## Tool Versions
+K3D_VERSION ?= $(ENV_K3D_VERSION)
+
+## k3d
+K3D_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh"
+.PHONY: k3d
+k3d: $(K3D) ## Download k3d locally if necessary. If wrong version is installed, it will be removed before downloading.
+$(K3D): $(LOCALBIN)
+	@if test -x $(K3D) && ! $(K3D) version | grep -q $(K3D_VERSION); then \
+		echo "$(K3D) version is not as expected '$(K3D_VERSION)'. Removing it before installing."; \
+		rm -rf $(K3D); \
+	fi
+	test -s $(K3D) || curl -s $(K3D_INSTALL_SCRIPT) | PATH="$(PATH):$(LOCALBIN)" USE_SUDO=false K3D_INSTALL_DIR=$(LOCALBIN) TAG=$(K3D_VERSION) bash
 ##@ k3d
 .PHONY: provision-k3d
 provision-k3d: k3d
 	K8S_VERSION=$(K3S_K8S_VERSION) hack/provision-k3d.sh
 
-.PHONY: provision-k3d-e2e
-provision-k3d-e2e: kyma kustomize provision-k3d ## Provision a k3d cluster and deploy module with the lifecycle manager. Manager image and module image are pushed to local k3d registry
-	hack/build-image.sh
-	KYMA=${KYMA} KUSTOMIZE=${KUSTOMIZE} hack/deploy-module.sh
-
 .PHONY: provision-k3d-istio
 provision-k3d-istio: provision-k3d
 	hack/build-image.sh
 	hack/deploy-istio.sh
-
 
 ##@ Gardener
 ## injected by the environment
