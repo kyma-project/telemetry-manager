18757fcbfe9dc112f31801d138136949de7b3764
Mostafa Shorim <mostafa.shorim@sap.com>
2024-08-05 00:30:45 +0200

    fix linting

--- internal/otelcollector/config/metric/gateway/receivers.go
+++ internal/otelcollector/config/metric/gateway/receivers.go
@@ -2,6 +2,7 @@ package gateway
 
 import (
 	"fmt"
+
 	"github.com/kyma-project/telemetry-manager/internal/otelcollector/config"
 	"github.com/kyma-project/telemetry-manager/internal/otelcollector/ports"
 )
