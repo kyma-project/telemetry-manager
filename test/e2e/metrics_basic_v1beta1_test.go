000962f24e0d617700c0a350a277a5d50cfed710
Mostafa Shorim <mostafa.shorim@sap.com>
2024-08-05 12:59:53 +0200

    rename v1beta1 test label to experimental

--- test/e2e/metrics_basic_v1beta1_test.go
+++ test/e2e/metrics_basic_v1beta1_test.go
@@ -26,7 +26,7 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 )
 
-var _ = Describe(suite.ID(), Label(suite.LabelMetrics, suite.LabelV1Beta1), Ordered, func() {
+var _ = Describe(suite.ID(), Label(suite.LabelMetrics, suite.LabelExperimental), Ordered, func() {
 	var (
 		mockNs           = suite.ID()
 		pipelineName     = suite.ID()
