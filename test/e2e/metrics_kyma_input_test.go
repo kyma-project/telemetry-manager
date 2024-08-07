e3632d3790a64344e1959beb27d698f457211c4c
Mostafa Shorim <mostafa.shorim@sap.com>
2024-08-07 17:00:14 +0200

    remove the dummy test label

--- test/e2e/metrics_kyma_input_test.go
+++ test/e2e/metrics_kyma_input_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 )
 
-var _ = Describe(suite.ID(), Label(suite.LabelMetrics, suite.LabelExperimental, "test"), Ordered, func() {
+var _ = Describe(suite.ID(), Label(suite.LabelMetrics, suite.LabelExperimental), Ordered, func() {
 	var (
 		mockNs = suite.ID()
 
