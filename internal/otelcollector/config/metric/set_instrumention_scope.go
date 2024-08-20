08b5f3624eaa5b5c254007003862aaddf8c324e5
Hisar Balik <hisar.balik@sap.com>
2024-08-20 14:59:37 +0200

    update scopne name of component otelcol/kymsstats

--- internal/otelcollector/config/metric/set_instrumention_scope.go
+++ internal/otelcollector/config/metric/set_instrumention_scope.go
@@ -10,7 +10,7 @@ var upstreamInstrumentationScopeName = map[InputSourceType]string{
 	InputSourceRuntime:    "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kubeletstatsreceiver",
 	InputSourcePrometheus: "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver",
 	InputSourceIstio:      "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver",
-	InputSourceKyma:       "otelcol/kymastats",
+	InputSourceKyma:       "github.com/kyma-project/opentelemetry-collector-components/receiver/kymastatsreceiver",
 }
 
 func MakeInstrumentationScopeProcessor(inputSource InputSourceType, instrumentationScopeVersion string) *TransformProcessor {
