cda3f9a596a670ec2b988686fbeb9814b5baed8c
Rakesh Garimella <rakesh.garimella@sap.com>
2024-08-28 16:26:09 +0200

    fix

--- internal/otelcollector/ports/ports.go
+++ internal/otelcollector/ports/ports.go
@@ -1,10 +1,10 @@
 package ports
 
 const (
-	OTLPHTTP    = int32(4318)
-	OTLPGRPC    = int32(4317)
-	Metrics     = int32(8888)
-	HealthCheck = int32(13133)
-	Pprof       = int32(1777)
-	IstioEnvoy  = int32(15090)
+	OTLPHTTP    int32 = 4318
+	OTLPGRPC    int32 = 4317
+	Metrics     int32 = 8888
+	HealthCheck int32 = 13133
+	Pprof       int32 = 1777
+	IstioEnvoy  int32 = 15090
 )
