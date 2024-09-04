f6aec48e1009dbf59c1ff7c394570e921cec58d6
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2024-09-04 15:58:53 +0200

    test: Embellish and refactor endpoint E2E tests

---
+++ test/e2e/traces_endpoint_invalid_port_test.go
@@ -0,0 +1,80 @@
+//go:build e2e
+
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kyma-project/telemetry-manager/internal/conditions"
+	"github.com/kyma-project/telemetry-manager/internal/testutils"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+)
+
+var _ = Describe(suite.ID(), Label(suite.LabelTraces), func() {
+	const (
+		invalidPortEndpoint = "http://example.com:9abc8"
+		missingPortEndpoint = "http://example.com:/"
+	)
+
+	var (
+		mockNs                = suite.ID()
+		pipelineNameInvalid = suite.IDWithSuffix("invalid")
+		pipelineNameMissing = suite.IDWithSuffix("missing")
+	)
+
+	makeResources := func() []client.Object {
+		var objs []client.Object
+		objs = append(objs, kitk8s.NewNamespace(mockNs).K8sObject())
+
+		tracePipelineInvalidPort := testutils.NewTracePipelineBuilder().
+			WithName(pipelineNameInvalid).
+			WithOTLPOutput(
+				testutils.OTLPEndpoint(invalidPortEndpoint),
+			).
+			Build()
+
+		tracePipelineMissingPort := testutils.NewTracePipelineBuilder().
+			WithName(pipelineNameMissing).
+			WithOTLPOutput(
+				testutils.OTLPEndpoint(missingPortEndpoint),
+			).
+			Build()
+
+		objs = append(objs,
+			&tracePipelineInvalidPort,
+			&tracePipelineMissingPort,
+		)
+
+		return objs
+	}
+
+	Context("When trace pipelines with an invalid or missing Endpoint port are created", Ordered, func() {
+		BeforeAll(func() {
+			k8sObjects := makeResources()
+
+			DeferCleanup(func() {
+				Expect(kitk8s.DeleteObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+			})
+			Expect(kitk8s.CreateObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+		})
+
+		It("Should set ConfigurationGenerated condition to False in pipelines", func() {
+			assert.TracePipelineHasCondition(ctx, k8sClient, pipelineNameInvalid, metav1.Condition{
+				Type:   conditions.TypeConfigurationGenerated,
+				Status: metav1.ConditionFalse,
+				Reason: conditions.ReasonEndpointInvalid,
+			})
+
+			assert.TracePipelineHasCondition(ctx, k8sClient, pipelineNameMissing, metav1.Condition{
+				Type:   conditions.TypeConfigurationGenerated,
+				Status: metav1.ConditionFalse,
+				Reason: conditions.ReasonEndpointInvalid,
+			})
+		})
+	})
+})
