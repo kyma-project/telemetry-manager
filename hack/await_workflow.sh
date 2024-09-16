4ae505375584a91c68b21af5a9b22f9a94ef40e3
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2024-09-16 10:35:33 +0200

    chore: Rename build-image check name

--- hack/await_workflow.sh
+++ hack/await_workflow.sh
@@ -9,7 +9,7 @@ set -o pipefail # prevents errors in a pipeline from being masked
 # Variables (you need to set these)
 REPO_OWNER="kyma-project"
 REPO_NAME="telemetry-manager"
-CHECK_NAME="build-image"
+CHECK_NAME="build-image / Build image"
 
 # retry until check is found and status is completed
 # timeout after 15 minutes
