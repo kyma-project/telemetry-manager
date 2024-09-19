bb557a969465af0c10039e5a6a7a245adf009fc2
Stanislav Khalash <stanislav.khalash@sap.com>
2024-09-19 11:59:08 +0200

    Implemented ConvertFrom

--- apis/telemetry/v1alpha1/logpipeline_conversion.go
+++ apis/telemetry/v1alpha1/logpipeline_conversion.go
@@ -6,7 +6,7 @@ import (
 	telemetryv1beta1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1beta1"
 )
 
-// ConvertTo converts this CronJob to the Hub version (v1).
+// ConvertTo converts this LogPipeline to the Hub version (v1beta1).
 func (src *LogPipeline) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*telemetryv1beta1.LogPipeline)
 
@@ -29,8 +29,7 @@ func (src *LogPipeline) ConvertTo(dstRaw conversion.Hub) error {
 		dst.Spec.Filters = append(dst.Spec.Filters, telemetryv1beta1.LogPipelineFilter(f))
 	}
 
-	srcHTTPOutput := src.Spec.Output.HTTP
-	if srcHTTPOutput != nil {
+	if srcHTTPOutput := src.Spec.Output.HTTP; srcHTTPOutput != nil {
 		dst.Spec.Output.HTTP = &telemetryv1beta1.LogPipelineHTTPOutput{
 			Host:      v1Alpha1ValueTypeToV1Beta1(srcHTTPOutput.Host),
 			User:      v1Alpha1ValueTypeToV1Beta1(srcHTTPOutput.User),
@@ -44,8 +43,7 @@ func (src *LogPipeline) ConvertTo(dstRaw conversion.Hub) error {
 		}
 	}
 
-	srcCustomOutput := src.Spec.Output.Custom
-	if srcCustomOutput != "" {
+	if srcCustomOutput := src.Spec.Output.Custom; srcCustomOutput != "" {
 		dst.Spec.Output.Custom = srcCustomOutput
 	}
 
@@ -60,6 +58,7 @@ func v1Alpha1ValueTypeToV1Beta1(src ValueType) telemetryv1beta1.ValueType {
 			},
 		}
 	}
+
 	return telemetryv1beta1.ValueType{
 		Value: src.Value,
 	}
@@ -67,20 +66,99 @@ func v1Alpha1ValueTypeToV1Beta1(src ValueType) telemetryv1beta1.ValueType {
 
 func v1Alpha1TLSToV1Beta1(src TLSConfig) telemetryv1beta1.LogPipelineHTTPOutputTLS {
 	var dst telemetryv1beta1.LogPipelineHTTPOutputTLS
+
 	if src.CA != nil {
 		ca := v1Alpha1ValueTypeToV1Beta1(*src.CA)
 		dst.CA = &ca
 	}
+
 	if src.Cert != nil {
 		cert := v1Alpha1ValueTypeToV1Beta1(*src.Cert)
 		dst.Cert = &cert
 	}
+
 	if src.Key != nil {
 		key := v1Alpha1ValueTypeToV1Beta1(*src.Key)
 		dst.Key = &key
 	}
+
 	dst.Disabled = src.Disabled
 	dst.SkipCertificateValidation = src.SkipCertificateValidation
 
 	return dst
+}
+
+// ConvertFrom converts from the Hub version (v1beta1) to this version.
+func (dst *LogPipeline) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*telemetryv1beta1.LogPipeline)
+
+	if srcAppInput := src.Spec.Input.Runtime; srcAppInput != nil {
+		dst.Spec.Input.Application = ApplicationInput{
+			Namespaces:       InputNamespaces(srcAppInput.Namespaces),
+			Containers:       InputContainers(srcAppInput.Containers),
+			KeepAnnotations:  srcAppInput.KeepAnnotations,
+			DropLabels:       srcAppInput.DropLabels,
+			KeepOriginalBody: srcAppInput.KeepOriginalBody,
+		}
+	}
+
+	for _, f := range src.Spec.Files {
+		dst.Spec.Files = append(dst.Spec.Files, FileMount(f))
+	}
+
+	for _, f := range src.Spec.Filters {
+		dst.Spec.Filters = append(dst.Spec.Filters, Filter(f))
+	}
+
+	if srcHTTPOutput := src.Spec.Output.HTTP; srcHTTPOutput != nil {
+		dst.Spec.Output.HTTP = &HTTPOutput{
+			Host:      v1Beta1ValueTypeToV1Alpha1(srcHTTPOutput.Host),
+			User:      v1Beta1ValueTypeToV1Alpha1(srcHTTPOutput.User),
+			Password:  v1Beta1ValueTypeToV1Alpha1(srcHTTPOutput.Password),
+			URI:       srcHTTPOutput.URI,
+			Port:      srcHTTPOutput.Port,
+			Compress:  srcHTTPOutput.Compress,
+			Format:    srcHTTPOutput.Format,
+			TLSConfig: v1Beta1TLSToV1Alpha1(srcHTTPOutput.TLSConfig),
+			Dedot:     srcHTTPOutput.Dedot,
+		}
+	}
+
+	if srcCustomOutput := src.Spec.Output.Custom; srcCustomOutput != "" {
+		dst.Spec.Output.Custom = srcCustomOutput
+	}
+
+	return nil
+}
+
+func v1Beta1TLSToV1Alpha1(src telemetryv1beta1.LogPipelineHTTPOutputTLS) TLSConfig {
+	var dst TLSConfig
+	if src.CA != nil {
+		ca := v1Beta1ValueTypeToV1Alpha1(*src.CA)
+		dst.CA = &ca
+	}
+	if src.Cert != nil {
+		cert := v1Beta1ValueTypeToV1Alpha1(*src.Cert)
+		dst.Cert = &cert
+	}
+	if src.Key != nil {
+		key := v1Beta1ValueTypeToV1Alpha1(*src.Key)
+		dst.Key = &key
+	}
+	dst.Disabled = src.Disabled
+	dst.SkipCertificateValidation = src.SkipCertificateValidation
+	return dst
+}
+
+func v1Beta1ValueTypeToV1Alpha1(src telemetryv1beta1.ValueType) ValueType {
+	if src.ValueFrom != nil && src.ValueFrom.SecretKeyRef != nil {
+		return ValueType{
+			ValueFrom: &ValueFromSource{
+				SecretKeyRef: (*SecretKeyRef)(src.ValueFrom.SecretKeyRef),
+			},
+		}
+	}
+	return ValueType{
+		Value: src.Value,
+	}
 }
