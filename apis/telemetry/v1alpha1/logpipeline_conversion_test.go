e430a86b050376a09d6f0471472261a63a11c44a
Stanislav Khalash <stanislav.khalash@sap.com>
2024-09-22 19:44:32 +0200

    Use go-cmp

--- apis/telemetry/v1alpha1/logpipeline_conversion_test.go
+++ apis/telemetry/v1alpha1/logpipeline_conversion_test.go
@@ -1,9 +1,9 @@
 package v1alpha1
 
 import (
-	"reflect"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
@@ -100,8 +100,7 @@ func TestConvertTo(t *testing.T) {
 	srcAfterRoundTrip := &LogPipeline{}
 	err = srcAfterRoundTrip.ConvertFrom(dst)
 	require.NoError(t, err)
-
-	require.True(t, reflect.DeepEqual(src, srcAfterRoundTrip), "expected source and source after round-trip to be equal")
+	require.Empty(t, cmp.Diff(src, srcAfterRoundTrip), "expected source be equal to itself after round-trip")
 }
 
 func TestConvertFrom(t *testing.T) {
@@ -193,8 +192,7 @@ func TestConvertFrom(t *testing.T) {
 	srcAfterRoundTrip := &telemetryv1beta1.LogPipeline{}
 	err = dst.ConvertTo(srcAfterRoundTrip)
 	require.NoError(t, err, "expected no error during ConvertFrom (round-trip)")
-
-	require.True(t, reflect.DeepEqual(src, srcAfterRoundTrip), "expected source and source after round-trip to be equal")
+	require.Empty(t, cmp.Diff(src, srcAfterRoundTrip), "expected source be equal to itself after round-trip")
 }
 
 func requireLogPipelinesEquivalent(t *testing.T, x *LogPipeline, y *telemetryv1beta1.LogPipeline) {
@@ -238,52 +236,3 @@ func requireLogPipelinesEquivalent(t *testing.T, x *LogPipeline, y *telemetryv1b
 	require.Equal(t, x.Status.UnsupportedMode, y.Status.UnsupportedMode, "status unsupported mode mismatch")
 	require.ElementsMatch(t, x.Status.Conditions, y.Status.Conditions, "status conditions mismatch")
 }
-
-// // TestRoundTripConversion tests round-trip conversion between and v1beta1
-// func TestRoundTripConversion(t *testing.T) {
-// 	original := &LogPipeline{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name: "log-pipeline-test",
-// 		},
-// 		Spec: LogPipelineSpec{
-// 			Input: LogPipelineInputSpec{
-// 				Application: ApplicationInput{
-// 					Namespaces:       []string{"default", "kube-system"},
-// 					Containers:       []string{"nginx", "app"},
-// 					KeepAnnotations:  true,
-// 					DropLabels:       true,
-// 					KeepOriginalBody: true,
-// 				},
-// 			},
-// 			Files:   []FileMount{"file1", "file2"},
-// 			Filters: []Filter{"filter1", "filter2"},
-// 			Output: LogPipelineOutputSpec{
-// 				Custom: "custom-output",
-// 				HTTP: &HTTPOutput{
-// 					Host:     ValueType{Value: "http://localhost"},
-// 					Port:     8080,
-// 					Compress: true,
-// 				},
-// 			},
-// 		},
-// 		Status: LogPipelineStatus("Running"),
-// 	}
-//
-// 	v1beta := &v1beta1.LogPipeline{}
-// 	v1alpha := &LogPipeline{}
-//
-// 	// Convert original -> v1beta1
-// 	err := original.ConvertTo(v1beta)
-// 	require.NoError(t, err, "expected no error during ConvertTo")
-//
-// 	// Convert back v1beta1 -> v1alpha1
-// 	err = v1alpha.ConvertFrom(v1beta)
-// 	require.NoError(t, err, "expected no error during ConvertFrom")
-//
-// 	// Assert that the original and the round-tripped version are the same
-// 	require.Equal(t, original.ObjectMeta, v1alpha.ObjectMeta, "metadata mismatch after round-trip conversion")
-// 	require.Equal(t, original.Spec.Input.Application.Namespaces, v1alpha.Spec.Input.Application.Namespaces, "input namespaces mismatch after round-trip conversion")
-// 	require.Equal(t, original.Spec.Output.Custom, v1alpha.Spec.Output.Custom, "custom output mismatch after round-trip conversion")
-// 	require.Equal(t, original.Spec.Output.HTTP.Host.Value, v1alpha.Spec.Output.HTTP.Host.Value, "HTTP host mismatch after round-trip conversion")
-// 	require.Equal(t, string(original.Status), string(v1alpha.Status), "status mismatch after round-trip conversion")
-// }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.1
 require (
 	github.com/go-logr/logr v1.4.2
 	github.com/go-logr/zapr v1.3.0
+	github.com/google/go-cmp v0.6.0
 	github.com/google/uuid v1.6.0
 	github.com/onsi/ginkgo/v2 v2.20.2
 	github.com/onsi/gomega v1.34.2
@@ -43,7 +44,6 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
