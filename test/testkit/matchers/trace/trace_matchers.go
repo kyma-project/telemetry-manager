8a8ed2ea448e78ca140926744d9702e0c2d62d8e
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2024-09-25 13:16:46 +0200

    add new matchers

--- test/testkit/matchers/trace/trace_matchers.go
+++ test/testkit/matchers/trace/trace_matchers.go
@@ -9,6 +9,60 @@ import (
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
+func HaveFlatTraces(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(jsonTraces []byte) ([]FlatTrace, error) {
+		tds, err := unmarshalTraces(jsonTraces)
+		if err != nil {
+			return nil, fmt.Errorf("HaveFlatTraces requires a valid OTLP JSON document: %w", err)
+		}
+
+		ft := flattenAllTraces(tds)
+		return ft, nil
+	}, matcher)
+
+}
+
+// HaveName extracts name from FlatTrace and applies the matcher to it.
+func HaveName(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) string {
+		return ft.Name
+	}, matcher)
+}
+
+// HaveSpanID extracts ScopeID from FlatTrace and applies the matcher to it.
+func HaveSpanID(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) string {
+		return ft.SpanID
+	}, matcher)
+}
+
+func HaveTraceID(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) string {
+		return ft.TraceID
+	}, matcher)
+}
+
+// HaveScopeName extracts scope name from FlatTrace and applies the matcher to it.
+func HaveScopeName(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) string {
+		return ft.ScopeName
+	}, matcher)
+}
+
+// HaveScopeVersion extracts scope version from FlatTrace and applies the matcher to it.
+func HaveScopeVersion(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) string {
+		return ft.ScopeVersion
+	}, matcher)
+}
+
+// HaveResourceAttributes extracts resource attributes from FlatTrace and applies the matcher to them.
+func HaveResourceAttributes(matcher types.GomegaMatcher) types.GomegaMatcher {
+	return gomega.WithTransform(func(ft FlatTrace) map[string]string {
+		return ft.ResourceAttributes
+	}, matcher)
+}
+
 func WithTds(matcher types.GomegaMatcher) types.GomegaMatcher {
 	return gomega.WithTransform(func(jsonlTraces []byte) ([]ptrace.Traces, error) {
 		tds, err := unmarshalTraces(jsonlTraces)
