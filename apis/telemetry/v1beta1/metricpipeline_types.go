0a19e3313b9919d319f5b0e1c9fd1d273c4f6ed0
Rakesh Garimella <rakesh.garimella@sap.com>
2024-10-15 17:57:26 +0200

    some more changes to manifest and added busola config

--- apis/telemetry/v1beta1/metricpipeline_types.go
+++ apis/telemetry/v1beta1/metricpipeline_types.go
@@ -90,7 +90,7 @@ type MetricPipelineRuntimeInput struct {
 	Namespaces *MetricPipelineInputNamespaceSelector `json:"namespaces,omitempty"`
 	// Describes the Kubernetes resources for which runtime metrics are scraped.
 	// +optional
-	// +kubebuilder:default={pod: {enabled: true}, container: {enabled: true}, node: {enabled: false}, volume: {enabled: false}}
+	// +kubebuilder:default={pod: {enabled: true}, container: {enabled: true}, node: {enabled: false}, volume: {enabled: false}, daemonSet: {enabled: false}, deployment: {enabled: false}, statefulSet: {enabled: false}, job: {enabled: false}}
 	Resources *MetricPipelineRuntimeInputResources `json:"resources,omitempty"`
 }
 
