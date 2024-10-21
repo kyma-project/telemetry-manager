b7204e3cc101b85018699d3a61fb15601e6ec835
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2024-10-21 09:31:28 +0200

    add docs

--- test/testkit/matchers/prometheus/prommetric_utils.go
+++ test/testkit/matchers/prometheus/prommetric_utils.go
@@ -2,12 +2,20 @@ package prometheus
 
 import prommodel "github.com/prometheus/client_model/go"
 
+// FlatMetricFamily holds all necessary information about a prometheus MetricFamily.
+// Gomega doesn't handle deeply nested data structure very well and generates large, unreadable diffs when paired with
+// the deeply nested structure of pmetrics.
+//
+// Introducing a go struct with a flat data structure by extracting necessary information from different levels of
+// metricfamily makes accessing the information easier and improves readability of the output.
 type FlatMetricFamily struct {
 	Name         string
 	MetricValues float64
 	Labels       map[string]string
 }
 
+// flattenAllMetricFamily flattens an array of prometheus MetricFamily to a slice of FlatMetricFamily.
+// It converts the deeply nested MetricFamily to a flat struct, making it more readable in the test output.
 func flattenAllMetricFamily(mfs map[string]*prommodel.MetricFamily) []FlatMetricFamily {
 	var fmf []FlatMetricFamily
 	for _, mf := range mfs {
@@ -16,6 +24,8 @@ func flattenAllMetricFamily(mfs map[string]*prommodel.MetricFamily) []FlatMetric
 	return fmf
 }
 
+// flattenMetricFamily converts a single MetricFamily into a slice of FlatMetricFamily
+// It loops through all the metrics in a MetricFamily and appends it to the FlatMetricFamily slice.
 func flattenMetricFamily(mf *prommodel.MetricFamily) []FlatMetricFamily {
 	var fmf []FlatMetricFamily
 	for _, m := range mf.Metric {
