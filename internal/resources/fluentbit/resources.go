b1ff90f9922aa18d3e92123fbf95d0eae9d1e1a0
Rakesh Garimella <rakesh.garimella@sap.com>
2024-11-04 10:24:09 +0100

    fix: Bump memory limit for directory size exporter

--- internal/resources/fluentbit/resources.go
+++ internal/resources/fluentbit/resources.go
@@ -43,13 +43,14 @@ func MakeDaemonSet(name types.NamespacedName, checksum string, dsConfig DaemonSe
 		},
 	}
 
+	// Set resource requests/limits for directory-size exporter
 	resourcesExporter := corev1.ResourceRequirements{
 		Requests: map[corev1.ResourceName]resource.Quantity{
 			corev1.ResourceMemory: resource.MustParse("5Mi"),
 		},
 		Limits: map[corev1.ResourceName]resource.Quantity{
 			corev1.ResourceCPU:    resource.MustParse("10m"),
-			corev1.ResourceMemory: resource.MustParse("20Mi"),
+			corev1.ResourceMemory: resource.MustParse("50Mi"),
 		},
 	}
 
