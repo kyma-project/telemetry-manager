20f78665414718301c025a29fa090cd99d689e78
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2024-11-19 16:24:38 +0100

    refactor to use functional opt pattern

--- internal/resources/otelcollector/rbac.go
+++ internal/resources/otelcollector/rbac.go
@@ -15,208 +15,162 @@ type Rbac struct {
 	roleBinding        *rbacv1.RoleBinding
 }
 
-func MakeTraceGatewayRBAC(name types.NamespacedName) Rbac {
-	return Rbac{
-		clusterRole:        makeTraceGatewayClusterRole(name),
-		clusterRoleBinding: makeClusterRoleBinding(name),
-		role:               nil,
-		roleBinding:        nil,
+type RBACOption func(*Rbac, types.NamespacedName)
+
+func NewRBAC(name types.NamespacedName, options ...RBACOption) *Rbac {
+	rbac := &Rbac{}
+
+	for _, o := range options {
+		o(rbac, name)
 	}
+
+	return rbac
+}
+
+func WithClusterRole(options ...ClusterRoleOption) RBACOption {
+	return func(r *Rbac, name types.NamespacedName) {
+		clusterRole := &rbacv1.ClusterRole{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name.Name,
+				Namespace: name.Namespace,
+				Labels:    labels.MakeDefaultLabel(name.Name),
+			},
+			Rules: []rbacv1.PolicyRule{},
+		}
+		for _, o := range options {
+			o(clusterRole)
+		}
+
+		r.clusterRole = clusterRole
+	}
+}
+
+func WithClusterRoleBinding() RBACOption {
+	return func(r *Rbac, name types.NamespacedName) {
+		r.clusterRoleBinding = &rbacv1.ClusterRoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name.Name,
+				Namespace: name.Namespace,
+				Labels:    labels.MakeDefaultLabel(name.Name),
+			},
+			Subjects: []rbacv1.Subject{{Name: name.Name, Namespace: name.Namespace, Kind: rbacv1.ServiceAccountKind}},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "ClusterRole",
+				Name:     name.Name,
+			},
+		}
+	}
+}
+
+func WithRole(options ...RoleOption) RBACOption {
+	return func(r *Rbac, name types.NamespacedName) {
+		role := &rbacv1.Role{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name.Name,
+				Namespace: name.Namespace,
+				Labels:    labels.MakeDefaultLabel(name.Name),
+			},
+			Rules: []rbacv1.PolicyRule{},
+		}
+
+		for _, o := range options {
+			o(role)
+		}
+
+		r.role = role
+	}
+}
+
+func WithRoleBinding() RBACOption {
+	return func(r *Rbac, name types.NamespacedName) {
+		r.roleBinding = &rbacv1.RoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name.Name,
+				Namespace: name.Namespace,
+				Labels:    labels.MakeDefaultLabel(name.Name),
+			},
+			Subjects: []rbacv1.Subject{
+				{
+					Kind:      rbacv1.ServiceAccountKind,
+					Name:      name.Name,
+					Namespace: name.Namespace,
+				},
+			},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "Role",
+				Name:     name.Name,
+			},
+		}
+	}
+}
+
+func MakeTraceGatewayRBAC(name types.NamespacedName) Rbac {
+	return *NewRBAC(
+		name,
+		WithClusterRole(WithK8sAttributeRules()),
+		WithClusterRoleBinding(),
+	)
 }
 
 func MakeMetricAgentRBAC(name types.NamespacedName) Rbac {
-	return Rbac{
-		clusterRole:        makeMetricAgentClusterRole(name),
-		clusterRoleBinding: makeClusterRoleBinding(name),
-		role:               makeMetricRole(name),
-		roleBinding:        makeMetricRoleBinding(name),
-	}
+	return *NewRBAC(
+		name,
+		WithClusterRole(WithKubeletStatsRules(), WithPrometheusRules(), WithK8sClusterRules()),
+		WithClusterRoleBinding(),
+		WithRole(WithSingletonCreatorRules()),
+		WithRoleBinding(),
+	)
 }
 
 func MakeMetricGatewayRBAC(name types.NamespacedName) Rbac {
-	return Rbac{
-		clusterRole:        makeMetricGatewayClusterRole(name),
-		clusterRoleBinding: makeClusterRoleBinding(name),
-		role:               makeMetricRole(name),
-		roleBinding:        makeMetricRoleBinding(name),
+	return *NewRBAC(
+		name,
+		WithClusterRole(WithK8sAttributeRules(), WithKymaStatsRules()),
+		WithClusterRoleBinding(),
+		WithRole(WithSingletonCreatorRules()),
+		WithRoleBinding(),
+	)
+}
+
+type RoleOption func(*rbacv1.Role)
+
+// returns a role option since resources needed are only namespace scoped
+func WithSingletonCreatorRules() RoleOption {
+	return func(r *rbacv1.Role) {
+		r.Rules = append(r.Rules, singletonCreatorRules...)
 	}
 }
 
-func makeTraceGatewayClusterRole(name types.NamespacedName) *rbacv1.ClusterRole {
-	k8sAttributeRules := []rbacv1.PolicyRule{
-		{
-			APIGroups: []string{""},
-			Resources: []string{"namespaces", "pods"},
-			Verbs:     []string{"get", "list", "watch"},
-		},
-		{
-			APIGroups: []string{"apps"},
-			Resources: []string{"replicasets"},
-			Verbs:     []string{"get", "list", "watch"},
-		},
-	}
-	clusterRoleRules := append([]rbacv1.PolicyRule{}, k8sAttributeRules...)
+type ClusterRoleOption func(*rbacv1.ClusterRole)
 
-	return &rbacv1.ClusterRole{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Rules: clusterRoleRules,
+func WithK8sClusterRules() ClusterRoleOption {
+	return func(cr *rbacv1.ClusterRole) {
+		cr.Rules = append(cr.Rules, k8sClusterRules...)
 	}
 }
 
-func makeMetricAgentClusterRole(name types.NamespacedName) *rbacv1.ClusterRole {
-	kubeletStatsRules := []rbacv1.PolicyRule{{
-		APIGroups: []string{""},
-		Resources: []string{"nodes", "nodes/stats", "nodes/proxy"},
-		Verbs:     []string{"get", "list", "watch"},
-	}}
-
-	prometheusRules := []rbacv1.PolicyRule{
-		{
-			APIGroups: []string{""},
-			Resources: []string{"nodes", "nodes/metrics", "services", "endpoints", "pods"},
-			Verbs:     []string{"get", "list", "watch"},
-		},
-		{
-			NonResourceURLs: []string{"/metrics", "/metrics/cadvisor"},
-			Verbs:           []string{"get"},
-		},
-	}
-
-	k8sClusterRules := []rbacv1.PolicyRule{{
-		APIGroups: []string{""},
-		Resources: []string{"events", "namespaces", "namespaces/status", "nodes", "nodes/spec", "pods", "pods/status", "replicationcontrollers", "replicationcontrollers/status", "resourcequotas", "services"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"apps"},
-		Resources: []string{"daemonsets", "deployments", "replicasets", "statefulsets"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"extensions"},
-		Resources: []string{"daemonsets", "deployments", "replicasets"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"batch"},
-		Resources: []string{"jobs", "cronjobs"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"autoscaling"},
-		Resources: []string{"horizontalpodautoscalers"},
-		Verbs:     []string{"get", "list", "watch"},
-	}}
-
-	clusterRoleRules := append([]rbacv1.PolicyRule{}, kubeletStatsRules...)
-	clusterRoleRules = append(clusterRoleRules, prometheusRules...)
-	clusterRoleRules = append(clusterRoleRules, k8sClusterRules...)
-
-	return &rbacv1.ClusterRole{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Rules: clusterRoleRules,
+func WithKubeletStatsRules() ClusterRoleOption {
+	return func(cr *rbacv1.ClusterRole) {
+		cr.Rules = append(cr.Rules, kubeletStatsRules...)
 	}
 }
 
-func makeMetricGatewayClusterRole(name types.NamespacedName) *rbacv1.ClusterRole {
-	k8sAttributeRules := []rbacv1.PolicyRule{
-		{
-			APIGroups: []string{""},
-			Resources: []string{"namespaces", "pods"},
-			Verbs:     []string{"get", "list", "watch"},
-		},
-		{
-			APIGroups: []string{"apps"},
-			Resources: []string{"replicasets"},
-			Verbs:     []string{"get", "list", "watch"},
-		},
-	}
-
-	kymaStatsRules := []rbacv1.PolicyRule{{
-		APIGroups: []string{"operator.kyma-project.io"},
-		Resources: []string{"telemetries"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"telemetry.kyma-project.io"},
-		Resources: []string{"metricpipelines"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"telemetry.kyma-project.io"},
-		Resources: []string{"tracepipelines"},
-		Verbs:     []string{"get", "list", "watch"},
-	}, {
-		APIGroups: []string{"telemetry.kyma-project.io"},
-		Resources: []string{"logpipelines"},
-		Verbs:     []string{"get", "list", "watch"},
-	}}
-
-	clusterRoleRules := append([]rbacv1.PolicyRule{}, k8sAttributeRules...)
-	clusterRoleRules = append(clusterRoleRules, kymaStatsRules...)
-
-	return &rbacv1.ClusterRole{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Rules: clusterRoleRules,
+func WithPrometheusRules() ClusterRoleOption {
+	return func(cr *rbacv1.ClusterRole) {
+		cr.Rules = append(cr.Rules, prometheusRules...)
 	}
 }
 
-func makeClusterRoleBinding(name types.NamespacedName) *rbacv1.ClusterRoleBinding {
-	return &rbacv1.ClusterRoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Subjects: []rbacv1.Subject{{Name: name.Name, Namespace: name.Namespace, Kind: rbacv1.ServiceAccountKind}},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "ClusterRole",
-			Name:     name.Name,
-		},
+func WithK8sAttributeRules() ClusterRoleOption {
+	return func(cr *rbacv1.ClusterRole) {
+		cr.Rules = append(cr.Rules, k8sAttributeRules...)
 	}
 }
 
-func makeMetricRole(name types.NamespacedName) *rbacv1.Role {
-	return &rbacv1.Role{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Rules: []rbacv1.PolicyRule{
-			{
-				APIGroups: []string{"coordination.k8s.io"},
-				Resources: []string{"leases"},
-				Verbs:     []string{"get", "list", "watch", "create", "update", "patch", "delete"},
-			},
-		}}
-}
-
-func makeMetricRoleBinding(name types.NamespacedName) *rbacv1.RoleBinding {
-	return &rbacv1.RoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name.Name,
-			Namespace: name.Namespace,
-			Labels:    labels.MakeDefaultLabel(name.Name),
-		},
-		Subjects: []rbacv1.Subject{
-			{
-				Kind:      rbacv1.ServiceAccountKind,
-				Name:      name.Name,
-				Namespace: name.Namespace,
-			},
-		},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "Role",
-			Name:     name.Name,
-		},
+func WithKymaStatsRules() ClusterRoleOption {
+	return func(cr *rbacv1.ClusterRole) {
+		cr.Rules = append(cr.Rules, kymaStatsRules...)
 	}
 }
