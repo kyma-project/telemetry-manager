9090bb0db1220272ab6d8bfd39ba3eadae7ddc0a
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2024-11-20 09:07:25 +0100

    move rules into func opts

--- internal/resources/otelcollector/rbac.go
+++ internal/resources/otelcollector/rbac.go
@@ -140,6 +140,12 @@ type RoleOption func(*rbacv1.Role)
 // WithSingletonCreatorRules returns a role option since resources needed are only namespace scoped
 func WithSingletonCreatorRules() RoleOption {
 	return func(r *rbacv1.Role) {
+		// policy rules needed for the singletonreceivercreator component
+		singletonCreatorRules := []rbacv1.PolicyRule{{
+			APIGroups: []string{"coordination.k8s.io"},
+			Resources: []string{"leases"},
+			Verbs:     []string{"get", "list", "watch", "create", "update", "patch", "delete"},
+		}}
 		r.Rules = append(r.Rules, singletonCreatorRules...)
 	}
 }
@@ -148,29 +154,94 @@ type ClusterRoleOption func(*rbacv1.ClusterRole)
 
 func WithK8sClusterRules() ClusterRoleOption {
 	return func(cr *rbacv1.ClusterRole) {
+		// policy rules needed for the k8sclusterreceiver component
+		k8sClusterRules := []rbacv1.PolicyRule{{
+			APIGroups: []string{""},
+			Resources: []string{"events", "namespaces", "namespaces/status", "nodes", "nodes/spec", "pods", "pods/status", "replicationcontrollers", "replicationcontrollers/status", "resourcequotas", "services"},
+			Verbs:     []string{"get", "list", "watch"},
+		}, {
+			APIGroups: []string{"apps"},
+			Resources: []string{"daemonsets", "deployments", "replicasets", "statefulsets"},
+			Verbs:     []string{"get", "list", "watch"},
+		}, {
+			APIGroups: []string{"extensions"},
+			Resources: []string{"daemonsets", "deployments", "replicasets"},
+			Verbs:     []string{"get", "list", "watch"},
+		}, {
+			APIGroups: []string{"batch"},
+			Resources: []string{"jobs", "cronjobs"},
+			Verbs:     []string{"get", "list", "watch"},
+		}, {
+			APIGroups: []string{"autoscaling"},
+			Resources: []string{"horizontalpodautoscalers"},
+			Verbs:     []string{"get", "list", "watch"},
+		}}
 		cr.Rules = append(cr.Rules, k8sClusterRules...)
 	}
 }
 
 func WithKubeletStatsRules() ClusterRoleOption {
+	// policy rules needed for the kubeletstatsreceiver component
+	kubeletStatsRules := []rbacv1.PolicyRule{{
+		APIGroups: []string{""},
+		Resources: []string{"nodes", "nodes/stats", "nodes/proxy"},
+		Verbs:     []string{"get", "list", "watch"},
+	}}
 	return func(cr *rbacv1.ClusterRole) {
 		cr.Rules = append(cr.Rules, kubeletStatsRules...)
 	}
 }
 
 func WithPrometheusRules() ClusterRoleOption {
+	// policy rules needed for the prometheusreceiver component
+	prometheusRules := []rbacv1.PolicyRule{{
+		APIGroups: []string{""},
+		Resources: []string{"nodes", "nodes/metrics", "services", "endpoints", "pods"},
+		Verbs:     []string{"get", "list", "watch"},
+	}, {
+		NonResourceURLs: []string{"/metrics", "/metrics/cadvisor"},
+		Verbs:           []string{"get"},
+	}}
 	return func(cr *rbacv1.ClusterRole) {
 		cr.Rules = append(cr.Rules, prometheusRules...)
 	}
 }
 
 func WithK8sAttributeRules() ClusterRoleOption {
+	// policy rules needed for the k8sattributeprocessor component
+	k8sAttributeRules := []rbacv1.PolicyRule{{
+		APIGroups: []string{""},
+		Resources: []string{"namespaces", "pods"},
+		Verbs:     []string{"get", "list", "watch"},
+	}, {
+		APIGroups: []string{"apps"},
+		Resources: []string{"replicasets"},
+		Verbs:     []string{"get", "list", "watch"},
+	}}
 	return func(cr *rbacv1.ClusterRole) {
 		cr.Rules = append(cr.Rules, k8sAttributeRules...)
 	}
 }
 
 func WithKymaStatsRules() ClusterRoleOption {
+	// policy rules needed for the kymastatsreceiver component
+	kymaStatsRules := []rbacv1.PolicyRule{{
+		APIGroups: []string{"operator.kyma-project.io"},
+		Resources: []string{"telemetries"},
+		Verbs:     []string{"get", "list", "watch"},
+	}, {
+		APIGroups: []string{"telemetry.kyma-project.io"},
+		Resources: []string{"metricpipelines"},
+		Verbs:     []string{"get", "list", "watch"},
+	}, {
+		APIGroups: []string{"telemetry.kyma-project.io"},
+		Resources: []string{"tracepipelines"},
+		Verbs:     []string{"get", "list", "watch"},
+	}, {
+		APIGroups: []string{"telemetry.kyma-project.io"},
+		Resources: []string{"logpipelines"},
+		Verbs:     []string{"get", "list", "watch"},
+	}}
 	return func(cr *rbacv1.ClusterRole) {
 		cr.Rules = append(cr.Rules, kymaStatsRules...)
 	}
