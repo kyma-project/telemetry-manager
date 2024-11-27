a7892d4fe6c17c0c50778368ed650b0a484e84ee
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2024-11-27 14:17:53 +0100

    chore: Move reconciler config to separate file

---
+++ internal/reconciler/logpipeline/fluentbit/config.go
@@ -0,0 +1,87 @@
+package fluentbit
+
+import (
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+
+	"github.com/kyma-project/telemetry-manager/internal/fluentbit/config/builder"
+	"github.com/kyma-project/telemetry-manager/internal/overrides"
+	"github.com/kyma-project/telemetry-manager/internal/resources/fluentbit"
+)
+
+type Config struct {
+	DaemonSet           types.NamespacedName
+	SectionsConfigMap   types.NamespacedName
+	FilesConfigMap      types.NamespacedName
+	LuaConfigMap        types.NamespacedName
+	ParsersConfigMap    types.NamespacedName
+	EnvConfigSecret     types.NamespacedName
+	TLSFileConfigSecret types.NamespacedName
+	PipelineDefaults    builder.PipelineDefaults
+	Overrides           overrides.Config
+	DaemonSetConfig     fluentbit.DaemonSetConfig
+	RestConfig          rest.Config
+}
+
+type ConfigOption func(*Config)
+
+func WithFluentBitImage(image string) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.FluentBitImage = image
+	}
+}
+
+func WithExporterImage(image string) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.ExporterImage = image
+	}
+}
+
+func WithPriorityClassName(name string) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.PriorityClassName = name
+	}
+}
+
+func WithCPULimit(limit resource.Quantity) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.CPULimit = limit
+	}
+}
+
+func WithMemoryLimit(limit resource.Quantity) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.MemoryLimit = limit
+	}
+}
+
+func WithCPURequest(request resource.Quantity) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.CPURequest = request
+	}
+}
+
+func WithMemoryRequest(request resource.Quantity) ConfigOption {
+	return func(c *Config) {
+		c.DaemonSetConfig.MemoryRequest = request
+	}
+}
+
+func NewConfig(baseName string, namespace string, options ...ConfigOption) *Config {
+	config := &Config{
+		SectionsConfigMap:   types.NamespacedName{Name: baseName + "-sections", Namespace: namespace},
+		FilesConfigMap:      types.NamespacedName{Name: baseName + "-files", Namespace: namespace},
+		LuaConfigMap:        types.NamespacedName{Name: baseName + "-luascripts", Namespace: namespace},
+		ParsersConfigMap:    types.NamespacedName{Name: baseName + "-parsers", Namespace: namespace},
+		EnvConfigSecret:     types.NamespacedName{Name: baseName + "-env", Namespace: namespace},
+		TLSFileConfigSecret: types.NamespacedName{Name: baseName + "-output-tls-config", Namespace: namespace},
+		DaemonSet:           types.NamespacedName{Name: baseName, Namespace: namespace},
+	}
+
+	for _, opt := range options {
+		opt(config)
+	}
+
+	return config
+}
--- internal/reconciler/logpipeline/fluentbit/reconciler.go
+++ internal/reconciler/logpipeline/fluentbit/reconciler.go
@@ -9,10 +9,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
@@ -21,7 +19,6 @@ import (
 	"github.com/kyma-project/telemetry-manager/internal/errortypes"
 	"github.com/kyma-project/telemetry-manager/internal/fluentbit/config/builder"
 	"github.com/kyma-project/telemetry-manager/internal/fluentbit/ports"
-	"github.com/kyma-project/telemetry-manager/internal/overrides"
 	"github.com/kyma-project/telemetry-manager/internal/reconciler/commonstatus"
 	"github.com/kyma-project/telemetry-manager/internal/reconciler/logpipeline"
 	commonresources "github.com/kyma-project/telemetry-manager/internal/resources/common"
@@ -37,82 +34,6 @@ const (
 	defaultStorageType       = "filesystem"
 	defaultFsBufferLimit     = "1G"
 )
-
-type Config struct {
-	DaemonSet           types.NamespacedName
-	SectionsConfigMap   types.NamespacedName
-	FilesConfigMap      types.NamespacedName
-	LuaConfigMap        types.NamespacedName
-	ParsersConfigMap    types.NamespacedName
-	EnvConfigSecret     types.NamespacedName
-	TLSFileConfigSecret types.NamespacedName
-	PipelineDefaults    builder.PipelineDefaults
-	Overrides           overrides.Config
-	DaemonSetConfig     fluentbit.DaemonSetConfig
-	RestConfig          rest.Config
-}
-
-type ConfigOption func(*Config)
-
-func WithFluentBitImage(image string) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.FluentBitImage = image
-	}
-}
-
-func WithExporterImage(image string) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.ExporterImage = image
-	}
-}
-
-func WithPriorityClassName(name string) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.PriorityClassName = name
-	}
-}
-
-func WithCPULimit(limit resource.Quantity) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.CPULimit = limit
-	}
-}
-
-func WithMemoryLimit(limit resource.Quantity) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.MemoryLimit = limit
-	}
-}
-
-func WithCPURequest(request resource.Quantity) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.CPURequest = request
-	}
-}
-
-func WithMemoryRequest(request resource.Quantity) ConfigOption {
-	return func(c *Config) {
-		c.DaemonSetConfig.MemoryRequest = request
-	}
-}
-
-func NewConfig(baseName string, namespace string, options ...ConfigOption) *Config {
-	config := &Config{
-		SectionsConfigMap:   types.NamespacedName{Name: baseName + "-sections", Namespace: namespace},
-		FilesConfigMap:      types.NamespacedName{Name: baseName + "-files", Namespace: namespace},
-		LuaConfigMap:        types.NamespacedName{Name: baseName + "-luascripts", Namespace: namespace},
-		ParsersConfigMap:    types.NamespacedName{Name: baseName + "-parsers", Namespace: namespace},
-		EnvConfigSecret:     types.NamespacedName{Name: baseName + "-env", Namespace: namespace},
-		TLSFileConfigSecret: types.NamespacedName{Name: baseName + "-output-tls-config", Namespace: namespace},
-		DaemonSet:           types.NamespacedName{Name: baseName, Namespace: namespace},
-	}
-
-	for _, opt := range options {
-		opt(config)
-	}
-
-	return config
-}
 
 type IstioStatusChecker interface {
 	IsIstioActive(ctx context.Context) bool
