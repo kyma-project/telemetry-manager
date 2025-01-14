c73078fbe35384dfd2f49534a58fe21d89c1cd51
Hisar Balik <hisar.balik@sap.com>
2025-01-14 10:11:00 +0100

    move metricpipeline common funtions to the metricpipeline utils

--- internal/utils/metricpipeline/metricpipeline.go
+++ internal/utils/metricpipeline/metricpipeline.go
@@ -13,3 +13,87 @@ func IsPrometheusInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool 
 func IsRuntimeInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
 	return input.Runtime != nil && input.Runtime.Enabled != nil && *input.Runtime.Enabled
 }
+
+func IsOTLPInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	return input.OTLP == nil || !input.OTLP.Disabled
+}
+
+func IsPrometheusDiagnosticInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	return input.Prometheus.DiagnosticMetrics != nil && input.Prometheus.DiagnosticMetrics.Enabled
+}
+
+func IsIstioDiagnosticInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	return input.Istio.DiagnosticMetrics != nil && input.Istio.DiagnosticMetrics.Enabled
+}
+
+func IsRuntimePodInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime pod metrics should be enabled by default if any of the fields (Resources, Pod or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Pod == nil || input.Runtime.Resources.Pod.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Pod.Enabled
+}
+
+func IsRuntimeContainerInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime container metrics should be enabled by default if any of the fields (Resources, Container or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Container == nil || input.Runtime.Resources.Container.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Container.Enabled
+}
+
+func IsRuntimeNodeInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime node metrics should be enabled by default if any of the fields (Resources, Node or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Node == nil || input.Runtime.Resources.Node.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Node.Enabled
+}
+
+func IsRuntimeVolumeInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime volume metrics should be enabled by default if any of the fields (Resources, Volume or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Volume == nil || input.Runtime.Resources.Volume.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Volume.Enabled
+}
+
+func IsRuntimeStatefulSetInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime Statefulset metrics should be enabled by default if any of the fields (Resources, Statefulset or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.StatefulSet == nil || input.Runtime.Resources.StatefulSet.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.StatefulSet.Enabled
+}
+
+func IsRuntimeDeploymentInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime Deployment metrics should be enabled by default if any of the fields (Resources, Deployment or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Deployment == nil || input.Runtime.Resources.Deployment.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Deployment.Enabled
+}
+
+func IsRuntimeDaemonSetInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime DaemonSet metrics should be enabled by default if any of the fields (Resources, DaemonSet or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.DaemonSet == nil || input.Runtime.Resources.DaemonSet.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.DaemonSet.Enabled
+}
+
+func IsRuntimeJobInputEnabled(input telemetryv1alpha1.MetricPipelineInput) bool {
+	// Runtime Job metrics should be enabled by default if any of the fields (Resources, Job or Enabled) is nil
+	if input.Runtime.Resources == nil || input.Runtime.Resources.Job == nil || input.Runtime.Resources.Job.Enabled == nil {
+		return true
+	}
+
+	return *input.Runtime.Resources.Job.Enabled
+}
