28478dc728ed22f860de926f7f0b7e990270e636
Hisar Balik <hisar.balik@sap.com>
2025-01-28 16:18:29 +0100

    remove unused code

--- test/integration/istio/metric_cloud_provider_test.go
+++ test/integration/istio/metric_cloud_provider_test.go
@@ -135,20 +135,6 @@ func backendContainsMetricsDeliveredForResource(proxyClient *apiserverproxy.Clie
 	}, 2*periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed(), "Failed to find metrics using ContainElements %v", resourceMetrics)
 }
 
-// Check with `ConsistsOf` for metrics present in the backend
-func backendConsistsOfMetricsDeliveredForResource(proxyClient *apiserverproxy.Client, backendExportURL string, resourceMetrics []string) {
-	Eventually(func(g Gomega) {
-		resp, err := proxyClient.Get(backendExportURL)
-		g.Expect(err).NotTo(HaveOccurred())
-		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
-		defer resp.Body.Close()
-
-		g.Expect(resp).To(HaveHTTPBody(
-			HaveFlatMetrics(HaveUniqueNamesForRuntimeScope(ConsistOf(resourceMetrics))),
-		))
-	}, 2*periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed(), "Failed to find metrics using consistsOf %v", resourceMetrics)
-}
-
 func backendContainsDesiredCloudResourceAttributes(proxyClient *apiserverproxy.Client, backendExportURL, attribute string) {
 	Eventually(func(g Gomega) {
 		resp, err := proxyClient.Get(backendExportURL)
