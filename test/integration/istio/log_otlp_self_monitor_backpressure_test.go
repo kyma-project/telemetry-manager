15bdcf543259338e66e0ff65010c3e976cc0c561
Rakesh Garimella <rakesh.garimella@sap.com>
2025-02-11 13:30:10 +0100

    add tests for self monitor

---
+++ test/integration/istio/log_otlp_self_monitor_backpressure_test.go
@@ -0,0 +1,93 @@
+//go:build istio
+
+package istio
+
+import (
+	operatorv1alpha1 "github.com/kyma-project/telemetry-manager/apis/operator/v1alpha1"
+	"github.com/kyma-project/telemetry-manager/internal/conditions"
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/loggen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe(suite.ID(), Label(suite.LabelSelfMonitoringLogsBackpressure), Ordered, func() {
+	var (
+		mockNs       = "istio-permissive-mtls"
+		pipelineName = suite.ID()
+	)
+
+	makeResources := func() []client.Object {
+		var objs []client.Object
+
+		backend := backend.New(mockNs, backend.SignalTypeLogsOtel, backend.WithAbortFaultInjection(75))
+		logProducer := loggen.New(mockNs).WithReplicas(3).WithLoad(loggen.LoadHigh)
+		objs = append(objs, backend.K8sObjects()...)
+		objs = append(objs, logProducer.K8sObject())
+
+		logPipeline := testutils.NewLogPipelineBuilder().
+			WithName(pipelineName).
+			WithOTLPOutput(testutils.OTLPEndpoint(backend.Endpoint())).
+			WithApplicationInput(true).
+			Build()
+		objs = append(objs, &logPipeline)
+
+		return objs
+	}
+
+	Context("When a logpipeline with otel exists", Ordered, func() {
+		BeforeAll(func() {
+			k8sObjects := makeResources()
+			DeferCleanup(func() {
+				Expect(kitk8s.DeleteObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+			})
+			Expect(kitk8s.CreateObjects(ctx, k8sClient, k8sObjects...)).Should(Succeed())
+		})
+
+		It("Should have a running logpipeline", func() {
+			assert.LogPipelineHealthy(ctx, k8sClient, pipelineName)
+		})
+
+		It("Should have a running log agent daemonset", func() {
+			assert.DaemonSetReady(ctx, k8sClient, kitkyma.LogAgentName)
+		})
+
+		It("Should have a running self-monitor", func() {
+			assert.DeploymentReady(ctx, k8sClient, kitkyma.SelfMonitorName)
+		})
+
+		It("Should have a log backend running", func() {
+			assert.DeploymentReady(ctx, k8sClient, types.NamespacedName{Namespace: mockNs, Name: backend.DefaultName})
+		})
+
+		It("Should have a log producer running", func() {
+			assert.DeploymentReady(ctx, k8sClient, types.NamespacedName{Namespace: mockNs, Name: loggen.DefaultName})
+		})
+
+		It("Should wait for the log flow to gradually become unhealthy", func() {
+			assert.LogPipelineConditionReasonsTransition(ctx, k8sClient, pipelineName, conditions.TypeFlowHealthy, []assert.ReasonStatus{
+				{Reason: conditions.ReasonSelfMonFlowHealthy, Status: metav1.ConditionTrue},
+				{Reason: conditions.ReasonSelfMonBufferFillingUp, Status: metav1.ConditionFalse},
+				{Reason: conditions.ReasonSelfMonSomeDataDropped, Status: metav1.ConditionFalse},
+			})
+
+			assert.TelemetryHasState(ctx, k8sClient, operatorv1alpha1.StateWarning)
+			assert.TelemetryHasCondition(ctx, k8sClient, metav1.Condition{
+				Type:   conditions.TypeLogComponentsHealthy,
+				Status: metav1.ConditionFalse,
+				Reason: conditions.ReasonSelfMonSomeDataDropped,
+			})
+		})
+	})
+})
