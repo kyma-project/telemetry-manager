05a1bbdfaf9fab0dceeb1bc351dd1f229f03160f
Teodor-Adrian Mihaescu <103431261+TeodorSAP@users.noreply.github.com>
2025-03-31 13:02:33 +0300

    chore: e2e tests refactoring (#1874)
    Co-authored-by: Korbinian Stoemmer <korbinian.stoemmer@sap.com>

---
+++ test/e2e/misc/manager_test.go
@@ -0,0 +1,209 @@
+//go:build e2e
+
+package misc
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	schedulingv1 "k8s.io/api/scheduling/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+)
+
+var _ = Describe(suite.ID(), func() {
+	Context("After deploying manifest", func() {
+		It("Should have kyma-system namespace", Label(suite.LabelTelemetry), func() {
+			var namespace corev1.Namespace
+			key := types.NamespacedName{
+				Name: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &namespace)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have a running manager deployment", Label(suite.LabelTelemetry), func() {
+			var deployment appsv1.Deployment
+			key := types.NamespacedName{
+				Name:      "telemetry-manager",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &deployment)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() bool {
+				listOptions := client.ListOptions{
+					LabelSelector: labels.SelectorFromSet(deployment.Spec.Selector.MatchLabels),
+					Namespace:     kitkyma.SystemNamespaceName,
+				}
+				var pods corev1.PodList
+				err := suite.K8sClient.List(suite.Ctx, &pods, &listOptions)
+				Expect(err).NotTo(HaveOccurred())
+				for _, pod := range pods.Items {
+					for _, containerStatus := range pod.Status.ContainerStatuses {
+						if containerStatus.State.Running == nil {
+							return false
+						}
+					}
+				}
+
+				return true
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue())
+		})
+
+		It("Should have a webhook service", Label(suite.LabelTelemetry), func() {
+			var service corev1.Service
+			key := types.NamespacedName{
+				Name:      "telemetry-manager-webhook",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &service)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() []corev1.EndpointAddress {
+				var endpoints corev1.Endpoints
+				err := suite.K8sClient.Get(suite.Ctx, key, &endpoints)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(endpoints.Subsets).NotTo(BeEmpty())
+				return endpoints.Subsets[0].Addresses
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).ShouldNot(BeEmpty())
+		})
+
+		It("Should have a metrics service", Label(suite.LabelTelemetry), func() {
+			var service corev1.Service
+			err := suite.K8sClient.Get(suite.Ctx, kitkyma.TelemetryManagerMetricsServiceName, &service)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(service.Annotations).Should(HaveKeyWithValue("prometheus.io/scrape", "true"))
+			Expect(service.Annotations).Should(HaveKeyWithValue("prometheus.io/port", "8080"))
+
+			Eventually(func() []corev1.EndpointAddress {
+				var endpoints corev1.Endpoints
+				err := suite.K8sClient.Get(suite.Ctx, kitkyma.TelemetryManagerMetricsServiceName, &endpoints)
+				Expect(err).NotTo(HaveOccurred())
+				return endpoints.Subsets[0].Addresses
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).ShouldNot(BeEmpty())
+		})
+
+		It("Should have LogPipelines CRD", Label(suite.LabelLogsFluentBit), func() {
+			var crd apiextensionsv1.CustomResourceDefinition
+			key := types.NamespacedName{
+				Name: "logpipelines.telemetry.kyma-project.io",
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &crd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(crd.Spec.Scope).To(Equal(apiextensionsv1.ClusterScoped))
+		})
+
+		It("Should have LogParsers CRD", Label(suite.LabelLogsFluentBit), func() {
+			var crd apiextensionsv1.CustomResourceDefinition
+			key := types.NamespacedName{
+				Name: "logparsers.telemetry.kyma-project.io",
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &crd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(crd.Spec.Scope).To(Equal(apiextensionsv1.ClusterScoped))
+		})
+
+		It("Should have TracePipelines CRD", Label(suite.LabelTraces), func() {
+			var crd apiextensionsv1.CustomResourceDefinition
+			key := types.NamespacedName{
+				Name: "tracepipelines.telemetry.kyma-project.io",
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &crd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(crd.Spec.Scope).To(Equal(apiextensionsv1.ClusterScoped))
+		})
+
+		It("Should have MetricPipelines CRD", Label(suite.LabelMetrics), Label(suite.LabelSetA), func() {
+			var crd apiextensionsv1.CustomResourceDefinition
+			key := types.NamespacedName{
+				Name: "metricpipelines.telemetry.kyma-project.io",
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &crd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(crd.Spec.Scope).To(Equal(apiextensionsv1.ClusterScoped))
+		})
+
+		It("Should have Telemetry CRD", Label(suite.LabelTelemetry), func() {
+			var crd apiextensionsv1.CustomResourceDefinition
+			key := types.NamespacedName{
+				Name: "telemetries.operator.kyma-project.io",
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &crd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(crd.Spec.Scope).To(Equal(apiextensionsv1.NamespaceScoped))
+		})
+
+		It("Should have a Busola extension for MetricPipelines CRD", Label(suite.LabelMetrics), Label(suite.LabelSetA), func() {
+			var cm corev1.ConfigMap
+			key := types.NamespacedName{
+				Name:      "telemetry-metricpipelines",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &cm)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have a Busola extension for LogPipelines CRD", Label(suite.LabelLogsFluentBit), func() {
+			var cm corev1.ConfigMap
+			key := types.NamespacedName{
+				Name:      "telemetry-logpipelines",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &cm)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have a Busola extension for TracePipelines CRD", Label(suite.LabelTraces), func() {
+			var cm corev1.ConfigMap
+			key := types.NamespacedName{
+				Name:      "telemetry-tracepipelines",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &cm)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have a Busola extension for Telemetry CRD", Label(suite.LabelTelemetry), func() {
+			var cm corev1.ConfigMap
+			key := types.NamespacedName{
+				Name:      "telemetry-module",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &cm)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have a NetworkPolicy", Label(suite.LabelTelemetry), func() {
+			var networkPolicy networkingv1.NetworkPolicy
+			key := types.NamespacedName{
+				Name:      "telemetry-manager",
+				Namespace: kitkyma.SystemNamespaceName,
+			}
+			err := suite.K8sClient.Get(suite.Ctx, key, &networkPolicy)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should have priority class resource created", Label(suite.LabelTelemetry), func() {
+			priorityClassNames := []string{"telemetry-priority-class", "telemetry-priority-class-high"}
+			var priorityClass schedulingv1.PriorityClass
+			for _, prioClass := range priorityClassNames {
+				key := types.NamespacedName{
+					Name:      prioClass,
+					Namespace: kitkyma.SystemNamespaceName,
+				}
+				err := suite.K8sClient.Get(suite.Ctx, key, &priorityClass)
+				Expect(err).NotTo(HaveOccurred())
+			}
+		})
+	})
+})
---
+++ test/e2e/selfmonitor/metrics_healthy_test.go
@@ -0,0 +1,116 @@
+//go:build e2e
+
+package selfmonitor
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	operatorv1alpha1 "github.com/kyma-project/telemetry-manager/apis/operator/v1alpha1"
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+	"github.com/kyma-project/telemetry-manager/internal/conditions"
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+)
+
+// Please remove the test when the compatibility mode annotation feature removed, planed for telemetry version 1.41.0
+var _ = Describe(suite.ID(), Label(suite.LabelSelfMonitoringMetricsHealthy), Ordered, func() {
+	var (
+		mockNs           = suite.ID()
+		pipelineName     = suite.ID()
+		backendExportURL string
+	)
+
+	makeResources := func() []client.Object {
+		var objs []client.Object
+
+		objs = append(objs, kitk8s.NewNamespace(mockNs).K8sObject())
+
+		backend := backend.New(mockNs, backend.SignalTypeMetrics)
+		objs = append(objs, backend.K8sObjects()...)
+		backendExportURL = backend.ExportURL(suite.ProxyClient)
+
+		pipeline := testutils.NewMetricPipelineBuilder().
+			WithName(pipelineName).
+			WithOTLPOutput(testutils.OTLPEndpoint(backend.Endpoint())).
+			Build()
+		objs = append(objs,
+			telemetrygen.NewPod(kitkyma.DefaultNamespaceName, telemetrygen.SignalTypeMetrics).K8sObject(),
+			&pipeline,
+		)
+
+		return objs
+	}
+
+	Context("When a metric pipeline exists", Ordered, func() {
+		BeforeAll(func() {
+			k8sObjects := makeResources()
+
+			DeferCleanup(func() {
+				Expect(kitk8s.DeleteObjects(suite.Ctx, suite.K8sClient, k8sObjects...)).Should(Succeed())
+			})
+			Expect(kitk8s.CreateObjects(suite.Ctx, suite.K8sClient, k8sObjects...)).Should(Succeed())
+		})
+
+		It("Should have global internal metrics compatibility annotation config", func() {
+			Eventually(func(g Gomega) string {
+				var telemetry operatorv1alpha1.Telemetry
+				err := suite.K8sClient.Get(suite.Ctx, kitkyma.TelemetryName, &telemetry)
+				g.Expect(err).NotTo(HaveOccurred())
+
+				telemetry.Annotations = map[string]string{
+					"telemetry.kyma-project.io/internal-metrics-compatibility-mode": "true",
+				}
+
+				err = suite.K8sClient.Update(suite.Ctx, &telemetry)
+				g.Expect(err).NotTo(HaveOccurred())
+				return telemetry.Annotations["telemetry.kyma-project.io/internal-metrics-compatibility-mode"]
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(Equal("true"))
+		})
+
+		It("Should have a running self-monitor", func() {
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, kitkyma.SelfMonitorName)
+		})
+
+		It("Should have service deployed", func() {
+			var service corev1.Service
+			Expect(suite.K8sClient.Get(suite.Ctx, kitkyma.SelfMonitorName, &service)).To(Succeed())
+		})
+
+		It("Should have a metrics backend running", func() {
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, types.NamespacedName{Name: backend.DefaultName, Namespace: mockNs})
+			assert.ServiceReady(suite.Ctx, suite.K8sClient, types.NamespacedName{Name: backend.DefaultName, Namespace: mockNs})
+		})
+
+		It("Should have a running pipeline", func() {
+			assert.MetricPipelineHealthy(suite.Ctx, suite.K8sClient, pipelineName)
+		})
+
+		It("Ensures the metric gateway deployment is ready", func() {
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, kitkyma.MetricGatewayName)
+		})
+
+		It("Should deliver telemetrygen metrics", func() {
+			assert.MetricsFromNamespaceDelivered(suite.ProxyClient, backendExportURL, kitkyma.DefaultNamespaceName, telemetrygen.MetricNames)
+		})
+
+		It("Should have TypeFlowHealthy condition set to True", func() {
+			Eventually(func(g Gomega) {
+				var pipeline telemetryv1alpha1.MetricPipeline
+				key := types.NamespacedName{Name: pipelineName}
+				g.Expect(suite.K8sClient.Get(suite.Ctx, key, &pipeline)).To(Succeed())
+				g.Expect(meta.IsStatusConditionTrue(pipeline.Status.Conditions, conditions.TypeFlowHealthy)).To(BeTrueBecause("Flow not healthy"))
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(Succeed())
+		})
+	})
+})
