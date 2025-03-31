cf0d089272f874c02af58b04c8d9e651fef28a1e
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-03-31 15:23:03 +0300

    Rename e2e test suites and add selfmonitor suite

--- test/e2e/misc/suite_test.go
+++ test/e2e/misc/suite_test.go
@@ -1,6 +1,6 @@
 //go:build e2e
 
-package otel
+package misc
 
 import (
 	"testing"
@@ -12,12 +12,12 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 )
 
-func TestMisc(t *testing.T) {
+func TestSuite(t *testing.T) {
 	format.MaxDepth = suite.GomegaMaxDepth
 	format.MaxLength = suite.GomegaMaxLenght
 
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "E2E Suite - Logs")
+	RunSpecs(t, "E2E Suite - Misc")
 }
 
 var _ = BeforeSuite(suite.BeforeSuiteFunc)
