dae8cb061ea174feb6fb8ebbd2d3d072bfa50424
Stanislav Khalash <stanislav.khalash@sap.com>
2025-04-08 12:26:52 +0200

    chore: standardize pod spec creation across components using functional options (#2027)

--- internal/resources/otelcollector/agent.go
+++ internal/resources/otelcollector/agent.go
@@ -52,7 +52,8 @@ type AgentApplierDeleter struct {
 	namespace     string
 	rbac          rbac
 
-	podSpecOptions []podSpecOption
+	podOpts       []commonresources.PodSpecOption
+	containerOpts []commonresources.ContainerOption
 }
 
 type AgentApplyOptions struct {
@@ -65,12 +66,14 @@ func NewLogAgentApplierDeleter(image, namespace, priorityClassName string) *Agen
 	extraLabels := map[string]string{
 		commonresources.LabelKeyIstioInject: "true", // inject Istio sidecar for SDS certificates and agent-to-gateway communication
 	}
+
 	volumes := []corev1.Volume{
 		makeIstioCertVolume(),
 		makePodLogsVolume(),
 		// HostPath Should be unique for each application using it
 		makeFileLogCheckpointVolume(),
 	}
+
 	volumeMounts := []corev1.VolumeMount{
 		makeIstioCertVolumeMount(),
 		makePodLogsVolumeMount(),
@@ -83,16 +86,18 @@ func NewLogAgentApplierDeleter(image, namespace, priorityClassName string) *Agen
 		image:         image,
 		namespace:     namespace,
 		rbac:          makeLogAgentRBAC(namespace),
-
-		podSpecOptions: []podSpecOption{
+		podOpts: []commonresources.PodSpecOption{
 			commonresources.WithPriorityClass(priorityClassName),
+			commonresources.WithVolumes(volumes),
+		},
+		containerOpts: []commonresources.ContainerOption{
 			commonresources.WithResources(makeAgentResourceRequirements(logAgentMemoryLimit, logAgentMemoryRequest, logAgentCPURequest)),
-			withEnvVarFromSource(config.EnvVarCurrentPodIP, fieldPathPodIP),
-			commonresources.WithGoMemLimitEnvVar(metricAgentMemoryLimit),
-			withVolumes(volumes),
-			withVolumeMounts(volumeMounts),
-			withSecurityContext(makeLogAgentSecurityContext()),
-			withPodSecurityContext(makeLogAgentPodSecurityContext()),
+			commonresources.WithEnvVarFromField(config.EnvVarCurrentPodIP, fieldPathPodIP),
+			commonresources.WithGoMemLimitEnvVar(logAgentMemoryLimit),
+			commonresources.WithVolumeMounts(volumeMounts),
+			commonresources.WithRunAsUser(userRoot),
+			commonresources.WithRunAsRoot(),
+			commonresources.WithCapabilities("FOWNER"),
 		},
 	}
 }
@@ -109,17 +114,16 @@ func NewMetricAgentApplierDeleter(image, namespace, priorityClassName string) *A
 		image:         image,
 		namespace:     namespace,
 		rbac:          makeMetricAgentRBAC(namespace),
-
-		podSpecOptions: []podSpecOption{
+		podOpts: []commonresources.PodSpecOption{
 			commonresources.WithPriorityClass(priorityClassName),
+			commonresources.WithVolumes([]corev1.Volume{makeIstioCertVolume()}),
+		},
+		containerOpts: []commonresources.ContainerOption{
 			commonresources.WithResources(makeAgentResourceRequirements(metricAgentMemoryLimit, metricAgentMemoryRequest, metricAgentCPURequest)),
-			withEnvVarFromSource(config.EnvVarCurrentPodIP, fieldPathPodIP),
-			withEnvVarFromSource(config.EnvVarCurrentNodeName, fieldPathNodeName),
+			commonresources.WithEnvVarFromField(config.EnvVarCurrentPodIP, fieldPathPodIP),
+			commonresources.WithEnvVarFromField(config.EnvVarCurrentNodeName, fieldPathNodeName),
 			commonresources.WithGoMemLimitEnvVar(metricAgentMemoryLimit),
-			withVolumes([]corev1.Volume{makeIstioCertVolume()}),
-			withVolumeMounts([]corev1.VolumeMount{makeIstioCertVolumeMount()}),
-			withSecurityContext(makeMetricAgentSecurityContext()),
-			withPodSecurityContext(makeMetricAgentPodSecurityContext()),
+			commonresources.WithVolumeMounts([]corev1.VolumeMount{makeIstioCertVolumeMount()}),
 		},
 	}
 }
@@ -182,7 +186,7 @@ func (aad *AgentApplierDeleter) makeAgentDaemonSet(configChecksum string) *appsv
 	annotations := map[string]string{commonresources.AnnotationKeyChecksumConfig: configChecksum}
 	maps.Copy(annotations, makeIstioAnnotations(IstioCertPath))
 
-	podSpec := makePodSpec(aad.baseName, aad.image, aad.podSpecOptions...)
+	podSpec := makePodSpec(aad.baseName, aad.image, aad.podOpts, aad.containerOpts)
 
 	selectorLabels := commonresources.MakeDefaultSelectorLabels(aad.baseName)
 	labels := commonresources.MakeDefaultLabels(aad.baseName, commonresources.LabelValueK8sComponentAgent)
@@ -291,57 +295,5 @@ func makeFileLogCheckPointVolumeMount() corev1.VolumeMount {
 	return corev1.VolumeMount{
 		Name:      checkpointVolumeName,
 		MountPath: CheckpointVolumePath,
-	}
-}
-
-func makeMetricAgentSecurityContext() *corev1.SecurityContext {
-	return &corev1.SecurityContext{
-		Privileged:               ptr.To(false),
-		RunAsUser:                ptr.To(collectorUser),
-		RunAsNonRoot:             ptr.To(true),
-		ReadOnlyRootFilesystem:   ptr.To(true),
-		AllowPrivilegeEscalation: ptr.To(false),
-		SeccompProfile: &corev1.SeccompProfile{
-			Type: corev1.SeccompProfileTypeRuntimeDefault,
-		},
-		Capabilities: &corev1.Capabilities{
-			Drop: []corev1.Capability{"ALL"},
-		},
-	}
-}
-
-func makeLogAgentSecurityContext() *corev1.SecurityContext {
-	return &corev1.SecurityContext{
-		Privileged:               ptr.To(false),
-		RunAsUser:                ptr.To(int64(0)),
-		RunAsNonRoot:             ptr.To(false),
-		ReadOnlyRootFilesystem:   ptr.To(true),
-		AllowPrivilegeEscalation: ptr.To(false),
-		SeccompProfile: &corev1.SeccompProfile{
-			Type: corev1.SeccompProfileTypeRuntimeDefault,
-		},
-		Capabilities: &corev1.Capabilities{
-			Add:  []corev1.Capability{"FOWNER"},
-			Drop: []corev1.Capability{"ALL"},
-		},
-	}
-}
-
-func makeMetricAgentPodSecurityContext() *corev1.PodSecurityContext {
-	return &corev1.PodSecurityContext{
-		RunAsUser:    ptr.To(collectorUser),
-		RunAsNonRoot: ptr.To(true),
-		SeccompProfile: &corev1.SeccompProfile{
-			Type: corev1.SeccompProfileTypeRuntimeDefault,
-		},
-	}
-}
-
-func makeLogAgentPodSecurityContext() *corev1.PodSecurityContext {
-	return &corev1.PodSecurityContext{
-		RunAsNonRoot: ptr.To(false),
-		SeccompProfile: &corev1.SeccompProfile{
-			Type: corev1.SeccompProfileTypeRuntimeDefault,
-		},
 	}
 }
--- internal/resources/otelcollector/core_pod_spec.go
+++ internal/resources/otelcollector/core_pod_spec.go
@@ -3,132 +3,66 @@ package otelcollector
 import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/utils/ptr"
 
 	"github.com/kyma-project/telemetry-manager/internal/otelcollector/ports"
+	commonresources "github.com/kyma-project/telemetry-manager/internal/resources/common"
 )
 
 const (
-	configMapKey                 = "relay.conf"
-	collectorUser          int64 = 10001
-	collectorContainerName       = "collector"
+	configFileName       = "relay.conf"
+	containerName        = "collector"
+	userDefault    int64 = 10001
+	userRoot       int64 = 0
 )
-
-type podSpecOption = func(pod *corev1.PodSpec)
-
-func withAffinity(affinity corev1.Affinity) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		pod.Affinity = &affinity
-	}
-}
 
 const (
 	fieldPathPodIP    = "status.podIP"
 	fieldPathNodeName = "spec.nodeName"
 )
 
-func withEnvVarFromSource(envVarName, fieldPath string) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		pod.Containers[0].Env = append(pod.Containers[0].Env, corev1.EnvVar{
-			Name: envVarName,
-			ValueFrom: &corev1.EnvVarSource{
-				FieldRef: &corev1.ObjectFieldSelector{
-					FieldPath:  fieldPath,
-					APIVersion: "v1",
-				},
-			},
-		})
-	}
-}
-
-func withVolumeMounts(volumeMounts []corev1.VolumeMount) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		for _, v := range volumeMounts {
-			for i := range pod.Containers {
-				pod.Containers[i].VolumeMounts = append(pod.Containers[i].VolumeMounts, v)
-			}
-		}
-	}
-}
-
-func withVolumes(volumes []corev1.Volume) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		pod.Volumes = append(pod.Volumes, volumes...)
-	}
-}
-
-func withSecurityContext(securityContext *corev1.SecurityContext) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		for i := range pod.Containers {
-			pod.Containers[i].SecurityContext = securityContext
-		}
-	}
-}
-
-func withPodSecurityContext(securityContext *corev1.PodSecurityContext) podSpecOption {
-	return func(pod *corev1.PodSpec) {
-		pod.SecurityContext = securityContext
-	}
-}
-
-func makePodSpec(baseName, image string, opts ...podSpecOption) corev1.PodSpec {
-	pod := corev1.PodSpec{
-		Containers: []corev1.Container{
-			{
-				Name:  collectorContainerName,
-				Image: image,
-				Args: []string{
-					"--config=/conf/" + configMapKey,
-				},
-				EnvFrom: []corev1.EnvFromSource{
-					{
-						SecretRef: &corev1.SecretEnvSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: baseName,
-							},
-							Optional: ptr.To(true),
-						},
+func makePodSpec(
+	baseName,
+	image string,
+	podOpts []commonresources.PodSpecOption,
+	containerOpts []commonresources.ContainerOption,
+) corev1.PodSpec {
+	volumes := []corev1.Volume{
+		{
+			Name: "config",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: baseName,
 					},
-				},
-				VolumeMounts: []corev1.VolumeMount{{Name: "config", MountPath: "/conf"}},
-				LivenessProbe: &corev1.Probe{
-					ProbeHandler: corev1.ProbeHandler{
-						HTTPGet: &corev1.HTTPGetAction{Path: "/", Port: intstr.IntOrString{IntVal: ports.HealthCheck}},
-					},
-				},
-				ReadinessProbe: &corev1.Probe{
-					ProbeHandler: corev1.ProbeHandler{
-						HTTPGet: &corev1.HTTPGetAction{Path: "/", Port: intstr.IntOrString{IntVal: ports.HealthCheck}},
-					},
-				},
-			},
-		},
-		ServiceAccountName: baseName,
-		SecurityContext: &corev1.PodSecurityContext{
-			RunAsUser:    ptr.To(collectorUser),
-			RunAsNonRoot: ptr.To(true),
-			SeccompProfile: &corev1.SeccompProfile{
-				Type: corev1.SeccompProfileTypeRuntimeDefault,
-			},
-		},
-		Volumes: []corev1.Volume{
-			{
-				Name: "config",
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: baseName,
-						},
-						Items: []corev1.KeyToPath{{Key: configMapKey, Path: configMapKey}},
-					},
+					Items: []corev1.KeyToPath{{Key: configFileName, Path: configFileName}},
 				},
 			},
 		},
 	}
 
-	for _, opt := range opts {
-		opt(&pod)
+	volumeMounts := []corev1.VolumeMount{{Name: "config", MountPath: "/conf"}}
+
+	healthProbe := &corev1.Probe{
+		ProbeHandler: corev1.ProbeHandler{
+			HTTPGet: &corev1.HTTPGetAction{Path: "/", Port: intstr.IntOrString{IntVal: ports.HealthCheck}},
+		},
 	}
 
-	return pod
+	defaultContainerOpts := []commonresources.ContainerOption{
+		commonresources.WithArgs([]string{"--config=/conf/" + configFileName}),
+		commonresources.WithEnvVarsFromSecret(baseName),
+		commonresources.WithProbes(healthProbe, healthProbe),
+		commonresources.WithRunAsUser(userDefault),
+		commonresources.WithVolumeMounts(volumeMounts),
+	}
+	containerOpts = append(defaultContainerOpts, containerOpts...)
+
+	defaultPodOpts := []commonresources.PodSpecOption{
+		commonresources.WithContainer(containerName, image, containerOpts...),
+		commonresources.WithPodRunAsUser(userDefault),
+		commonresources.WithVolumes(volumes),
+	}
+	podOpts = append(defaultPodOpts, podOpts...)
+
+	return commonresources.MakePodSpec(baseName, podOpts...)
 }
