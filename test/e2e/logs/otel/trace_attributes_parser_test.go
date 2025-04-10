19136507911cc89bd6d6b36d813055fba9c5141e
Hisar Balik <hisar.balik@sap.com>
2025-04-10 12:41:00 +0200

    feat: add support to extract trace information from log (#2022)

---
+++ test/e2e/logs/otel/trace_attributes_parser_test.go
@@ -0,0 +1,173 @@
+//go:build e2e
+
+package otel
+
+import (
+	"io"
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/log"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/loggen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+)
+
+var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, suite.LabelExperimental), Ordered, func() {
+	var (
+		mockNs           = suite.ID()
+		backendNs        = suite.IDWithSuffix("backend")
+		pipelineName     = suite.ID()
+		backendExportURL string
+	)
+
+	makeResources := func() []client.Object {
+		var objs []client.Object
+		objs = append(objs, kitk8s.NewNamespace(mockNs).K8sObject())
+		objs = append(objs, kitk8s.NewNamespace(backendNs).K8sObject())
+
+		backend := backend.New(backendNs, backend.SignalTypeLogsOtel, backend.WithPersistentHostSecret(suite.IsUpgrade()))
+		logProducer := loggen.New(mockNs).WithUseJSON()
+		objs = append(objs, backend.K8sObjects()...)
+		objs = append(objs, logProducer.K8sObject())
+		backendExportURL = backend.ExportURL(suite.ProxyClient)
+
+		hostSecretRef := backend.HostSecretRefV1Alpha1()
+		pipelineBuilder := testutils.NewLogPipelineBuilder().
+			WithName(pipelineName).
+			WithIncludeNamespaces(mockNs).
+			WithApplicationInput(true).
+			WithOTLPOutput(
+				testutils.OTLPEndpointFromSecret(
+					hostSecretRef.Name,
+					hostSecretRef.Namespace,
+					hostSecretRef.Key,
+				),
+			)
+		if suite.IsUpgrade() {
+			pipelineBuilder.WithLabels(kitk8s.PersistentLabel)
+		}
+		logPipeline := pipelineBuilder.Build()
+
+		objs = append(objs,
+			&logPipeline,
+		)
+		return objs
+	}
+
+	Context("When a log pipeline with runtime input exists", Ordered, func() {
+		BeforeAll(func() {
+			k8sObjects := makeResources()
+
+			DeferCleanup(func() {
+				Expect(kitk8s.DeleteObjects(suite.Ctx, suite.K8sClient, k8sObjects...)).Should(Succeed())
+			})
+			Expect(kitk8s.CreateObjects(suite.Ctx, suite.K8sClient, k8sObjects...)).Should(Succeed())
+		})
+
+		It("Should have a running log agent daemonset", func() {
+			assert.DaemonSetReady(suite.Ctx, suite.K8sClient, kitkyma.LogAgentName)
+		})
+
+		It("Should have a log backend running", func() {
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, types.NamespacedName{Name: backend.DefaultName, Namespace: backendNs})
+		})
+
+		It("Should have a running pipeline", func() {
+			assert.LogPipelineOtelHealthy(suite.Ctx, suite.K8sClient, pipelineName)
+		})
+
+		It("Should deliver loggen logs", func() {
+			assert.LogsFromNamespaceDelivered(suite.ProxyClient, backendExportURL, mockNs)
+		})
+
+		It("Should have trace and span ids in logs", func() {
+			Eventually(func(g Gomega) {
+				resp, err := suite.ProxyClient.Get(backendExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+				bodyContent, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveOtelTimestamp(Not(BeEmpty())),
+					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveTraceId(Not(BeEmpty())),
+					HaveSpanId(Not(BeEmpty())),
+					HaveTraceId(Equal("255c2212dd02c02ac59a923ff07aec74")),
+				))))
+			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
+
+		It("Should parse traceparent", func() {
+			Eventually(func(g Gomega) {
+				resp, err := suite.ProxyClient.Get(backendExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+				bodyContent, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveOtelTimestamp(Not(BeEmpty())),
+					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveSpanId(Not(BeEmpty())),
+					HaveTraceId(Equal("80e1afed08e019fc1110464cfa66635c")),
+				))))
+			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
+
+		It("Should remove trace_id, span_id, trace_flags, and traceparent attributes", func() {
+			Consistently(func(g Gomega) {
+				resp, err := suite.ProxyClient.Get(backendExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+				bodyContent, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveOtelTimestamp(Not(BeEmpty())),
+					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveAttributes(Not(HaveKey("trace_id"))),
+					HaveAttributes(Not(HaveKey("span_id"))),
+					HaveAttributes(Not(HaveKey("trace_flags"))),
+					HaveAttributes(Not(HaveKey("traceparent"))),
+				))))
+			}, periodic.ConsistentlyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
+
+		It("Should have span_id log attribute but no trace data, not parsable", func() {
+			Consistently(func(g Gomega) {
+				resp, err := suite.ProxyClient.Get(backendExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+				bodyContent, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveOtelTimestamp(Not(BeEmpty())),
+					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveTraceId(BeEmpty()),
+					HaveSpanId(BeEmpty()),
+					HaveAttributes(HaveKey("span_id")),
+				))))
+			}, periodic.ConsistentlyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
+	})
+})
