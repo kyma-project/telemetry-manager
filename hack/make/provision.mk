9eb1146c6d2adb1de53a19f374eb65567e688935
Andreas Thaler <andreas.thaler01@sap.com>
2025-04-11 15:00:22 +0200

    chore: support next k8s version 1.32 which is still in preview

--- hack/make/provision.mk
+++ hack/make/provision.mk
@@ -33,12 +33,12 @@ export GARDENER_MACHINE_TYPE ?= $(ENV_GARDENER_MACHINE_TYPE)
 export GARDENER_MIN_NODES ?= $(ENV_GARDENER_MIN_NODES)
 export GARDENER_MAX_NODES ?= $(ENV_GARDENER_MAX_NODES)
 
-ifneq (,$(GARDENER_SA_PATH))
-export GARDENER_K8S_VERSION_FULL=$(shell kubectl --kubeconfig=${GARDENER_SA_PATH} get cloudprofiles.core.gardener.cloud gcp -o go-template='{{range .spec.kubernetes.versions}}{{if and (eq .classification "supported") (lt .version "${GARDENER_K8S_VERSION}.a") (gt .version "${GARDENER_K8S_VERSION}")}}{{.version}}{{break}}{{end}}{{end}}')
-endif
-
 .PHONY: provision-gardener
-provision-gardener: ## Provision gardener cluster with latest k8s version
+provision-gardener: $(JQ) ## Provision gardener cluster with latest k8s version
+    
+	kubectl --kubeconfig=${GARDENER_SA_PATH} get cloudprofiles.core.gardener.cloud gcp -o jsonpath='{.spec.kubernetes.versions}' > /tmp/versions.json
+	export GARDENER_K8S_VERSION_FULL=$($(JQ) -r '(map(select(.version | startswith("${GARDENER_K8S_VERSION}")) | select(.classification == "supported")) | .[0].version) // (map(select(.version | startswith("{GARDENER_K8S_VERSION}")) | select(.classification == "preview")) | .[0].version)' /tmp/versions.json)
+
 	env
 	envsubst < hack/shoot_gcp.yaml > /tmp/shoot.yaml 
 	cat /tmp/shoot.yaml
