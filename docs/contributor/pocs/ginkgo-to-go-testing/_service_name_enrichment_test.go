ea8924de50920598dba6a366f2c16cde541b0b89
Stanislav Khalash <stanislav.khalash@sap.com>
2025-04-23 13:58:01 +0200

    Replace example

--- docs/contributor/pocs/ginkgo-to-go-testing/_service_name_enrichment_test.go
+++ docs/contributor/pocs/ginkgo-to-go-testing/_service_name_enrichment_test.go
@@ -1,4 +1,4 @@
-package otel
+package migrated
 
 import (
 	"context"
@@ -12,17 +12,12 @@ import (
 
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/require"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	operatorv1alpha1 "github.com/kyma-project/telemetry-manager/apis/operator/v1alpha1"
 	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
-	telemetryv1beta1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1beta1"
 	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
-	"github.com/kyma-project/telemetry-manager/test/testkit/apiserverproxy"
 	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
 	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
@@ -32,26 +27,12 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
 	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
-	istionetworkingclientv1 "istio.io/client-go/pkg/apis/networking/v1"
-	istiosecurityclientv1 "istio.io/client-go/pkg/apis/security/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
-var (
-	scheme = runtime.NewScheme()
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
-
-	utilruntime.Must(operatorv1alpha1.AddToScheme(scheme))
-	utilruntime.Must(telemetryv1alpha1.AddToScheme(scheme))
-	utilruntime.Must(telemetryv1beta1.AddToScheme(scheme))
-	utilruntime.Must(istiosecurityclientv1.AddToScheme(scheme))
-	utilruntime.Must(istionetworkingclientv1.AddToScheme(scheme))
+func TestMain(m *testing.M) {
+	suite.BeforeSuiteFunc()
+	m.Run()
+	suite.AfterSuiteFunc()
 }
 
 func TestOTelLogPipeline_ServiceNameEnrichment(t *testing.T) {
@@ -105,6 +86,8 @@ func TestOTelLogPipeline_ServiceNameEnrichment(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			// t.Parallel()
+
 			var (
 				mockNs       = suite.IDWithSuffix(tc.name + "-mocks")
 				genName      = suite.IDWithSuffix(tc.name + "-gen")
@@ -134,23 +117,21 @@ func TestOTelLogPipeline_ServiceNameEnrichment(t *testing.T) {
 			)
 			resources = append(resources, backend.K8sObjects()...)
 
-			k8sClient, proxyClient := initializeClients(t)
-
 			t.Cleanup(func() {
 				// Cannot use t.Context() here because it is already canceled at this point
-				err := kitk8s.DeleteObjects(context.Background(), k8sClient, resources...)
+				err := kitk8s.DeleteObjects(context.Background(), suite.K8sClient, resources...)
 				require.NoError(t, err)
 			})
-			Expect(kitk8s.CreateObjects(t.Context(), k8sClient, resources...)).Should(Succeed())
+			Expect(kitk8s.CreateObjects(t.Context(), suite.K8sClient, resources...)).Should(Succeed())
 
 			t.Log("Waiting for resources to be ready")
 
-			assert.DeploymentReady(t.Context(), k8sClient, kitkyma.LogGatewayName)
-			assert.DeploymentReady(t.Context(), k8sClient, types.NamespacedName{Name: backend.Name(), Namespace: mockNs})
-			assert.LogPipelineOtelHealthy(t.Context(), k8sClient, pipelineName)
-			assert.LogsFromNamespaceDelivered(proxyClient, backend.ExportURL(proxyClient), mockNs)
+			assert.DeploymentReady(t.Context(), suite.K8sClient, kitkyma.LogGatewayName)
+			assert.DeploymentReady(t.Context(), suite.K8sClient, types.NamespacedName{Name: backend.Name(), Namespace: mockNs})
+			assert.LogPipelineOtelHealthy(t.Context(), suite.K8sClient, pipelineName)
+			assert.OtelLogsFromNamespaceDelivered(suite.ProxyClient, backend.ExportURL(suite.ProxyClient), mockNs)
 
-			verifyServiceNameAttr(proxyClient, backend.ExportURL(proxyClient), genName, appLabelValue)
+			verifyServiceNameAttr(backend.ExportURL(suite.ProxyClient), genName, appLabelValue)
 		})
 	}
 }
@@ -174,26 +155,9 @@ func skipIfDoesNotMatchLabel(t *testing.T, label string) {
 	}
 }
 
-func initializeClients(t *testing.T) (client.Client, *apiserverproxy.Client) {
-	t.Helper()
-	t.Log("Initializing clients")
-
-	kubeconfig := clientcmd.NewDefaultClientConfigLoadingRules().GetDefaultFilename()
-	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	Expect(err).NotTo(HaveOccurred())
-
-	k8sClient, err := client.New(cfg, client.Options{Scheme: scheme})
-	Expect(err).NotTo(HaveOccurred())
-
-	proxyClient, err := apiserverproxy.NewClient(cfg)
-	Expect(err).NotTo(HaveOccurred())
-
-	return k8sClient, proxyClient
-}
-
-func verifyServiceNameAttr(proxyClient *apiserverproxy.Client, backendExportURL, givenPodPrefix, expectedServiceName string) {
+func verifyServiceNameAttr(backendExportURL, givenPodPrefix, expectedServiceName string) {
 	Eventually(func(g Gomega) {
-		resp, err := proxyClient.Get(backendExportURL)
+		resp, err := suite.ProxyClient.Get(backendExportURL)
 		g.Expect(err).NotTo(HaveOccurred())
 		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
 
