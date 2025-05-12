64f973bdac3653b67b74b03faa9dd00badd9df4e
Andreas Thaler <andreas.thaler01@sap.com>
2025-05-12 09:26:37 +0200

    fix: remove level attribute after parsing, support log.level as well (#2105)

--- test/e2e/logs/otel/application/trace_and_severity_parser_test.go
+++ test/e2e/logs/otel/application/trace_and_severity_parser_test.go
@@ -100,9 +100,14 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, s
 				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
 					HaveOtelTimestamp(Not(BeEmpty())),
 					HaveObservedTimestamp(Not(BeEmpty())),
-					HaveTraceId(Not(BeEmpty())),
-					HaveSpanId(Not(BeEmpty())),
+					HaveAttributes(HaveKeyWithValue("name", "a")),
 					HaveTraceId(Equal("255c2212dd02c02ac59a923ff07aec74")),
+					HaveSpanId(Equal("c5c735f175ad06a6")),
+					HaveTraceFlags(Equal(uint32(0))),
+					HaveAttributes(Not(HaveKey("trace_id"))),
+					HaveAttributes(Not(HaveKey("span_id"))),
+					HaveAttributes(Not(HaveKey("trace_flags"))),
+					HaveAttributes(Not(HaveKey("traceparent"))),
 				))))
 			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
 		})
@@ -120,31 +125,16 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, s
 				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
 					HaveOtelTimestamp(Not(BeEmpty())),
 					HaveObservedTimestamp(Not(BeEmpty())),
-					HaveSpanId(Not(BeEmpty())),
+					HaveAttributes(HaveKeyWithValue("name", "b")),
 					HaveTraceId(Equal("80e1afed08e019fc1110464cfa66635c")),
-				))))
-			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
-		})
-
-		It("Should remove trace_id, span_id, trace_flags, and traceparent attributes", func() {
-			Consistently(func(g Gomega) {
-				resp, err := suite.ProxyClient.Get(backendExportURL)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
-
-				bodyContent, err := io.ReadAll(resp.Body)
-				defer resp.Body.Close()
-				g.Expect(err).NotTo(HaveOccurred())
-
-				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
-					HaveOtelTimestamp(Not(BeEmpty())),
-					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveSpanId(Equal("7a085853722dc6d2")),
+					HaveTraceFlags(Equal(uint32(1))),
 					HaveAttributes(Not(HaveKey("trace_id"))),
 					HaveAttributes(Not(HaveKey("span_id"))),
 					HaveAttributes(Not(HaveKey("trace_flags"))),
 					HaveAttributes(Not(HaveKey("traceparent"))),
 				))))
-			}, periodic.ConsistentlyTimeout, periodic.TelemetryInterval).Should(Succeed())
+			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
 		})
 
 		It("Should have span_id log attribute but no trace data, not parsable", func() {
@@ -160,11 +150,39 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, s
 				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
 					HaveOtelTimestamp(Not(BeEmpty())),
 					HaveObservedTimestamp(Not(BeEmpty())),
+					HaveAttributes(HaveKeyWithValue("name", "c")),
 					HaveTraceId(BeEmpty()),
 					HaveSpanId(BeEmpty()),
+					HaveTraceFlags(Equal(uint32(0))), // default value
 					HaveAttributes(HaveKey("span_id")),
 				))))
 			}, periodic.ConsistentlyTimeout, periodic.TelemetryInterval).Should(Succeed())
+		})
+
+		It("Should have severityText and severityNumber in logs", func() {
+			Eventually(func(g Gomega) {
+				resp, err := suite.ProxyClient.Get(backendExportURL)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+				bodyContent, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveAttributes(HaveKeyWithValue("name", "b")),
+					HaveSeverityNumber(Equal(13)),
+					HaveSeverityText(Equal("WARN")),
+					HaveAttributes(Not(HaveKey("log.level"))),
+				))))
+
+				g.Expect(bodyContent).To(HaveFlatOtelLogs(ContainElement(SatisfyAll(
+					HaveAttributes(HaveKeyWithValue("name", "a")),
+					HaveSeverityNumber(Equal(9)),
+					HaveSeverityText(Equal("INFO")),
+					HaveAttributes(Not(HaveKey("level"))),
+				))))
+			}, periodic.EventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
 		})
 	})
 })
--- test/testkit/mocks/loggen/log_producer.go
+++ test/testkit/mocks/loggen/log_producer.go
@@ -110,11 +110,9 @@ done`
 	if lp.useJSON {
 		logCmd = `while true
 do
-	echo '{"name": "John Doe", "age": 30, "city": "Munich", "trace_id": "255c2212dd02c02ac59a923ff07aec74", "span_id": "c5c735f175ad06a6", "trace_flags": "01"}'
-    sleep 1
-    echo '{"name": "John Doe", "age": 30, "city": "Munich", "traceparent": "00-80e1afed08e019fc1110464cfa66635c-7a085853722dc6d2-01"}'
-    sleep 1
-    echo '{"name": "John Doe", "age": 30, "city": "Munich", "span_id": "123456789"}'
+	echo '{"name": "a", "level": "INFO", "age": 30, "city": "Munich", "trace_id": "255c2212dd02c02ac59a923ff07aec74", "span_id": "c5c735f175ad06a6", "trace_flags": "00"}'
+    echo '{"name": "b", "log.level":"WARN", "age": 30, "city": "Munich", "traceparent": "00-80e1afed08e019fc1110464cfa66635c-7a085853722dc6d2-01"}'
+    echo '{"name": "c", "age": 30, "city": "Munich", "span_id": "123456789"}'
 	sleep 10
 done`
 	}
@@ -123,7 +121,7 @@ done`
 		Containers: []corev1.Container{
 			{
 				Name:    DefaultContainerName,
-				Image:   "alpine:3.17.2",
+				Image:   "alpine:latest",
 				Command: []string{"/bin/sh", "-c", logCmd}},
 		},
 	}
