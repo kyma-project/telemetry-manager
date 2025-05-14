9d89981614e643096226221972e91ffde2d003c9
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-05-14 14:45:39 +0200

    resources_test and service_name_test WIP

---
+++ test/e2e/logs/migrated/shared/service_name_test.go
@@ -0,0 +1,134 @@
+package shared
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/log"
+	kitbackend "github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/loggen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	"github.com/kyma-project/telemetry-manager/test/testkit/unique"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// TODO: TO BE FIXED
+func TestServiceName(t *testing.T) {
+	RegisterTestingT(t)
+
+	tests := []struct {
+		name  string
+		input telemetryv1alpha1.LogPipelineInput
+	}{
+		{
+			name:  "agent",
+			input: testutils.BuildLogPipelineApplicationInput(),
+		},
+		{
+			name:  "gateway",
+			input: testutils.BuildLogPipelineOTLPInput(),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			const (
+				jobName               = "job"
+				podWithNoLabelsName   = "pod-with-no-labels"
+				kubeAppLabelKey       = "app.kubernetes.io/name"
+				kubeAppLabelValue     = "kube-workload"
+				appLabelKey           = "app"
+				appLabelValue         = "workload"
+				podWithBothLabelsName = "pod-with-both-app-labels" // #nosec G101 -- This is a false positive
+			)
+
+			var (
+				uniquePrefix = unique.Prefix()
+				pipelineName = uniquePrefix()
+				mockNs       = uniquePrefix()
+			)
+
+			backend := kitbackend.New(mockNs, kitbackend.SignalTypeLogsOTel)
+			backendExportURL := backend.ExportURL(suite.ProxyClient)
+			hostSecretRef := backend.HostSecretRefV1Alpha1()
+
+			pipeline := testutils.NewLogPipelineBuilder().
+				WithName(pipelineName).
+				WithInput(tc.input).
+				WithOTLPOutput(
+					testutils.OTLPEndpointFromSecret(
+						hostSecretRef.Name,
+						hostSecretRef.Namespace,
+						hostSecretRef.Key,
+					),
+				).
+				Build()
+
+			logGenerator := loggen.New(mockNs)
+
+			resources := []client.Object{
+				kitk8s.NewNamespace(mockNs).K8sObject(),
+				&pipeline,
+				kitk8s.NewPod(podWithBothLabelsName, mockNs).
+					WithLabel(kubeAppLabelKey, kubeAppLabelValue).
+					WithLabel(appLabelKey, appLabelValue).
+					WithPodSpec(logGenerator.PodSpec()).
+					K8sObject(),
+				kitk8s.NewJob(jobName, mockNs).WithPodSpec(logGenerator.PodSpec()).K8sObject(),
+				kitk8s.NewPod(podWithNoLabelsName, mockNs).WithPodSpec(logGenerator.PodSpec()).K8sObject(),
+			}
+			resources = append(resources, backend.K8sObjects()...)
+
+			t.Cleanup(func() {
+				require.NoError(t, kitk8s.DeleteObjects(context.Background(), suite.K8sClient, resources...)) //nolint:usetesting // Remove ctx from DeleteObjects
+			})
+			Expect(kitk8s.CreateObjects(t.Context(), suite.K8sClient, resources...)).Should(Succeed())
+
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, kitkyma.LogGatewayName)
+			assert.DaemonSetReady(suite.Ctx, suite.K8sClient, kitkyma.LogAgentName)
+			assert.DeploymentReady(suite.Ctx, suite.K8sClient, types.NamespacedName{Name: kitbackend.DefaultName, Namespace: mockNs})
+			assert.OTelLogPipelineHealthy(suite.Ctx, suite.K8sClient, pipelineName)
+			assert.OTelLogsFromNamespaceDelivered(suite.ProxyClient, backendExportURL, mockNs)
+
+			verifyServiceNameAttr := func(givenPodPrefix, expectedServiceName string) {
+				Eventually(func(g Gomega) {
+					resp, err := suite.ProxyClient.Get(backendExportURL)
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+
+					bodyContent, err := io.ReadAll(resp.Body)
+					defer resp.Body.Close()
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(bodyContent).To(HaveFlatOTelLogs(
+						ContainElement(SatisfyAll(
+							HaveResourceAttributes(HaveKeyWithValue("service.name", expectedServiceName)),
+							HaveResourceAttributes(HaveKeyWithValue("k8s.pod.name", ContainSubstring(givenPodPrefix))),
+						)),
+					))
+				}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed(), fmt.Sprintf("could not find logs matching service.name: %s, k8s.pod.name: %s.*", expectedServiceName, givenPodPrefix))
+			}
+
+			verifyServiceNameAttr(podWithBothLabelsName, kubeAppLabelValue)
+			verifyServiceNameAttr(jobName, jobName)
+			verifyServiceNameAttr(podWithNoLabelsName, podWithNoLabelsName)
+			assert.TelemetryDataDelivered(suite.ProxyClient, backendExportURL, HaveFlatOTelLogs(
+				Not(ContainElement(
+					HaveResourceAttributes(HaveKey(ContainSubstring("kyma"))),
+				)),
+			))
+		})
+	}
+}
---
+++ test/testkit/assert/resource.go
@@ -0,0 +1,35 @@
+package assert
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	. "github.com/onsi/gomega"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Resource struct {
+	Object client.Object
+	Name   types.NamespacedName
+}
+
+func NewResource[T client.Object](name types.NamespacedName) Resource {
+	var obj T
+	return Resource{
+		Object: obj,
+		Name:   name,
+	}
+}
+
+func ResourcesExist(ctx context.Context, k8sClient client.Client, resources ...Resource) {
+	for _, resource := range resources {
+		Eventually(func(g Gomega) bool {
+			err := suite.K8sClient.Get(ctx, resource.Name, resource.Object)
+			return apierrors.IsNotFound(err)
+		}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(BeTrue(), fmt.Sprintf("resource %s of type %s should exist", resource.Name, resource.Object.GetObjectKind().GroupVersionKind().Kind))
+	}
+}
