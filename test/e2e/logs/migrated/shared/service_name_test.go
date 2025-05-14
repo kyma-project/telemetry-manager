c21432ef485e760f3309d9aa18c2522c7056a198
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-05-14 20:27:10 +0200

    Fully migrate service_name test (agent+gateway)

--- test/e2e/logs/migrated/shared/service_name_test.go
+++ test/e2e/logs/migrated/shared/service_name_test.go
@@ -9,6 +9,7 @@ import (
 
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -20,24 +21,27 @@ import (
 	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/log"
 	kitbackend "github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
 	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/loggen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
 	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 	"github.com/kyma-project/telemetry-manager/test/testkit/unique"
 )
 
-// TODO: TO BE FIXED
 func TestServiceName_OTel(t *testing.T) {
 	tests := []struct {
-		label string
-		input telemetryv1alpha1.LogPipelineInput
+		label       string
+		input       telemetryv1alpha1.LogPipelineInput
+		expectAgent bool
 	}{
 		{
-			label: suite.LabelLogAgent,
-			input: testutils.BuildLogPipelineApplicationInput(),
+			label:       suite.LabelLogAgent,
+			input:       testutils.BuildLogPipelineApplicationInput(),
+			expectAgent: true,
 		},
 		{
-			label: suite.LabelLogGateway,
-			input: testutils.BuildLogPipelineOTLPInput(),
+			label:       suite.LabelLogGateway,
+			input:       testutils.BuildLogPipelineOTLPInput(),
+			expectAgent: false,
 		},
 	}
 
@@ -48,17 +52,22 @@ func TestServiceName_OTel(t *testing.T) {
 			const (
 				jobName               = "job"
 				podWithNoLabelsName   = "pod-with-no-labels"
+				podWithAppLabelName   = "pod-with-app-label"
 				kubeAppLabelKey       = "app.kubernetes.io/name"
 				kubeAppLabelValue     = "kube-workload"
 				appLabelKey           = "app"
 				appLabelValue         = "workload"
 				podWithBothLabelsName = "pod-with-both-app-labels" // #nosec G101 -- This is a false positive
+				serviceKey            = "service.name"
+				podKey                = "k8s.pod.name"
 			)
 
 			var (
-				uniquePrefix = unique.Prefix()
-				pipelineName = uniquePrefix()
-				mockNs       = uniquePrefix()
+				uniquePrefix    = unique.Prefix()
+				pipelineName    = uniquePrefix()
+				deploymentName  = uniquePrefix("gateway")
+				statefulSetName = uniquePrefix("gateway")
+				mockNs          = uniquePrefix()
 			)
 
 			backend := kitbackend.New(mockNs, kitbackend.SignalTypeLogsOTel)
@@ -68,6 +77,7 @@ func TestServiceName_OTel(t *testing.T) {
 			pipeline := testutils.NewLogPipelineBuilder().
 				WithName(pipelineName).
 				WithInput(tc.input).
+				WithKeepOriginalBody(tc.expectAgent).
 				WithOTLPOutput(
 					testutils.OTLPEndpointFromSecret(
 						hostSecretRef.Name,
@@ -77,20 +87,34 @@ func TestServiceName_OTel(t *testing.T) {
 				).
 				Build()
 
-			logGenerator := loggen.New(mockNs)
-
 			resources := []client.Object{
 				kitk8s.NewNamespace(mockNs).K8sObject(),
 				&pipeline,
-				kitk8s.NewPod(podWithBothLabelsName, mockNs).
-					WithLabel(kubeAppLabelKey, kubeAppLabelValue).
-					WithLabel(appLabelKey, appLabelValue).
-					WithPodSpec(logGenerator.PodSpec()).
-					K8sObject(),
-				kitk8s.NewJob(jobName, mockNs).WithPodSpec(logGenerator.PodSpec()).K8sObject(),
-				kitk8s.NewPod(podWithNoLabelsName, mockNs).WithPodSpec(logGenerator.PodSpec()).K8sObject(),
 			}
 			resources = append(resources, backend.K8sObjects()...)
+
+			if tc.expectAgent {
+				podSpecLogs := loggen.New(mockNs).PodSpec()
+				resources = append(resources,
+					kitk8s.NewPod(podWithBothLabelsName, mockNs).
+						WithLabel(kubeAppLabelKey, kubeAppLabelValue).
+						WithLabel(appLabelKey, appLabelValue).
+						WithPodSpec(podSpecLogs).
+						K8sObject(),
+					kitk8s.NewJob(jobName, mockNs).WithPodSpec(podSpecLogs).K8sObject(),
+					kitk8s.NewPod(podWithNoLabelsName, mockNs).WithPodSpec(podSpecLogs).K8sObject(),
+				)
+			} else {
+				podSpecWithUndefinedService := telemetrygen.PodSpec(telemetrygen.SignalTypeLogs, telemetrygen.WithServiceName(""))
+				resources = append(resources,
+					kitk8s.NewPod(podWithAppLabelName, mockNs).
+						WithLabel(appLabelKey, appLabelValue).
+						WithPodSpec(podSpecWithUndefinedService).
+						K8sObject(),
+					kitk8s.NewDeployment(deploymentName, mockNs).WithPodSpec(podSpecWithUndefinedService).K8sObject(),
+					kitk8s.NewStatefulSet(statefulSetName, mockNs).WithPodSpec(podSpecWithUndefinedService).K8sObject(),
+				)
+			}
 
 			t.Cleanup(func() {
 				require.NoError(t, kitk8s.DeleteObjects(context.Background(), suite.K8sClient, resources...)) //nolint:usetesting // Remove ctx from DeleteObjects
@@ -98,7 +122,17 @@ func TestServiceName_OTel(t *testing.T) {
 			Expect(kitk8s.CreateObjects(t.Context(), suite.K8sClient, resources...)).Should(Succeed())
 
 			assert.DeploymentReady(suite.Ctx, suite.K8sClient, kitkyma.LogGatewayName)
-			assert.DaemonSetReady(suite.Ctx, suite.K8sClient, kitkyma.LogAgentName)
+
+			if tc.expectAgent {
+				assert.DaemonSetReady(suite.Ctx, suite.K8sClient, kitkyma.LogAgentName)
+			}
+
+			Eventually(func(g Gomega) int32 {
+				var deployment appsv1.Deployment
+				err := suite.K8sClient.Get(suite.Ctx, kitkyma.LogGatewayName, &deployment)
+				g.Expect(err).NotTo(HaveOccurred())
+				return *deployment.Spec.Replicas
+			}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(Equal(int32(2)))
 			assert.DeploymentReady(suite.Ctx, suite.K8sClient, types.NamespacedName{Name: kitbackend.DefaultName, Namespace: mockNs})
 			assert.OTelLogPipelineHealthy(suite.Ctx, suite.K8sClient, pipelineName)
 			assert.OTelLogsFromNamespaceDelivered(suite.ProxyClient, backendExportURL, mockNs)
@@ -114,19 +148,25 @@ func TestServiceName_OTel(t *testing.T) {
 					g.Expect(err).NotTo(HaveOccurred())
 					g.Expect(bodyContent).To(HaveFlatOTelLogs(
 						ContainElement(SatisfyAll(
-							HaveResourceAttributes(HaveKeyWithValue("service.name", expectedServiceName)),
-							HaveResourceAttributes(HaveKeyWithValue("k8s.pod.name", ContainSubstring(givenPodPrefix))),
+							HaveResourceAttributes(HaveKeyWithValue(serviceKey, expectedServiceName)),
+							HaveResourceAttributes(HaveKeyWithValue(podKey, ContainSubstring(givenPodPrefix))),
 						)),
 					))
 				}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed(), fmt.Sprintf("could not find logs matching service.name: %s, k8s.pod.name: %s.*", expectedServiceName, givenPodPrefix))
 			}
 
-			verifyServiceNameAttr(podWithBothLabelsName, kubeAppLabelValue)
-			verifyServiceNameAttr(jobName, jobName)
-			verifyServiceNameAttr(podWithNoLabelsName, podWithNoLabelsName)
+			if tc.expectAgent {
+				verifyServiceNameAttr(podWithBothLabelsName, kubeAppLabelValue)
+				verifyServiceNameAttr(jobName, jobName)
+				verifyServiceNameAttr(podWithNoLabelsName, podWithNoLabelsName)
+			} else {
+				verifyServiceNameAttr(podWithAppLabelName, appLabelValue)
+				verifyServiceNameAttr(deploymentName, deploymentName)
+				verifyServiceNameAttr(statefulSetName, statefulSetName)
+			}
 
 			// Verify that temporary kyma resource attributes are removed from the logs
-			assert.DataConsistentlyMatching(suite.ProxyClient, backendExportURL, HaveFlatOTelLogs(
+			assert.DataEventuallyMatching(suite.ProxyClient, backendExportURL, HaveFlatOTelLogs(
 				Not(ContainElement(
 					HaveResourceAttributes(HaveKey(ContainSubstring("kyma"))),
 				)),
