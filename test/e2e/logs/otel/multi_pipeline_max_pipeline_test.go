1b104a48d8ee1ad0d09df4ac9100b779761e03cc
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-05-16 15:30:11 +0200

    change e2e test

--- test/e2e/logs/otel/multi_pipeline_max_pipeline_test.go
+++ test/e2e/logs/otel/multi_pipeline_max_pipeline_test.go
@@ -8,9 +8,9 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
 	telemetrycontrollers "github.com/kyma-project/telemetry-manager/controllers/telemetry"
 	"github.com/kyma-project/telemetry-manager/internal/conditions"
 	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
@@ -29,8 +29,8 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelExperimental,
 		const maxNumberOfLogPipelines = telemetrycontrollers.MaxPipelineCount
 
 		var (
-			pipelines      []client.Object
-			pipelinesNames = make([]string, 0, maxNumberOfLogPipelines)
+			pipelines          []client.Object
+			additionalPipeline client.Object
 		)
 
 		backend := backend.New(mockNs, backend.SignalTypeLogsOtel, backend.WithPersistentHostSecret(suite.IsUpgrade()))
@@ -51,7 +51,6 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelExperimental,
 						),
 					).Build()
 				pipelines = append(pipelines, &pipeline)
-				pipelinesNames = append(pipelinesNames, pipelineName)
 			}
 			objs = append(objs, pipelines...)
 
@@ -67,26 +66,24 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelExperimental,
 		})
 
 		It("Should have only running pipelines", func() {
-			for _, pipelineName := range pipelinesNames {
-				assert.LogPipelineHealthy(suite.Ctx, suite.K8sClient, pipelineName)
+			for _, pipeline := range pipelines {
+				assert.LogPipelineHealthy(suite.Ctx, suite.K8sClient, pipeline.GetName())
 			}
 		})
 
-		additionalPipelineName := fmt.Sprintf("%s-limit-exceeding", suite.ID())
-		var pipeline telemetryv1alpha1.LogPipeline
-
 		It("Should create an additional pipeline in not healthy state", func() {
-			pipeline = testutils.NewLogPipelineBuilder().WithName(additionalPipelineName).Build()
+			additionalPipelineName := fmt.Sprintf("%s-limit-exceeding", suite.ID())
+			additionalPipeline = ptr.To(testutils.NewLogPipelineBuilder().WithName(additionalPipelineName).Build())
 
-			Expect(kitk8s.CreateObjects(suite.Ctx, suite.K8sClient, &pipeline)).Should(Succeed())
+			Expect(kitk8s.CreateObjects(suite.Ctx, suite.K8sClient, additionalPipeline)).Should(Succeed())
 
-			assert.LogPipelineHasCondition(suite.Ctx, suite.K8sClient, additionalPipelineName, metav1.Condition{
+			assert.LogPipelineHasCondition(suite.Ctx, suite.K8sClient, additionalPipeline.GetName(), metav1.Condition{
 				Type:   conditions.TypeConfigurationGenerated,
 				Status: metav1.ConditionFalse,
 				Reason: conditions.ReasonMaxPipelinesExceeded,
 			})
 
-			assert.LogPipelineHasCondition(suite.Ctx, suite.K8sClient, additionalPipelineName, metav1.Condition{
+			assert.LogPipelineHasCondition(suite.Ctx, suite.K8sClient, additionalPipeline.GetName(), metav1.Condition{
 				Type:   conditions.TypeFlowHealthy,
 				Status: metav1.ConditionFalse,
 				Reason: conditions.ReasonSelfMonConfigNotGenerated,
@@ -97,7 +94,7 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelExperimental,
 			var deletePipeline client.Object
 			deletePipeline, pipelines = pipelines[0], pipelines[1:]
 			Expect(kitk8s.DeleteObjects(suite.Ctx, suite.K8sClient, deletePipeline)).Should(Succeed())
-			assert.LogPipelineHealthy(suite.Ctx, suite.K8sClient, additionalPipelineName)
+			assert.LogPipelineHealthy(suite.Ctx, suite.K8sClient, additionalPipeline.GetName())
 		})
 
 	})
