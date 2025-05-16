929f442afcdf9469775147cf7445ebde859b2339
Hisar Balik <hisar.balik@sap.com>
2025-05-16 11:27:15 +0200

    fix flaky test

--- test/e2e/logs/otel/application/multi_pipeline_namespace_selector_test.go
+++ test/e2e/logs/otel/application/multi_pipeline_namespace_selector_test.go
@@ -48,7 +48,6 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, s
 			WithName(pipelineNameIncludeApp1Ns).
 			WithOTLPInput(false).
 			WithApplicationInput(true, testutils.IncludeLogNamespaces(app1Ns)).
-			WithExcludeNamespaces(app2Ns).
 			WithOTLPOutput(testutils.OTLPEndpoint(backend1.Endpoint())).
 			Build()
 		objs = append(objs, &pipelineIncludeApp1Ns)
@@ -60,8 +59,7 @@ var _ = Describe(suite.ID(), Label(suite.LabelLogsOtel, suite.LabelSignalPull, s
 		pipelineExcludeApp1Ns := testutils.NewLogPipelineBuilder().
 			WithName(pipelineNameExcludeApp1Ns).
 			WithOTLPInput(false).
-			WithApplicationInput(true, testutils.ExcludeLogNamespaces(app1Ns)).
-			WithIncludeNamespaces(app2Ns).
+			WithApplicationInput(true, testutils.IncludeLogNamespaces(app2Ns)).
 			WithOTLPOutput(testutils.OTLPEndpoint(backend2.Endpoint())).
 			Build()
 		objs = append(objs, &pipelineExcludeApp1Ns)
