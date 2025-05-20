619b90d5c8dd66cb2c60fb7af9df8ddd53225dc6
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-05-20 10:36:49 +0200

    more tests

--- internal/resourcelock/lock.go
+++ internal/resourcelock/lock.go
@@ -24,9 +24,9 @@ type Checker struct {
 	maxOwners int
 }
 
-func NewLocker(client client.Client, lockName types.NamespacedName, maxOwners int) *Checker {
-	if !strings.HasSuffix(lockName.Name, "-lock") {
-		lockName.Name = fmt.Sprintf("%s-lock", lockName.Name)
+func newChecker(client client.Client, lockName types.NamespacedName, maxOwners int, suffix string) *Checker {
+	if !strings.HasSuffix(lockName.Name, "-"+suffix) {
+		lockName.Name = fmt.Sprintf("%s-%s", lockName.Name, suffix)
 	}
 
 	return &Checker{
@@ -36,22 +36,19 @@ func NewLocker(client client.Client, lockName types.NamespacedName, maxOwners in
 	}
 }
 
-func NewSyncer(client client.Client, lockName types.NamespacedName) *Checker {
-	if !strings.HasSuffix(lockName.Name, "-sync") {
-		lockName.Name = fmt.Sprintf("%s-sync", lockName.Name)
-	}
-
-	return &Checker{
-		client:   client,
-		lockName: lockName,
-	}
+func NewLocker(client client.Client, lockName types.NamespacedName, maxOwners int) *Checker {
+	return newChecker(client, lockName, maxOwners, "lock")
 }
 
-func (l *Checker) TryAcquireLock(ctx context.Context, owner metav1.Object) error {
+func NewSyncer(client client.Client, lockName types.NamespacedName) *Checker {
+	return newChecker(client, lockName, 0, "syncer")
+}
+
+func (c *Checker) TryAcquireLock(ctx context.Context, owner metav1.Object) error {
 	var lock corev1.ConfigMap
-	if err := l.client.Get(ctx, l.lockName, &lock); err != nil {
+	if err := c.client.Get(ctx, c.lockName, &lock); err != nil {
 		if apierrors.IsNotFound(err) {
-			return l.createLock(ctx, owner)
+			return c.createLock(ctx, owner)
 		}
 
 		return fmt.Errorf("failed to get lock: %w", err)
@@ -63,12 +60,12 @@ func (l *Checker) TryAcquireLock(ctx context.Context, owner metav1.Object) error
 		}
 	}
 
-	if l.maxOwners == 0 || len(lock.GetOwnerReferences()) < l.maxOwners {
-		if err := controllerutil.SetOwnerReference(owner, &lock, l.client.Scheme()); err != nil {
+	if c.maxOwners == 0 || len(lock.GetOwnerReferences()) < c.maxOwners {
+		if err := controllerutil.SetOwnerReference(owner, &lock, c.client.Scheme()); err != nil {
 			return fmt.Errorf("failed to set owner reference: %w", err)
 		}
 
-		if err := l.client.Update(ctx, &lock); err != nil {
+		if err := c.client.Update(ctx, &lock); err != nil {
 			return fmt.Errorf("failed to update lock: %w", err)
 		}
 
@@ -78,9 +75,9 @@ func (l *Checker) TryAcquireLock(ctx context.Context, owner metav1.Object) error
 	return ErrMaxPipelinesExceeded
 }
 
-func (l *Checker) IsLockHolder(ctx context.Context, obj metav1.Object) error {
+func (c *Checker) IsLockHolder(ctx context.Context, obj metav1.Object) error {
 	var lock corev1.ConfigMap
-	if err := l.client.Get(ctx, l.lockName, &lock); err != nil {
+	if err := c.client.Get(ctx, c.lockName, &lock); err != nil {
 		return &errortypes.APIRequestFailedError{
 			Err: fmt.Errorf("failed to get lock: %w", err),
 		}
@@ -95,19 +92,19 @@ func (l *Checker) IsLockHolder(ctx context.Context, obj metav1.Object) error {
 	return ErrMaxPipelinesExceeded
 }
 
-func (l *Checker) createLock(ctx context.Context, owner metav1.Object) error {
+func (c *Checker) createLock(ctx context.Context, owner metav1.Object) error {
 	lock := corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      l.lockName.Name,
-			Namespace: l.lockName.Namespace,
+			Name:      c.lockName.Name,
+			Namespace: c.lockName.Namespace,
 		},
 	}
 
-	if err := controllerutil.SetOwnerReference(owner, &lock, l.client.Scheme()); err != nil {
+	if err := controllerutil.SetOwnerReference(owner, &lock, c.client.Scheme()); err != nil {
 		return fmt.Errorf("failed to set owner reference: %w", err)
 	}
 
-	if err := l.client.Create(ctx, &lock); err != nil {
+	if err := c.client.Create(ctx, &lock); err != nil {
 		return fmt.Errorf("failed to create lock: %w", err)
 	}
 
