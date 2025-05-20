c905c5863b11360ceb2089333898059fb8f9d70a
Mostafa Shorim <mostafa.shorim@sap.com>
2025-05-20 17:54:02 +0200

    re-generate mocks

--- internal/reconciler/metricpipeline/mocks/flow_health_prober.go
+++ internal/reconciler/metricpipeline/mocks/flow_health_prober.go
@@ -83,8 +83,8 @@ func (_c *FlowHealthProber_Probe_Call) Run(run func(ctx context.Context, pipelin
 	return _c
 }
 
-func (_c *FlowHealthProber_Probe_Call) Return(oTelPipelineProbeResult prober.OTelGatewayProbeResult, err error) *FlowHealthProber_Probe_Call {
-	_c.Call.Return(oTelPipelineProbeResult, err)
+func (_c *FlowHealthProber_Probe_Call) Return(oTelGatewayProbeResult prober.OTelGatewayProbeResult, err error) *FlowHealthProber_Probe_Call {
+	_c.Call.Return(oTelGatewayProbeResult, err)
 	return _c
 }
 
