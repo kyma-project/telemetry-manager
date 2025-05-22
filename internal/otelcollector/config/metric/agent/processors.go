72e7b9656eea4efeb43a40c5bf79fda8a8344b3c
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2025-05-22 10:08:52 +0200

    remove workaround for context inference issue

--- internal/otelcollector/config/metric/agent/processors.go
+++ internal/otelcollector/config/metric/agent/processors.go
@@ -83,17 +83,8 @@ func makeInsertSkipEnrichmentAttributeProcessor() *metric.TransformProcessor {
 		ErrorMode: "ignore",
 		MetricStatements: []config.TransformProcessorStatements{
 			{
-				// Workaround for a context inference issue: Removing the explicit context
-				// for this transform processor causes failures due to conditions and statements
-				// using different context paths.
-				//
-				// To resolve this, a 'where' clause is added to ensure expressions with 'metric.'
-				// can be used within conditions.
-				//
-				// The inference relies on path names found in the statements.
-				// It happens automatically because path names are prefixed with the context name.
 				Statements: []string{
-					fmt.Sprintf("set(resource.attributes[\"%s\"], \"true\") where metric.name != \"\"", metric.SkipEnrichmentAttribute),
+					fmt.Sprintf("set(resource.attributes[\"%s\"], \"true\")", metric.SkipEnrichmentAttribute),
 				},
 				Conditions: makeMetricNameConditionsWithIsMatch(metricsToSkipEnrichment),
 			},
