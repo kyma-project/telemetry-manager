e889a5d01763ab6d7feeacd8c749fd40f9ed3b5d
Rakesh Garimella <rakesh.garimella@sap.com>
2025-06-10 11:13:40 +0200

    fixes and remove migrated e2e tests

--- test/e2e-migrated/metrics/endpint_with_path_validation_test.go
+++ test/e2e-migrated/metrics/endpint_with_path_validation_test.go
@@ -2,19 +2,14 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
-	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
-	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 )
-
-// TODO: Decide if we need this
 
 func TestEndpointAndPath(t *testing.T) {
 	suite.RegisterTestCase(t, suite.LabelMetrics)
@@ -28,8 +23,6 @@ func TestEndpointAndPath(t *testing.T) {
 		WithName("metricpipeline-accept-with-http-and-path").
 		WithOTLPOutput(testutils.OTLPEndpoint("mock-endpoint:4817"), testutils.OTLPEndpointPath("/v1/mock/metrics"), testutils.OTLPProtocol("http")).
 		Build()
-
-	endpointDataKey := fmt.Sprintf("%s_%s", "OTLP_ENDPOINT", kitkyma.MakeEnvVarCompliant("metricpipeline-accept-with-http-and-path"))
 
 	metricPipelineWithHTTPAndWithoutPath := testutils.NewMetricPipelineBuilder().
 		WithName("metricpipeline-accept-with-http-and-no-path").
@@ -46,7 +39,5 @@ func TestEndpointAndPath(t *testing.T) {
 		require.NoError(t, kitk8s.DeleteObjects(context.Background(), resources...)) //nolint:usetesting // Remove ctx from DeleteObjects
 	})
 	Expect(kitk8s.CreateObjects(t.Context(), resources...)).Should(Succeed())
-
-	assert.SecretHasKeyValue(t.Context(), kitkyma.MetricGatewaySecretName, endpointDataKey, "mock-endpoint:4817/v1/mock/metrics")
 
 }
