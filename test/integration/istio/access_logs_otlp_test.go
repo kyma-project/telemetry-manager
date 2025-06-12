8e7de249f543524d89b640430bacbda187375ab0
Hisar Balik <hisar.balik@sap.com>
2025-06-12 11:31:44 +0200

    update

--- test/integration/istio/access_logs_otlp_test.go
+++ test/integration/istio/access_logs_otlp_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
 )
 
-var _ = Describe(suite.ID(), Label("foo"), Ordered, func() {
+var _ = Describe(suite.ID(), Label(suite.LabelIntegration), Ordered, func() {
 	const (
 		sampleAppNs = "istio-permissive-mtls"
 	)
