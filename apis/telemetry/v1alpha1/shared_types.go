a9b92f928815347eac8aee5b87a2f11034d04f0e
Rakesh Garimella <rakesh.garimella@sap.com>
2025-06-24 09:03:56 +0200

    fix namepace in text

--- apis/telemetry/v1alpha1/shared_types.go
+++ apis/telemetry/v1alpha1/shared_types.go
@@ -16,7 +16,7 @@ type SecretKeyRef struct {
 	// The name of the Secret containing the referenced value
 	// +kubebuilder:validation:Required
 	Name string `json:"name,omitempty"`
-	// The name of the Namespace containing the Secret with the referenced value.
+	// The name of the namespace containing the Secret with the referenced value.
 	// +kubebuilder:validation:Required
 	Namespace string `json:"namespace,omitempty"`
 	// The name of the attribute of the Secret holding the referenced value.
