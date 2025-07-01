3bfe7f94be8a7437a3522f87686a287ea51f4452
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-07-01 14:48:33 +0300

    Add MaxPipeline label to max_pipeline e2e test

--- test/e2e-migrated/metrics/multi_pipeline_max_pipeline_test.go
+++ test/e2e-migrated/metrics/multi_pipeline_max_pipeline_test.go
@@ -22,7 +22,7 @@ import (
 )
 
 func TestMultiPipelineMaxPipeline(t *testing.T) {
-	suite.RegisterTestCase(t, suite.LabelMetrics)
+	suite.RegisterTestCase(t, suite.LabelMetrics, suite.LabelMaxPipeline)
 
 	const maxNumberOfMetricPipelines = telemetrycontrollers.MaxPipelineCount
 
