45e9583f69a26bbf7f346910f97586c7418573fd
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-07-02 09:55:50 +0300

    Fix extract labels context issue

--- test/e2e-migrated/metrics/extract_labels_test.go
+++ test/e2e-migrated/metrics/extract_labels_test.go
@@ -81,7 +81,7 @@ func TestExtractLabels(t *testing.T) {
 	t.Cleanup(func() {
 		require.NoError(t, kitk8s.DeleteObjects(context.Background(), resources...)) //nolint:usetesting // Remove ctx from DeleteObjects
 
-		Expect(suite.K8sClient.Get(t.Context(), kitkyma.TelemetryName, &telemetry)).Should(Succeed())
+		Expect(suite.K8sClient.Get(context.Background(), kitkyma.TelemetryName, &telemetry)).Should(Succeed()) //nolint:usetesting // Remove ctx from Get
 		telemetry.Spec.Enrichments = &operatorv1alpha1.EnrichmentSpec{}
 		require.NoError(t, suite.K8sClient.Update(context.Background(), &telemetry)) //nolint:usetesting // Remove ctx from Update
 	})
