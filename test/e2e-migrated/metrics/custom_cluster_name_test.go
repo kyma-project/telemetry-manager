ae12e8202a1a4ad697edc829af271ad903f9e6dc
Jeffrey Limnardy <jeffrey.limnardy@sap.com>
2025-07-15 18:56:21 +0700

    remove contexts from testkit functions

--- test/e2e-migrated/metrics/custom_cluster_name_test.go
+++ test/e2e-migrated/metrics/custom_cluster_name_test.go
@@ -64,17 +64,17 @@ func TestCustomClusterName(t *testing.T) {
 	resources = append(resources, backend.K8sObjects()...)
 
 	t.Cleanup(func() {
-		require.NoError(t, kitk8s.DeleteObjects(context.Background(), resources...)) //nolint:usetesting // Remove ctx from DeleteObjects
+		require.NoError(t, kitk8s.DeleteObjects(resources...))
 
 		Expect(suite.K8sClient.Get(context.Background(), kitkyma.TelemetryName, &telemetry)).Should(Succeed()) //nolint:usetesting // Remove ctx from Get
 		telemetry.Spec.Enrichments.Cluster = &operatorv1alpha1.Cluster{}
 		require.NoError(t, suite.K8sClient.Update(context.Background(), &telemetry)) //nolint:usetesting // Remove ctx from Update
 	})
-	Expect(kitk8s.CreateObjects(t.Context(), resources...)).Should(Succeed())
+	Expect(kitk8s.CreateObjects(t, resources...)).Should(Succeed())
 
 	assert.BackendReachable(t, backend)
-	assert.DeploymentReady(t.Context(), kitkyma.MetricGatewayName)
-	assert.MetricPipelineHealthy(t.Context(), pipelineName)
+	assert.DeploymentReady(t, kitkyma.MetricGatewayName)
+	assert.MetricPipelineHealthy(t, pipelineName)
 
 	assert.BackendDataEventuallyMatches(t, backend,
 		HaveFlatMetrics(ContainElement(
