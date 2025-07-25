ffd73717422d7f8d5384f7a4855dac64c5300456
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-07-25 17:29:27 +0200

    Migrate traces istio integration test

---
+++ test/integration-migrated/istio/traces_istio_routing_test.go
@@ -0,0 +1,203 @@
+package istio
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kyma-project/telemetry-manager/internal/otelcollector/ports"
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/trace"
+	kitbackend "github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/prommetricgen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/periodic"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	"github.com/kyma-project/telemetry-manager/test/testkit/unique"
+)
+
+// TestTracesIstio verifies trace delivery and Istio integration in a scenario with two trace backends and two trace generators (apps), each paired with a dedicated trace pipeline.
+// One backend and one app are deployed with Istio sidecar injection enabled (inside the mesh), while the other backend and app are deployed without Istio (outside the mesh).
+// The test validates that traces are correctly routed to the appropriate backends, Istio sidecar injection is functioning as expected, and only the desired spans are present in the collected traces.
+func TestTracesRoutingIstio(t *testing.T) {
+	suite.RegisterTestCase(t, suite.LabelIstio)
+
+	const (
+		appName          = "app"
+		istiofiedAppName = "app-istiofied"
+		istiofiedAppNs   = permissiveNs
+	)
+
+	var (
+		uniquePrefix       = unique.Prefix()
+		pipeline1Name      = uniquePrefix("1")
+		pipeline2Name      = uniquePrefix("2")
+		backendNs          = uniquePrefix("backend")
+		istiofiedBackendNs = uniquePrefix("backend-istiofied")
+		appNs              = uniquePrefix("app")
+	)
+
+	backend := kitbackend.New(backendNs, kitbackend.SignalTypeTraces)
+	backendExportURL := backend.ExportURL(suite.ProxyClient)
+
+	istiofiedBackend := kitbackend.New(istiofiedBackendNs, kitbackend.SignalTypeTraces)
+	istiofiedBackendExportURL := istiofiedBackend.ExportURL(suite.ProxyClient)
+
+	tracePipeline := testutils.NewTracePipelineBuilder().
+		WithName(pipeline1Name).
+		WithOTLPOutput(testutils.OTLPEndpoint(backend.Endpoint())).
+		Build()
+
+	istioTracePipeline := testutils.NewTracePipelineBuilder().
+		WithName(pipeline2Name).
+		WithOTLPOutput(testutils.OTLPEndpoint(istiofiedBackend.Endpoint())).
+		Build()
+
+	traceGatewayExternalService := kitk8s.NewService("telemetry-otlp-traces-external", kitkyma.SystemNamespaceName).
+		WithPort("grpc-otlp", ports.OTLPGRPC).
+		WithPort("http-metrics", ports.Metrics)
+	metricServiceURL := suite.ProxyClient.ProxyURLForService(kitkyma.SystemNamespaceName, "telemetry-otlp-traces-external", "metrics", ports.Metrics)
+
+	app := prommetricgen.New(appNs, prommetricgen.WithName(appName))
+	appURL := app.PodURL(suite.ProxyClient)
+
+	istiofiedApp := prommetricgen.New(istiofiedAppNs, prommetricgen.WithName(istiofiedAppName))
+	istiofiedAppURL := istiofiedApp.PodURL(suite.ProxyClient)
+
+	resources := []client.Object{
+		kitk8s.NewNamespace(backendNs).K8sObject(),
+		kitk8s.NewNamespace(istiofiedBackendNs, kitk8s.WithIstioInjection()).K8sObject(),
+		kitk8s.NewNamespace(appNs).K8sObject(),
+		&istioTracePipeline,
+		&tracePipeline,
+		traceGatewayExternalService.K8sObject(kitk8s.WithLabel("app.kubernetes.io/name", "telemetry-trace-gateway")),
+		app.Pod().K8sObject(),
+		istiofiedApp.Pod().K8sObject(),
+	}
+	resources = append(resources, backend.K8sObjects()...)
+	resources = append(resources, istiofiedBackend.K8sObjects()...)
+
+	t.Cleanup(func() {
+		Expect(kitk8s.DeleteObjects(resources...)).To(Succeed())
+	})
+	Expect(kitk8s.CreateObjects(t, resources...)).To(Succeed())
+
+	assert.BackendReachable(t, backend)
+	assert.BackendReachable(t, istiofiedBackend)
+
+	verifyAppIsRunning(t, istiofiedAppNs, map[string]string{"app.kubernetes.io/name": "metric-producer"})
+	verifySidecarPresent(t, istiofiedAppNs, map[string]string{"app.kubernetes.io/name": "metric-producer"})
+	verifyAppIsRunning(t, appNs, map[string]string{"app.kubernetes.io/name": "metric-producer"})
+	assert.DeploymentReady(t, kitkyma.TraceGatewayName)
+	assert.TracePipelineHealthy(t, pipeline1Name)
+	assert.TracePipelineHealthy(t, pipeline2Name)
+
+	Eventually(func(g Gomega) {
+		resp, err := suite.ProxyClient.Get(metricServiceURL)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+		defer resp.Body.Close()
+	}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(Succeed())
+
+	for _, podURLs := range []string{appURL, istiofiedAppURL} {
+		Eventually(func(g Gomega) {
+			resp, err := suite.ProxyClient.Get(podURLs)
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+			defer resp.Body.Close()
+		}, periodic.EventuallyTimeout, periodic.DefaultInterval).Should(Succeed())
+	}
+
+	verifyIstioSpans(t, backendExportURL, istiofiedAppNs)
+	verifyIstioSpans(t, istiofiedBackendExportURL, istiofiedAppNs)
+	verifyCustomAppSpans(t, backendExportURL, istiofiedAppName, istiofiedAppNs)
+	verifyCustomAppSpans(t, istiofiedBackendExportURL, istiofiedAppName, istiofiedAppNs)
+	verifyCustomAppSpans(t, backendExportURL, appName, appNs)
+	verifyCustomAppSpans(t, istiofiedBackendExportURL, appName, appNs)
+	verifyNoIstioNoiseSpans(t, backendExportURL)
+	verifyNoIstioNoiseSpans(t, istiofiedBackendExportURL)
+}
+
+func verifySidecarPresent(t testkit.T, namespace string, labelSelector map[string]string) {
+	t.Helper()
+
+	Eventually(func(g Gomega) {
+		listOptions := client.ListOptions{
+			LabelSelector: labels.SelectorFromSet(labelSelector),
+			Namespace:     namespace,
+		}
+
+		hasIstioSidecar, err := assert.PodsHaveContainer(t, listOptions, "istio-proxy")
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(hasIstioSidecar).To(BeTrueBecause("Istio sidecar not present"))
+	}, periodic.EventuallyTimeout*2, periodic.DefaultInterval).Should(Succeed())
+}
+
+func verifyAppIsRunning(t testkit.T, namespace string, labelSelector map[string]string) {
+	t.Helper()
+
+	listOptions := client.ListOptions{
+		LabelSelector: labels.SelectorFromSet(labelSelector),
+		Namespace:     namespace,
+	}
+
+	assert.PodsReady(t, listOptions)
+}
+
+func verifyIstioSpans(t testkit.T, backendURL, namespace string) {
+	t.Helper()
+
+	Eventually(func(g Gomega) {
+		resp, err := suite.ProxyClient.Get(backendURL)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+		defer resp.Body.Close()
+
+		g.Expect(resp).To(HaveHTTPBody(HaveFlatTraces(ContainElement(SatisfyAll(
+			// Identify istio-proxy traces by component=proxy attribute
+			HaveSpanAttributes(HaveKeyWithValue("component", "proxy")),
+			HaveSpanAttributes(HaveKeyWithValue("istio.namespace", namespace)),
+		)))))
+	}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+}
+
+func verifyNoIstioNoiseSpans(t testkit.T, backendURL string) {
+	t.Helper()
+
+	Eventually(func(g Gomega) {
+		resp, err := suite.ProxyClient.Get(backendURL)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+		defer resp.Body.Close()
+
+		g.Expect(resp).NotTo(HaveHTTPBody(HaveFlatTraces(ContainElement(SatisfyAll(
+			// Identify istio-proxy traces by component=proxy attribute
+			HaveSpanAttributes(HaveKeyWithValue("component", "proxy")),
+			// All calls to telemetry-otlp-traces should be dropped
+			HaveSpanAttributes(HaveKeyWithValue("http.url", "http://telemetry-otlp-traces.kyma-system.svc.cluster.local:4318/v1/traces")),
+		)))))
+	}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+}
+
+func verifyCustomAppSpans(t testkit.T, backendURL, name, namespace string) {
+	t.Helper()
+
+	Eventually(func(g Gomega) {
+		resp, err := suite.ProxyClient.Get(backendURL)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+		g.Expect(resp).To(HaveHTTPBody(HaveFlatTraces(ContainElement(SatisfyAll(
+			// Identify sample app by serviceName attribute
+			HaveResourceAttributes(HaveKeyWithValue("service.name", "metric-producer")),
+			HaveResourceAttributes(HaveKeyWithValue("k8s.pod.name", name)),
+			HaveResourceAttributes(HaveKeyWithValue("k8s.namespace.name", namespace)),
+		)))))
+		defer resp.Body.Close()
+	}, periodic.TelemetryEventuallyTimeout, periodic.TelemetryInterval).Should(Succeed())
+}
