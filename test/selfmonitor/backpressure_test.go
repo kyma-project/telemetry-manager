ce24bd56ed300d59bd81bec8792a90c3972d2da8
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-07-28 10:02:21 +0200

    Create TODO framework for migrated selfmonitor tests

---
+++ test/selfmonitor/backpressure_test.go
@@ -0,0 +1,4 @@
+package selfmonitor
+
+// TODO: Implement a table-driven test for the backpressure self-monitoring scenario, covering all telemetry data types.
+// (this scenario requires istio as well)
---
+++ test/selfmonitor/healthy_test.go
@@ -0,0 +1,3 @@
+package selfmonitor
+
+// TODO: Implement a table-driven test for the healthy self-monitoring scenario, covering all telemetry data types.
