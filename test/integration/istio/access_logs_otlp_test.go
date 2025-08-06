770394e8a6d05d443b28485bb74dee96e6de6d7e
Rakesh Garimella <Rakesh.garimella@sap.com>
2025-08-06 22:56:27 +0200

    chore: remove ginkgo dependency (#2356)
    Co-authored-by: Stanislav Khalash <stanislav.khalash@sap.com>

--- test/integration/istio/access_logs_otlp_test.go
+++ test/integration/istio/access_logs_otlp_test.go
@@ -26,7 +26,7 @@ import (
 )
 
 func TestAccessLogsOTLP(t *testing.T) {
-	suite.RegisterTestCase(t, suite.LabelIstio)
+	suite.RegisterTestCase(t, suite.LabelGardener, suite.LabelIstio)
 
 	var (
 		uniquePrefix = unique.Prefix()
