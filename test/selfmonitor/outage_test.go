46a7448ea2e49f12650832ebf16fd03a5532430c
Mostafa Shorim <mostafa.shorim@sap.com>
2025-08-06 16:12:49 +0200

    use replicase instead of workers in selfmonitoring tests

--- test/selfmonitor/outage_test.go
+++ test/selfmonitor/outage_test.go
@@ -39,10 +39,7 @@ func TestOutage(t *testing.T) {
 				return &p
 			},
 			generator: func(ns string) *appsv1.Deployment {
-				return stdoutloggen.NewDeployment(ns,
-					stdoutloggen.WithRate(800),
-					stdoutloggen.WithWorkers(5),
-				).K8sObject()
+				return stdoutloggen.NewDeployment(ns, stdoutloggen.WithRate(800)).WithReplicas(5).K8sObject()
 			},
 			assert: func(t *testing.T) {
 				assert.DeploymentReady(t, kitkyma.LogGatewayName)
@@ -105,10 +102,7 @@ func TestOutage(t *testing.T) {
 				return &p
 			},
 			generator: func(ns string) *appsv1.Deployment {
-				return stdoutloggen.NewDeployment(ns,
-					stdoutloggen.WithRate(800),
-					stdoutloggen.WithWorkers(5),
-				).K8sObject()
+				return stdoutloggen.NewDeployment(ns, stdoutloggen.WithRate(800)).WithReplicas(5).K8sObject()
 			},
 			assert: func(t *testing.T) {
 				assert.DaemonSetReady(t, kitkyma.FluentBitDaemonSetName)
