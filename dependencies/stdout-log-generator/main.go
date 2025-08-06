d1ebff51ef54e00be56c60d4d03a7e544012e719
Mostafa Shorim <mostafa.shorim@sap.com>
2025-08-06 17:13:42 +0200

    revert changes to stdoutloggen

--- dependencies/stdout-log-generator/main.go
+++ dependencies/stdout-log-generator/main.go
@@ -6,9 +6,8 @@ import (
 	"fmt"
 	"log"
 	"maps"
-	"math/rand"
 	"net/http"
-	"os"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -17,58 +16,27 @@ import (
 	"golang.org/x/time/rate"
 )
 
-type logFormat string
-
-const (
-	defaultByteSize           = 1 << 11 // 2 KiB = 2 * 2^10 = 2^11
-	jsonFormat      logFormat = "json"
-	plaintextFormat logFormat = "plaintext"
+var logsGenerated = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "logs_generated_total",
+		Help: "Total number of logs generated",
+	},
 )
 
-var (
-	logsGeneratedTotal = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "logs_generated_total",
-			Help: "Total number of logs generated",
-		},
-	)
-
-	logsGeneratedRate = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: "logs_generated_rate",
-			Help: "Actual rate of logs generated per second",
-		},
-	)
-
-	allowedFormats = map[logFormat]struct{}{
-		jsonFormat:      {},
-		plaintextFormat: {},
-	}
+const (
+	defaultByteSize = 1 << 11
 )
 
 func main() {
-	startTime := time.Now()
-
-	formatFlag := pflag.StringP("format", "", string(jsonFormat), fmt.Sprintf("Log format (%s or %s)", jsonFormat, plaintextFormat))
-	bytesFlag := pflag.IntP("bytes", "b", defaultByteSize, "Size of each log in bytes")
-	logsPerSecondFlag := pflag.IntP("rate", "r", 1, "Approximately how many logs per second each worker should generate. Zero means no throttling")
-	fieldsFlag := pflag.StringToStringP("fields", "f", map[string]string{},
-		fmt.Sprintf(`Custom fields in key=value format (comma-separated or repeated). These fields will be included in each %s log record (e.g. --fields key1=value1,key2=value2 or --fields key1=value1 --fields key2=value2). This flag is only relevant when the format is %s.`, jsonFormat, jsonFormat),
-	)
-	textFlag := pflag.StringP("text", "t", "",
-		fmt.Sprintf(`Custom text to be logged in each %s log. This flag will be ignored if the "bytes" flag is provided in which random text will be logged having the size provieded by the "bytes" flag. This flag is only relevant when the format is %s.`, plaintextFormat, plaintextFormat),
-	)
+	bytes := pflag.IntP("bytes", "b", defaultByteSize, "Size of each log in bytes")
+	logsPerSecond := pflag.IntP("rate", "r", 1, "Approximately how many logs per second each worker should generate. Zero means no throttling")
+	workers := pflag.IntP("workers", "w", 1, "Number of workers (goroutines) to run")
+	fields := pflag.StringToStringP("fields", "f", map[string]string{}, "Custom fields in key=value format (comma-separated or repeated). These fields will be included in each log record (e.g. --fields key1=value1,key2=value2 or --fields key1=value1 --fields key2=value2)")
 
 	pflag.Parse()
 
-	// Validate the format flag
-	format := logFormat(*formatFlag)
-	if _, ok := allowedFormats[format]; !ok {
-		log.Fatalf("Invalid format: %s. Allowed values are: %s, %s", format, jsonFormat, plaintextFormat)
-	}
-
-	// Register the metrics
-	prometheus.MustRegister(logsGeneratedTotal, logsGeneratedRate)
+	// Register the metric
+	prometheus.MustRegister(logsGenerated)
 
 	// Expose /metrics endpoint
 	go func() {
@@ -81,111 +49,62 @@ func main() {
 		}
 	}()
 
-	limitPerSecond := rate.Limit(*logsPerSecondFlag)
-	if *logsPerSecondFlag == 0 {
+	limitPerSecond := rate.Limit(*logsPerSecond)
+	if *logsPerSecond == 0 {
 		// No limit if logsPerSec is 0
 		limitPerSecond = rate.Inf
 	}
 
-	// Start generation of logs
-	switch format {
-	case jsonFormat:
-		generateJSONLogs(startTime, *bytesFlag, limitPerSecond, *fieldsFlag)
-	case plaintextFormat:
-		generatePlaintextLogs(startTime, *bytesFlag, limitPerSecond, *textFlag)
-	default:
-		log.Fatalf("Unexpected log format: %s", format)
+	// Start workers
+	for range *workers {
+		go generateLogs(*bytes, limitPerSecond, *fields)
 	}
 
 	select {}
 }
 
-func generateJSONLogs(startTime time.Time, logSize int, limitPerSecond rate.Limit, fields map[string]string) {
-	logsCounter := 0
+func generateLogs(logSize int, limitPerSecond rate.Limit, fields map[string]string) {
 	limiter := rate.NewLimiter(limitPerSecond, 1)
 
-	logRecord := map[string]string{"padding": ""}
-	maps.Copy(logRecord, fields)
-
-	JSONLog, err := json.Marshal(logRecord)
-	if err != nil {
-		log.Fatalf("Error encoding log record to JSON: %v\n", err)
-	}
-
-	length := len(JSONLog)
-
-	// Check if the size of the JSON log is already larger than the target size
-	const quotes = 2 // number of quotes around every string in JSON
-
-	overhead := length - quotes // subtract the existing quotes for the current empty string in the padding field
-
-	paddingLen := logSize - overhead - quotes // number of characters generated in the padding should exclude the quotes which will be added for the padding field
-	if paddingLen < 0 {
-		log.Fatalf("The size of the JSON log with custom fields is larger than the requested log size\n")
-	}
-
 	for {
-		// Pad with random characters until the JSON log reaches the target size
-		logRecord["padding"] = offsetString(paddingLen)
+		// Create a log record with the provided fields
+		logRecord := make(map[string]string)
+		maps.Copy(logRecord, fields)
 
-		// Avoid using json.Marshal() here for faster execution
-		err = json.NewEncoder(os.Stdout).Encode(logRecord)
+		// Add a timestamp and initially an empty body to the log record
+		logRecord["timestamp"] = time.Now().Format(time.RFC3339)
+		logRecord["body"] = ""
+
+		logJson, err := json.Marshal(logRecord)
 		if err != nil {
-			log.Fatalf("Error encoding log record to JSON: %v\n", err)
+			log.Fatalf("Error marshaling log record: %v\n", err)
 		}
 
-		logsCounter++
+		// Check if the size of the JSON log is already larger than the target size
+		overhead := len(logJson) - len(`""`) // subtract the empty body quotes in logJson
 
-		logsGeneratedTotal.Inc()
-		logsGeneratedRate.Set(float64(logsCounter) / time.Since(startTime).Seconds())
+		const quotes = 2 // number of quotes around every string in json
+
+		bodyLen := logSize - overhead - quotes
+		if bodyLen < 0 {
+			log.Fatalf("The size of the JSON log with custom fields and a timestamp is larger than the requested log size\n")
+		}
+
+		// Pad the body until the JSON log reaches the target size
+		logRecord["body"] = strings.Repeat("a", bodyLen)
+
+		logJson, err = json.Marshal(logRecord)
+		if err != nil {
+			log.Fatalf("Error marshaling log record: %v\n", err)
+		}
+
+		//nolint:forbidigo // actual printing of the prepared "log"
+		fmt.Println(string(logJson))
+
+		logsGenerated.Inc()
 
 		if err := limiter.Wait(context.Background()); err != nil {
 			log.Printf("Error waiting for rate limiter: %v\n", err)
 		}
 	}
-}
-
-func generatePlaintextLogs(startTime time.Time, logSize int, limitPerSecond rate.Limit, customText string) string {
-	logsCounter := 0
-	limiter := rate.NewLimiter(limitPerSecond, 1)
-
-	for {
-		var plaintextLog string
-
-		if customText != "" {
-			// Use the custom text if provided
-			plaintextLog = customText
-		} else {
-			// Generate random text if no custom text is provided
-			plaintextLog = offsetString(logSize)
-		}
-
-		//nolint:forbidigo // actual printing of the prepared plaintextLog
-		fmt.Println(plaintextLog)
-
-		logsCounter++
-
-		logsGeneratedTotal.Inc()
-		logsGeneratedRate.Set(float64(logsCounter) / time.Since(startTime).Seconds())
-
-		if err := limiter.Wait(context.Background()); err != nil {
-			log.Printf("Error waiting for rate limiter: %v\n", err)
-		}
-	}
-}
-
-func offsetString(length int) string {
-	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	// Random starting offset
-	// Avoid getting a random character in each loop iteration for faster execution
-	//nolint:gosec //no need for cryptographic security here
-	start := rand.Intn(len(letters))
-
-	b := make([]byte, length)
-	for i := range length {
-		pos := (start + start*i) % len(letters)
-		b[i] = letters[pos]
-	}
-
-	return string(b)
 }
