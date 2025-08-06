99e8784bd8e0ee24d9360d3fa049655f2a6284c7
Mostafa Shorim <mostafa.shorim@sap.com>
2025-08-06 17:15:40 +0200

    use image from PR-2357 for stdoutloggen mock

--- test/testkit/mocks/stdoutloggen/stdoutloggen.go
+++ test/testkit/mocks/stdoutloggen/stdoutloggen.go
@@ -12,7 +12,7 @@ import (
 const (
 	DefaultName          = "stdoutloggen"
 	DefaultContainerName = "stdoutloggen"
-	DefaultImageName     = "europe-docker.pkg.dev/kyma-project/dev/stdout-log-generator:PR-2344"
+	DefaultImageName     = "europe-docker.pkg.dev/kyma-project/dev/stdout-log-generator:PR-2357"
 )
 
 type Option func(*corev1.PodSpec)
