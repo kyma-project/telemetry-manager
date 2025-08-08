40f58d300da2f2d634c94e35c48f6a9b207fd79a
Stanislav Khalash <stanislav.khalash@sap.com>
2025-08-08 09:50:48 +0200

    chore: harmonize OTel Collector config builders (#2367)

--- internal/otelcollector/config/metric/gateway/connectors.go
+++ internal/otelcollector/config/metric/gateway/connectors.go
@@ -6,9 +6,9 @@ import (
 	"github.com/kyma-project/telemetry-manager/internal/otelcollector/config/metric"
 )
 
-func makeRoutingConnectorConfig(pipelineName string) RoutingConnector {
-	attributesEnrichmentPipelineID := formatAttributesEnrichmentPipelineID(pipelineName)
-	outputPipelineID := formatOutputPipelineID(pipelineName)
+func routingConnectorConfig(pipelineName string) RoutingConnector {
+	attributesEnrichmentPipelineID := formatMetricEnrichmentPipelineID(pipelineName)
+	outputPipelineID := formatMetricOutputPipelineID(pipelineName)
 
 	return RoutingConnector{
 		DefaultPipelines: []string{attributesEnrichmentPipelineID},
