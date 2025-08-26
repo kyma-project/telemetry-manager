aeb375a3929a661b0e94407b4396fc0563b63af1
Stanislav Khalash <stanislav.khalash@sap.com>
2025-08-26 15:36:29 +0200

    chore: refactor Metric Gateway config builder (#2438)

---
+++ internal/otelcollector/config/metricgateway/types.go
@@ -0,0 +1,26 @@
+package metricgateway
+
+type KymaStatsReceiver struct {
+	AuthType           string      `yaml:"auth_type"`
+	CollectionInterval string      `yaml:"collection_interval"`
+	Resources          []ModuleGVR `yaml:"resources"`
+	K8sLeaderElector   string      `yaml:"k8s_leader_elector"`
+}
+
+type MetricConfig struct {
+	Enabled bool `yaml:"enabled"`
+}
+
+type ModuleGVR struct {
+	Group    string `yaml:"group"`
+	Version  string `yaml:"version"`
+	Resource string `yaml:"resource"`
+}
+
+type FilterProcessor struct {
+	Metrics FilterProcessorMetrics `yaml:"metrics"`
+}
+
+type FilterProcessorMetrics struct {
+	Metric []string `yaml:"metric,omitempty"`
+}
