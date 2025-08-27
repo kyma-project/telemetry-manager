0cfe97eec3d94be193558df4d73e0f6ec1995550
Rakesh Garimella <Rakesh.garimella@sap.com>
2025-08-27 13:21:41 +0530

    chore: add e2e tests for transform processor (#2432)
    Co-authored-by: Hisar Balik <hisar.balik@sap.com>

---
+++ test/e2e/metrics/transform_test_infer_context_test.go
@@ -0,0 +1,66 @@
+package metrics
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
+	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
+	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
+	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/metric"
+	kitbackend "github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
+	"github.com/kyma-project/telemetry-manager/test/testkit/mocks/telemetrygen"
+	"github.com/kyma-project/telemetry-manager/test/testkit/suite"
+	"github.com/kyma-project/telemetry-manager/test/testkit/unique"
+)
+
+func TestTransformInfer_InferContext(t *testing.T) {
+	suite.RegisterTestCase(t, suite.LabelExperimental)
+
+	var (
+		uniquePrefix = unique.Prefix()
+		pipelineName = uniquePrefix()
+		backendNs    = uniquePrefix("backend")
+		genNs        = uniquePrefix("gen")
+	)
+
+	backend := kitbackend.New(backendNs, kitbackend.SignalTypeMetrics)
+	pipeline := testutils.NewMetricPipelineBuilder().
+		WithName(pipelineName).
+		WithTransform(telemetryv1alpha1.TransformSpec{
+			Statements: []string{"set(resource.attributes[\"test\"], \"passed\")",
+				"set(metric.description, \"test passed\")",
+			},
+		}).
+		WithOTLPOutput(testutils.OTLPEndpoint(backend.Endpoint())).
+		Build()
+
+	resources := []client.Object{
+		kitk8s.NewNamespace(backendNs).K8sObject(),
+		kitk8s.NewNamespace(genNs).K8sObject(),
+		&pipeline,
+		telemetrygen.NewPod(genNs, telemetrygen.SignalTypeMetrics).K8sObject(),
+	}
+	resources = append(resources, backend.K8sObjects()...)
+
+	t.Cleanup(func() {
+		Expect(kitk8s.DeleteObjects(resources...)).To(Succeed())
+	})
+	Expect(kitk8s.CreateObjects(t, resources...)).To(Succeed())
+
+	assert.BackendReachable(t, backend)
+	assert.DeploymentReady(t, kitkyma.MetricGatewayName)
+	assert.MetricPipelineHealthy(t, pipelineName)
+	assert.MetricsFromNamespaceDelivered(t, backend, genNs, telemetrygen.MetricNames)
+
+	assert.BackendDataConsistentlyMatches(t, backend,
+		HaveFlatMetrics(ContainElement(SatisfyAll(
+			HaveResourceAttributes(HaveKeyWithValue("test", "passed")),
+			HaveDescription(Equal("test passed")),
+		))),
+	)
+}
