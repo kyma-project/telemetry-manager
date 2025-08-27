d8ab2e57b604dfbed394f7042bcc40f2a6a8daee
Stanislav Khalash <stanislav.khalash@sap.com>
2025-08-27 14:03:16 +0200

    Merge processors.go into config_builder.go

--- internal/otelcollector/config/metricagent/config_builder.go
+++ internal/otelcollector/config/metricagent/config_builder.go
@@ -119,7 +119,7 @@ func (b *Builder) Build(ctx context.Context, pipelines []telemetryv1alpha1.Metri
 
 	if inputs.istio {
 		if err := b.addServicePipeline(ctx, "metrics/istio",
-			b.addPrometheusIstioReceiver(opts, inputs.envoy),
+			b.addPrometheusIstioReceiver(inputs.envoy),
 			b.addMemoryLimiterProcessor(),
 			b.addIstioNoiseFilterProcessor(),
 			b.addDeleteServiceNameProcessor(),
@@ -203,7 +203,7 @@ func (b *Builder) addPrometheusAppServicesReceiver(opts BuildOptions) buildCompo
 	)
 }
 
-func (b *Builder) addPrometheusIstioReceiver(opts BuildOptions, envoyMetricsEnabled bool) buildComponentFunc {
+func (b *Builder) addPrometheusIstioReceiver(envoyMetricsEnabled bool) buildComponentFunc {
 	return b.addReceiver(
 		staticComponentID("prometheus/istio"),
 		func(mp *telemetryv1alpha1.MetricPipeline) any {
@@ -490,4 +490,70 @@ func shouldEnableEnvoyMetricsScraping(pipelines []telemetryv1alpha1.MetricPipeli
 	}
 
 	return false
+}
+
+// Processor configuration functions (merged from processors.go)
+
+func deleteServiceNameProcessorConfig() *common.ResourceProcessor {
+	return &common.ResourceProcessor{
+		Attributes: []common.AttributeAction{
+			{
+				Action: "delete",
+				Key:    "service.name",
+			},
+		},
+	}
+}
+
+func insertSkipEnrichmentAttributeProcessorConfig() *common.TransformProcessor {
+	metricsToSkipEnrichment := []string{
+		"node",
+		"statefulset",
+		"daemonset",
+		"deployment",
+		"job",
+	}
+
+	return common.MetricTransformProcessorConfig([]common.TransformProcessorStatements{{
+		Conditions: metricNameConditionsWithIsMatch(metricsToSkipEnrichment),
+		Statements: []string{fmt.Sprintf("set(resource.attributes[\"%s\"], \"true\")", common.SkipEnrichmentAttribute)},
+	}})
+}
+
+func dropNonPVCVolumesMetricsProcessorConfig() *FilterProcessor {
+	return &FilterProcessor{
+		Metrics: FilterProcessorMetrics{
+			Metric: []string{
+				common.JoinWithAnd(
+					// identify volume metrics by checking existence of "k8s.volume.name" resource attribute
+					common.ResourceAttributeIsNotNil("k8s.volume.name"),
+					common.ResourceAttributeNotEquals("k8s.volume.type", "persistentVolumeClaim"),
+				),
+			},
+		},
+	}
+}
+
+func dropVirtualNetworkInterfacesProcessorConfig() *FilterProcessor {
+	return &FilterProcessor{
+		Metrics: FilterProcessorMetrics{
+			Datapoint: []string{
+				common.JoinWithAnd(
+					common.IsMatch("metric.name", "^k8s.node.network.*"),
+					common.Not(common.IsMatch("attributes[\"interface\"]", "^(eth|en).*")),
+				),
+			},
+		},
+	}
+}
+
+func metricNameConditionsWithIsMatch(metrics []string) []string {
+	var conditions []string
+
+	for _, m := range metrics {
+		condition := common.IsMatch("metric.name", fmt.Sprintf("^k8s.%s.*", m))
+		conditions = append(conditions, condition)
+	}
+
+	return conditions
 }
