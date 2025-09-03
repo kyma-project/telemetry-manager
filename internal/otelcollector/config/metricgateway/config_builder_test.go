fbe5f03d75ff6ba5e73f21f2e382f70c68e4cf71
Mostafa Shorim <mostafa.shorim@sap.com>
2025-09-03 17:58:05 +0200

    add unit test for pipeline using http protocol with custom 'Path' field in metric gateway

--- internal/otelcollector/config/metricgateway/config_builder_test.go
+++ internal/otelcollector/config/metricgateway/config_builder_test.go
@@ -34,6 +34,18 @@ func TestMakeConfig(t *testing.T) {
 			},
 		},
 		{
+			name:           "pipeline using http protocol with custom 'Path' field",
+			goldenFileName: "http-protocol-with-custom-path.yaml",
+			pipelines: []telemetryv1alpha1.MetricPipeline{
+				testutils.NewMetricPipelineBuilder().
+					WithName("test").
+					WithOTLPOutput(
+						testutils.OTLPProtocol("http"),
+						testutils.OTLPEndpointPath("v2/otlp/v1/metrics"),
+					).Build(),
+			},
+		},
+		{
 			name:           "complex pipeline with comprehensive configuration",
 			goldenFileName: "setup-comprehensive.yaml",
 			pipelines: []telemetryv1alpha1.MetricPipeline{
