1cb315cac7c02aad2ede3fdf57b318017f6f3bef
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-10-30 19:41:55 +0100

    fix variable name

--- hack/await_image.sh
+++ hack/await_image.sh
@@ -8,8 +8,7 @@ set -o pipefail # prevents errors in a pipeline from being masked
 
 # Expected variables:
 #             IMAGE_REPO - binary image repository
-#             GITHUB_TOKEN - github token
-#             TRIGGER - event which triggered the workflow, for PRs it is the commit SHA, for push events it is the GITHUB_REF
+#             IMAGE_TAG - event which triggered the workflow, for PRs it is the commit SHA, for push events it is the GITHUB_REF
 #             QUERY_INTERVAL - time to wait between queries in seconds
 
 PROTOCOL=docker://
@@ -18,13 +17,13 @@ PROTOCOL=docker://
 TIMEOUT=900
 START_TIME=$SECONDS
 
-echo "Waiting for binary image: ${IMAGE_REPO}:${TRIGGER}"
+echo "Waiting for binary image: ${IMAGE_REPO}:${IMAGE_TAG}"
 until $(skopeo list-tags ${PROTOCOL}${IMAGE_REPO} | jq '.Tags|any(. == env.TRIGGER)'); do
   if (( SECONDS - START_TIME > TIMEOUT )); then
-    echo "Timeout reached: ${IMAGE_REPO}:${TRIGGER} not found within $(( TIMEOUT/60 )) minutes"
+    echo "Timeout reached: ${IMAGE_REPO}:${IMAGE_TAG} not found within $(( TIMEOUT/60 )) minutes"
     exit 1
   fi
   sleep "$QUERY_INTERVAL"
 done
 
-echo "Binary image: ${IMAGE_REPO}:${TRIGGER} available"
+echo "Binary image: ${IMAGE_REPO}:${IMAGE_TAG} available"
