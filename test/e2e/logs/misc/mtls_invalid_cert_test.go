02e10c7dbd01ab48b93c9917e87bd09337f3e915
jeffreylimnardy <jeffrey.limnardy@sap.com>
2025-10-30 13:52:47 +0100

    adjust other test files

--- test/e2e/logs/misc/mtls_invalid_cert_test.go
+++ test/e2e/logs/misc/mtls_invalid_cert_test.go
@@ -1,4 +1,4 @@
-package shared
+package misc
 
 import (
 	"testing"
@@ -8,7 +8,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	operatorv1alpha1 "github.com/kyma-project/telemetry-manager/apis/operator/v1alpha1"
-	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
 	"github.com/kyma-project/telemetry-manager/internal/conditions"
 	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
 	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
@@ -18,81 +17,63 @@ import (
 	"github.com/kyma-project/telemetry-manager/test/testkit/unique"
 )
 
-func TestMTLSInvalidCA_OTel(t *testing.T) {
-	tests := []struct {
-		label string
-		input telemetryv1alpha1.LogPipelineInput
-	}{
-		{
-			label: suite.LabelLogAgent,
-			input: testutils.BuildLogPipelineApplicationInput(),
-		},
-		{
-			label: suite.LabelLogGateway,
-			input: testutils.BuildLogPipelineOTLPInput(),
-		},
+func TestMTLSInvalidCert_OTel(t *testing.T) {
+	suite.RegisterTestCase(t, suite.LabelLogsMisc)
+
+	var (
+		uniquePrefix = unique.Prefix()
+		pipelineName = uniquePrefix()
+		backendNs    = uniquePrefix("backend")
+	)
+
+	invalidServerCerts, invalidClientCerts, err := testutils.NewCertBuilder(kitbackend.DefaultName, backendNs).
+		WithInvalidClientCert().
+		Build()
+	Expect(err).ToNot(HaveOccurred())
+
+	backend := kitbackend.New(backendNs, kitbackend.SignalTypeLogsOTel, kitbackend.WithTLS(*invalidServerCerts))
+
+	pipeline := testutils.NewLogPipelineBuilder().
+		WithName(pipelineName).
+		WithOTLPOutput(
+			testutils.OTLPEndpoint(backend.Endpoint()),
+			testutils.OTLPClientTLSFromString(
+				invalidClientCerts.CaCertPem.String(),
+				invalidClientCerts.ClientCertPem.String(),
+				invalidClientCerts.ClientKeyPem.String(),
+			)).
+		Build()
+
+	resources := []client.Object{
+		&pipeline,
 	}
-	for _, tc := range tests {
-		t.Run(tc.label, func(t *testing.T) {
-			suite.RegisterTestCase(t, tc.label)
 
-			var (
-				uniquePrefix = unique.Prefix(tc.label)
-				pipelineName = uniquePrefix()
-				backendNs    = uniquePrefix("backend")
-			)
+	t.Cleanup(func() {
+		Expect(kitk8s.DeleteObjects(resources...)).To(Succeed())
+	})
+	Expect(kitk8s.CreateObjects(t, resources...)).To(Succeed())
 
-			invalidServerCerts, invalidClientCerts, err := testutils.NewCertBuilder(kitbackend.DefaultName, backendNs).
-				WithInvalidCA().
-				Build()
-			Expect(err).ToNot(HaveOccurred())
+	assert.LogPipelineHasCondition(t, pipelineName, metav1.Condition{
+		Type:   conditions.TypeConfigurationGenerated,
+		Status: metav1.ConditionFalse,
+		Reason: conditions.ReasonTLSConfigurationInvalid,
+	})
 
-			backend := kitbackend.New(backendNs, kitbackend.SignalTypeLogsOTel, kitbackend.WithTLS(*invalidServerCerts))
+	assert.LogPipelineHasCondition(t, pipelineName, metav1.Condition{
+		Type:   conditions.TypeFlowHealthy,
+		Status: metav1.ConditionFalse,
+		Reason: conditions.ReasonSelfMonConfigNotGenerated,
+	})
 
-			pipeline := testutils.NewLogPipelineBuilder().
-				WithName(pipelineName).
-				WithInput(tc.input).
-				WithOTLPOutput(
-					testutils.OTLPEndpoint(backend.Endpoint()),
-					testutils.OTLPClientTLSFromString(
-						invalidClientCerts.CaCertPem.String(),
-						invalidClientCerts.ClientCertPem.String(),
-						invalidClientCerts.ClientKeyPem.String(),
-					)).
-				Build()
-
-			resources := []client.Object{
-				&pipeline,
-			}
-
-			t.Cleanup(func() {
-				Expect(kitk8s.DeleteObjects(resources...)).To(Succeed())
-			})
-			Expect(kitk8s.CreateObjects(t, resources...)).To(Succeed())
-
-			assert.LogPipelineHasCondition(t, pipelineName, metav1.Condition{
-				Type:   conditions.TypeConfigurationGenerated,
-				Status: metav1.ConditionFalse,
-				Reason: conditions.ReasonTLSConfigurationInvalid,
-			})
-
-			assert.LogPipelineHasCondition(t, pipelineName, metav1.Condition{
-				Type:   conditions.TypeFlowHealthy,
-				Status: metav1.ConditionFalse,
-				Reason: conditions.ReasonSelfMonConfigNotGenerated,
-			})
-
-			assert.TelemetryHasState(t, operatorv1alpha1.StateWarning)
-			assert.TelemetryHasCondition(t, suite.K8sClient, metav1.Condition{
-				Type:   conditions.TypeLogComponentsHealthy,
-				Status: metav1.ConditionFalse,
-				Reason: conditions.ReasonTLSConfigurationInvalid,
-			})
-		})
-	}
+	assert.TelemetryHasState(t, operatorv1alpha1.StateWarning)
+	assert.TelemetryHasCondition(t, suite.K8sClient, metav1.Condition{
+		Type:   conditions.TypeLogComponentsHealthy,
+		Status: metav1.ConditionFalse,
+		Reason: conditions.ReasonTLSConfigurationInvalid,
+	})
 }
 
-func TestMTLSInvalidCA_FluentBit(t *testing.T) {
+func TestMTLSInvalidCert_FluentBit(t *testing.T) {
 	suite.RegisterTestCase(t, suite.LabelFluentBit)
 
 	var (
@@ -102,7 +83,7 @@ func TestMTLSInvalidCA_FluentBit(t *testing.T) {
 	)
 
 	invalidServerCerts, invalidClientCerts, err := testutils.NewCertBuilder(kitbackend.DefaultName, backendNs).
-		WithInvalidCA().
+		WithInvalidClientCert().
 		Build()
 	Expect(err).ToNot(HaveOccurred())
 
