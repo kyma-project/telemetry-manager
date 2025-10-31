c01ed8a9587b5a6bd7bfbe854062ddfe7d674d3b
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-10-31 10:59:42 +0100

    review comments

--- internal/otelcollector/config/metricagent/config_builder.go
+++ internal/otelcollector/config/metricagent/config_builder.go
@@ -27,6 +27,7 @@ type Builder struct {
 }
 
 type BuildOptions struct {
+	// IstioActive indicates whether Istio is installed in the cluster.
 	IstioActive                 bool
 	IstioCertPath               string
 	InstrumentationScopeVersion string
