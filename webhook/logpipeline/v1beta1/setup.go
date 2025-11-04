4dfd6a9f45ff03807730503c310837f874ab819b
Andreas Thaler <andreas.thaler01@sap.com>
2025-11-04 16:26:26 +0100

    feat: improve Beta API (#2616)
    Co-authored-by: Stanislav Khalash <stanislav.khalash@sap.com>

--- webhook/logpipeline/v1beta1/setup.go
+++ webhook/logpipeline/v1beta1/setup.go
@@ -4,14 +4,17 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	telemetryv1beta1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1beta1"
+	"github.com/kyma-project/telemetry-manager/internal/namespaces"
 )
 
 func SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&telemetryv1beta1.LogPipeline{}).
 		WithDefaulter(&defaulter{
+			ExcludeNamespaces:            namespaces.System(),
 			RuntimeInputEnabled:          true,
 			RuntimeInputKeepOriginalBody: true,
 			DefaultOTLPOutputProtocol:    telemetryv1beta1.OTLPProtocolGRPC,
+			OTLPInputEnabled:             true,
 		}).
 		Complete()
 }
