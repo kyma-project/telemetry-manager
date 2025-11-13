cf0bc59dd06d925581dca05aa46892b8f9449141
Stanislav Khalash <stanislav.khalash@sap.com>
2025-11-13 11:02:27 +0100

    Introduce global config

---
+++ internal/config/global.go
@@ -0,0 +1,48 @@
+package config
+
+type Global struct {
+	namespace      string
+	enableFIPSMode bool
+	version        string
+}
+
+type Option func(*Global)
+
+func WithNamespace(namespace string) Option {
+	return func(g *Global) {
+		g.namespace = namespace
+	}
+}
+
+func WithEnableFIPSMode(enableFIPSMode bool) Option {
+	return func(g *Global) {
+		g.enableFIPSMode = enableFIPSMode
+	}
+}
+
+func WithVersion(version string) Option {
+	return func(g *Global) {
+		g.version = version
+	}
+}
+
+func NewGlobal(opts ...Option) *Global {
+	g := &Global{}
+	for _, opt := range opts {
+		opt(g)
+	}
+
+	return g
+}
+
+func (g *Global) Namespace() string {
+	return g.namespace
+}
+
+func (g *Global) EnableFIPSMode() bool {
+	return g.enableFIPSMode
+}
+
+func (g *Global) Version() string {
+	return g.version
+}
