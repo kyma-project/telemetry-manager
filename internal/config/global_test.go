0923e30c4785bb61ce61e1cb3d641641879e343e
Stanislav Khalash <stanislav.khalash@sap.com>
2025-11-17 09:38:01 +0100

    Improve tests

--- internal/config/global_test.go
+++ internal/config/global_test.go
@@ -7,6 +7,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	errMsgInvalidNamespace  = "must be a valid Kubernetes namespace name"
+	errMsgEmptyOrWhitespace = "cannot be empty or whitespace only"
+)
+
 func TestDefaults(t *testing.T) {
 	g := NewGlobal()
 
@@ -51,80 +56,94 @@ func TestMultipleOptions(t *testing.T) {
 
 func TestValidateNamespace(t *testing.T) {
 	tests := []struct {
-		name          string
-		namespace     string
-		shouldError   bool
-		expectedField string
-		expectedValue string
-		expectedMsg   string
+		name        string
+		namespace   string
+		shouldError bool
+		expectedErr *ValidationError
 	}{
 		{
-			name:          "valid empty namespace",
-			namespace:     "",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "valid empty namespace",
+			namespace:   "",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 		{
 			name:        "valid simple namespace",
 			namespace:   "test",
 			shouldError: false,
+			expectedErr: nil,
 		},
 		{
 			name:        "valid namespace with hyphen",
 			namespace:   "test-namespace",
 			shouldError: false,
+			expectedErr: nil,
 		},
 		{
 			name:        "valid namespace with numbers",
 			namespace:   "ns123",
 			shouldError: false,
+			expectedErr: nil,
 		},
 		{
 			name:        "valid single character",
 			namespace:   "a",
 			shouldError: false,
+			expectedErr: nil,
 		},
 		{
-			name:          "invalid namespace with spaces",
-			namespace:     "invalid namespace",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "invalid namespace",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "invalid namespace with spaces",
+			namespace:   "invalid namespace",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "invalid namespace",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 		{
-			name:          "invalid namespace starting with hyphen",
-			namespace:     "-invalid",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "-invalid",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "invalid namespace starting with hyphen",
+			namespace:   "-invalid",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "-invalid",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 		{
-			name:          "invalid namespace ending with hyphen",
-			namespace:     "invalid-",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "invalid-",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "invalid namespace ending with hyphen",
+			namespace:   "invalid-",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "invalid-",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 		{
-			name:          "invalid namespace with uppercase",
-			namespace:     "Invalid",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "Invalid",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "invalid namespace with uppercase",
+			namespace:   "Invalid",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "Invalid",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 		{
-			name:          "invalid namespace with special characters",
-			namespace:     "test@namespace",
-			shouldError:   true,
-			expectedField: "namespace",
-			expectedValue: "test@namespace",
-			expectedMsg:   "must be a valid Kubernetes namespace name",
+			name:        "invalid namespace with special characters",
+			namespace:   "test@namespace",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "namespace",
+				Value:   "test@namespace",
+				Message: errMsgInvalidNamespace,
+			},
 		},
 	}
 
@@ -139,9 +158,7 @@ func TestValidateNamespace(t *testing.T) {
 
 				var validationErr *ValidationError
 				require.True(t, errors.As(err, &validationErr))
-				require.Equal(t, tt.expectedField, validationErr.Field)
-				require.Equal(t, tt.expectedValue, validationErr.Value)
-				require.Equal(t, tt.expectedMsg, validationErr.Message)
+				require.Equal(t, tt.expectedErr, validationErr)
 			} else {
 				require.NoError(t, err)
 			}
@@ -151,38 +168,36 @@ func TestValidateNamespace(t *testing.T) {
 
 func TestValidateVersion(t *testing.T) {
 	tests := []struct {
-		name          string
-		version       string
-		shouldError   bool
-		expectedField string
-		expectedValue string
-		expectedMsg   string
+		name        string
+		version     string
+		shouldError bool
+		expectedErr *ValidationError
 	}{
 		{
 			name:        "valid version",
 			version:     "1.2.3",
 			shouldError: false,
+			expectedErr: nil,
 		},
 		{
-			name:        "valid complex version",
-			version:     "2.0.0-rc.1",
-			shouldError: false,
+			name:        "empty version",
+			version:     "",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "version",
+				Value:   "",
+				Message: errMsgEmptyOrWhitespace,
+			},
 		},
 		{
-			name:          "empty version",
-			version:       "",
-			shouldError:   true,
-			expectedField: "version",
-			expectedValue: "",
-			expectedMsg:   "cannot be empty or whitespace only",
-		},
-		{
-			name:          "whitespace only version",
-			version:       "   ",
-			shouldError:   true,
-			expectedField: "version",
-			expectedValue: "   ",
-			expectedMsg:   "cannot be empty or whitespace only",
+			name:        "whitespace only version",
+			version:     "   ",
+			shouldError: true,
+			expectedErr: &ValidationError{
+				Field:   "version",
+				Value:   "   ",
+				Message: errMsgEmptyOrWhitespace,
+			},
 		},
 	}
 
@@ -197,9 +212,7 @@ func TestValidateVersion(t *testing.T) {
 
 				var validationErr *ValidationError
 				require.True(t, errors.As(err, &validationErr))
-				require.Equal(t, tt.expectedField, validationErr.Field)
-				require.Equal(t, tt.expectedValue, validationErr.Value)
-				require.Equal(t, tt.expectedMsg, validationErr.Message)
+				require.Equal(t, tt.expectedErr, validationErr)
 			} else {
 				require.NoError(t, err)
 			}
@@ -220,7 +233,7 @@ func TestValidateMultipleErrors(t *testing.T) {
 	// Should return namespace error first since it's checked first
 	require.Equal(t, "namespace", validationErr.Field)
 	require.Equal(t, "Invalid@Namespace", validationErr.Value)
-	require.Equal(t, "must be a valid Kubernetes namespace name", validationErr.Message)
+	require.Equal(t, errMsgInvalidNamespace, validationErr.Message)
 }
 
 func TestValidateSuccess(t *testing.T) {
