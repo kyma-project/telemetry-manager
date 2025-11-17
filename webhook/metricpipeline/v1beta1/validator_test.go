165fc8c267d478d4296e5657ad4690b430aad1f4
jeffreylimnardy <jeffrey.limnardy@sap.com>
2025-11-17 15:56:20 +0100

    add for v1beta1

---
+++ webhook/metricpipeline/v1beta1/validator_test.go
@@ -0,0 +1,153 @@
+package v1beta1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	telemetryv1alpha1 "github.com/kyma-project/telemetry-manager/apis/telemetry/v1alpha1"
+)
+
+func TestMetricPipelineValidator_ValidateCreate(t *testing.T) {
+	tests := []struct {
+		name      string
+		pipeline  *telemetryv1alpha1.MetricPipeline
+		expectErr bool
+	}{
+		{
+			name: "valid filter",
+			pipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{
+						{
+							Conditions: []string{`IsMatch(metric.name, "envoy") == true`},
+						},
+					},
+					Transforms: []telemetryv1alpha1.TransformSpec{},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid filter - bad OTTL expression",
+			pipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{
+						{
+							Conditions: []string{`IsMatch(metric.name, "envoy") ?= true`},
+						},
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "empty fields - should pass",
+			pipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters:    []telemetryv1alpha1.FilterSpec{},
+					Transforms: []telemetryv1alpha1.TransformSpec{},
+				},
+			},
+			expectErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := &MetricPipelineValidator{}
+
+			_, err := validator.ValidateCreate(t.Context(), tt.pipeline)
+
+			if tt.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestMetricPipelineValidator_ValidateUpdate(t *testing.T) {
+	tests := []struct {
+		name        string
+		oldPipeline *telemetryv1alpha1.MetricPipeline
+		newPipeline *telemetryv1alpha1.MetricPipeline
+		expectErr   bool
+	}{
+		{
+			name: "valid update",
+			oldPipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{},
+				},
+			},
+			newPipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{
+						{
+							Conditions: []string{`IsMatch(metric.name, "envoy") == true`},
+						},
+					},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid update - bad filter",
+			oldPipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{},
+				},
+			},
+			newPipeline: &telemetryv1alpha1.MetricPipeline{
+				Spec: telemetryv1alpha1.MetricPipelineSpec{
+					Filters: []telemetryv1alpha1.FilterSpec{
+						{
+							Conditions: []string{`log.severity_number <? SEVERITY_NUMBER_WARN`},
+						},
+					},
+				},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := &MetricPipelineValidator{}
+
+			_, err := validator.ValidateUpdate(t.Context(), tt.oldPipeline, tt.newPipeline)
+
+			if tt.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestMetricPipelineValidator_ValidateDelete(t *testing.T) {
+	validator := &MetricPipelineValidator{}
+
+	pipeline := &telemetryv1alpha1.MetricPipeline{}
+
+	warnings, err := validator.ValidateDelete(t.Context(), pipeline)
+
+	require.NoError(t, err)
+	require.Empty(t, warnings)
+}
+
+func TestMetricPipelineValidator_WrongType(t *testing.T) {
+	validator := &MetricPipelineValidator{}
+
+	// Pass wrong type
+	wrongObject := &telemetryv1alpha1.TracePipeline{}
+
+	warnings, err := validator.ValidateCreate(t.Context(), wrongObject)
+
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "expected a MetricPipeline but got")
+	require.Empty(t, warnings)
+}
