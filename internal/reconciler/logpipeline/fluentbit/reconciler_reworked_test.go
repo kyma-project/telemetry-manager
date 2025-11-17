a1d740210673235606d923a7d3a4b803bd8f9162
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-11-17 10:09:00 +0100

    migrate remaining test code to new file

--- internal/reconciler/logpipeline/fluentbit/reconciler_reworked_test.go
+++ internal/reconciler/logpipeline/fluentbit/reconciler_reworked_test.go
@@ -1018,3 +1018,33 @@ func createAppInputTestReconciler(fakeClient client.Client, pipeline telemetryv1
 		errToMsgStub,
 	)
 }
+func requireHasStatusCondition(t *testing.T, pipeline telemetryv1alpha1.LogPipeline, condType string, status metav1.ConditionStatus, reason, message string) {
+	cond := meta.FindStatusCondition(pipeline.Status.Conditions, condType)
+	require.NotNil(t, cond, "could not find condition of type %s", condType)
+	require.Equal(t, status, cond.Status)
+	require.Equal(t, reason, cond.Reason)
+	require.Equal(t, message, cond.Message)
+	require.Equal(t, pipeline.Generation, cond.ObservedGeneration)
+	require.NotEmpty(t, cond.LastTransitionTime)
+}
+
+func containsPipelines(pp []telemetryv1alpha1.LogPipeline) any {
+	return mock.MatchedBy(func(pipelines []telemetryv1alpha1.LogPipeline) bool {
+		if len(pipelines) != len(pp) {
+			return false
+		}
+
+		pipelineMap := make(map[string]bool)
+		for _, p := range pipelines {
+			pipelineMap[p.Name] = true
+		}
+
+		for _, p := range pp {
+			if !pipelineMap[p.Name] {
+				return false
+			}
+		}
+
+		return true
+	})
+}
