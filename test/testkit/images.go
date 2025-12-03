4014258e8310514eec9d9ad971c78996952dde21
Mostafa Shorim <mostafa.shorim@sap.com>
2025-12-03 14:10:00 +0100

    try first PR image from OCC

--- test/testkit/images.go
+++ test/testkit/images.go
@@ -5,5 +5,5 @@ package testkit
 
 const (
 	DefaultTelemetryGenImage  = "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen:v0.141.0"
-	DefaultOTelCollectorImage = "europe-docker.pkg.dev/kyma-project/prod/kyma-otel-collector:0.141.0-main"
+	DefaultOTelCollectorImage = "europe-docker.pkg.dev/kyma-project/dev/kyma-otel-collector:PR-432"
 )
