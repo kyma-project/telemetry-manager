9aeabf9f6e83bc21366bc0dc684c0743d70c9430
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2025-12-10 18:38:05 +0100

    fix: upgrade tests fail due to automatic resource cleanup (#2758)

--- test/e2e/upgrade/traces_upgrade_test.go
+++ test/e2e/upgrade/traces_upgrade_test.go
@@ -44,7 +44,7 @@ func TestTracesUpgrade(t *testing.T) {
 	resources = append(resources, backend.K8sObjects()...)
 
 	t.Run("before upgrade", func(t *testing.T) {
-		Expect(kitk8s.CreateObjects(t, resources...)).To(Succeed())
+		Expect(kitk8s.CreateObjectsWithoutAutomaticCleanup(t, resources...)).To(Succeed())
 
 		assert.DeploymentReady(t, kitkyma.TraceGatewayName)
 		assert.TracePipelineHealthy(t, pipelineName)
