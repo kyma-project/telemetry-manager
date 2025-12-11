cf5d3353bdb0065ff636d58d6ac20ff9cf2832fe
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2025-12-11 13:34:15 +0100

    Fix dependency mismatch

--- test/e2e/metrics/gateway/empty_enrichment_values_test.go
+++ test/e2e/metrics/gateway/empty_enrichment_values_test.go
@@ -9,6 +9,7 @@ import (
 	testutils "github.com/kyma-project/telemetry-manager/internal/utils/test"
 	"github.com/kyma-project/telemetry-manager/test/testkit/assert"
 	kitk8s "github.com/kyma-project/telemetry-manager/test/testkit/k8s"
+	kitk8sobjects "github.com/kyma-project/telemetry-manager/test/testkit/k8s/objects"
 	kitkyma "github.com/kyma-project/telemetry-manager/test/testkit/kyma"
 	. "github.com/kyma-project/telemetry-manager/test/testkit/matchers/metric"
 	kitbackend "github.com/kyma-project/telemetry-manager/test/testkit/mocks/backend"
@@ -58,8 +59,8 @@ func TestEmptyEnrichmentValues(t *testing.T) {
 	)
 
 	resources := []client.Object{
-		kitk8s.NewNamespace(backendNs).K8sObject(),
-		kitk8s.NewNamespace(genNs).K8sObject(),
+		kitk8sobjects.NewNamespace(backendNs).K8sObject(),
+		kitk8sobjects.NewNamespace(genNs).K8sObject(),
 		&pipeline,
 		generator.K8sObject(),
 	}
