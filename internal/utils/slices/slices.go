0b4a5ddbdee967c58c59404d2f98c2f1199c6112
jeffreylimnardy <jeffrey.limnardy@sap.com>
2025-12-16 14:55:35 +0100

    lint

--- internal/utils/slices/slices.go
+++ internal/utils/slices/slices.go
@@ -1,6 +1,6 @@
 package slices
 
-import "k8s.io/apimachinery/pkg/conversion"
+import apiconversion "k8s.io/apimachinery/pkg/conversion"
 
 func TransformFunc[S1 ~[]E1, E1, E2 any](s S1, transform func(E1) E2) []E2 {
 	results := make([]E2, len(s))
@@ -11,7 +11,7 @@ func TransformFunc[S1 ~[]E1, E1, E2 any](s S1, transform func(E1) E2) []E2 {
 	return results
 }
 
-func TransformWithConversion[E1, E2 any](s []E1, convert func(*E1, *E2, conversion.Scope) error) ([]E2, error) {
+func TransformWithConversion[E1, E2 any](s []E1, convert func(*E1, *E2, apiconversion.Scope) error) ([]E2, error) {
 	results := make([]E2, len(s))
 	for i := range s {
 		if err := convert(&s[i], &results[i], nil); err != nil {
