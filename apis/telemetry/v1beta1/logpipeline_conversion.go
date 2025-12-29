65f5ab55db2aa24ff342a366de67ec1982b50919
jeffreylimnardy <jeffrey.limnardy@sap.com>
2025-12-29 18:20:23 +0100

    revert

---
+++ apis/telemetry/v1beta1/logpipeline_conversion.go
@@ -0,0 +1,5 @@
+package v1beta1
+
+// Hub marks this type as a conversion hub.
+func (lp *LogPipeline) Hub() {
+}
--- apis/telemetry/v1beta1/metricpipeline_conversion.go
+++ apis/telemetry/v1beta1/metricpipeline_conversion.go
@@ -1,7 +1,4 @@
 package v1beta1
 
 // Hub marks this type as a conversion hub.
-func (lp *LogPipeline) Hub() {
-}
-
 func (mp *MetricPipeline) Hub() {}
