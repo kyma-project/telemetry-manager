c9c0cc495c5777de41f7cbcfbf4518a67f89351b
jeffreylimnardy <jeffrey.limnardy@sap.com>
2025-12-29 11:20:21 +0100

    remove unused code

--- internal/utils/fuzzing/fuzzer.go
+++ internal/utils/fuzzing/fuzzer.go
@@ -13,63 +13,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/apimachinery/pkg/util/json"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 	"sigs.k8s.io/randfill"
 )
-
-const (
-	// DataAnnotation is the annotation that conversion webhooks
-	// use to retain the data in case of down-conversion from the hub.
-	DataAnnotation = "cluster.x-k8s.io/conversion-data"
-)
-
-// MarshalData stores the source object as json data in the destination object annotations map.
-// It ignores the metadata of the source object.
-func MarshalData(src metav1.Object, dst metav1.Object) error {
-	u, err := runtime.DefaultUnstructuredConverter.ToUnstructured(src)
-	if err != nil {
-		return err
-	}
-
-	delete(u, "metadata")
-
-	data, err := json.Marshal(u)
-	if err != nil {
-		return err
-	}
-
-	annotations := dst.GetAnnotations()
-	if annotations == nil {
-		annotations = map[string]string{}
-	}
-
-	annotations[DataAnnotation] = string(data)
-	dst.SetAnnotations(annotations)
-
-	return nil
-}
-
-// UnmarshalData tries to retrieve the data from the annotation and unmarshals it into the object passed as input.
-func UnmarshalData(from metav1.Object, to any) (bool, error) {
-	annotations := from.GetAnnotations()
-
-	data, ok := annotations[DataAnnotation]
-	if !ok {
-		return false, nil
-	}
-
-	if err := json.Unmarshal([]byte(data), to); err != nil {
-		return false, err
-	}
-
-	delete(annotations, DataAnnotation)
-	from.SetAnnotations(annotations)
-
-	return true, nil
-}
 
 // GetFuzzer returns a new fuzzer to be used for testing.
 func GetFuzzer(scheme *runtime.Scheme, funcs ...fuzzer.FuzzerFuncs) *randfill.Filler {
@@ -182,17 +130,6 @@ func FuzzTestFunc(input FuzzTestFuncInput) func(*testing.T) {
 				}
 
 				g.Expect(spokeAfter.ConvertFrom(hubCopy)).To(gomega.Succeed())
-
-				// Remove data annotation eventually added by ConvertFrom for avoiding data loss in hub-spoke-hub round trips
-				// NOTE: There are use case when we want to skip this operation, e.g. if the spoke object does not have ObjectMeta (e.g. kubeadm types).
-				if !input.SkipSpokeAnnotationCleanup {
-					metaAfter, ok := spokeAfter.(metav1.Object)
-					if !ok {
-						t.Fatalf("spoke does not implement metav1.Object")
-					}
-
-					delete(metaAfter.GetAnnotations(), DataAnnotation)
-				}
 
 				if input.SpokeAfterMutation != nil {
 					input.SpokeAfterMutation(spokeAfter)
