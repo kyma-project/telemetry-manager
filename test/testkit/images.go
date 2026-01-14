c05e251e931054c3a65850bf3bb905fd3626982a
Teodor-Adrian Mihaescu <teodor-adrian.mihaescu@sap.com>
2026-01-14 15:29:33 +0100

    feat: bump OTel image to version 0.143.0

--- test/testkit/images.go
+++ test/testkit/images.go
@@ -4,7 +4,7 @@
 package testkit
 
 const (
-	DefaultTelemetryGenImage         = "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen:v0.141.0"
+	DefaultTelemetryGenImage         = "ghcr.io/open-telemetry/opentelemetry-collector-contrib/telemetrygen:v0.143.0"
 	DefaultOTelCollectorContribImage = "otel/opentelemetry-collector-contrib:latest"
-	DefaultOTelCollectorImage        = "europe-docker.pkg.dev/kyma-project/prod/kyma-otel-collector:0.141.0-main"
+	DefaultOTelCollectorImage        = "europe-docker.pkg.dev/kyma-project/prod/kyma-otel-collector:0.143.0-main"
 )
