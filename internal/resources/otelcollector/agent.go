831781bfa4143d70b28b8708d44131bbf38035b8
Rakesh Garimella <rakesh.garimella@sap.com>
2026-01-21 15:40:38 +0100

    refactor agents

--- internal/resources/otelcollector/agent.go
+++ internal/resources/otelcollector/agent.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"maps"
 	"slices"
 	"strconv"
 	"strings"
@@ -212,10 +213,14 @@ func (aad *AgentApplierDeleter) DeleteResources(ctx context.Context, c client.Cl
 
 func (aad *AgentApplierDeleter) makeAgentDaemonSet(configChecksum string, opts AgentApplyOptions) *appsv1.DaemonSet {
 	annotations := aad.makeAnnotationsFunc(configChecksum, opts)
-	labels := commonresources.MakeDefaultLabels(aad.baseName, commonresources.LabelValueK8sComponentAgent)
-	selectorLabels := commonresources.MakeDefaultSelectorLabels(aad.baseName)
 
-	metadata := commonresources.MakeResourceMetadata(&aad.globals, labels, aad.extraPodLabel, annotations)
+	// Create final annotations for the DaemonSet and Pods with additional annotations
+	podAnnotations := make(map[string]string)
+	resourceAnnotations := make(map[string]string)
+
+	maps.Copy(resourceAnnotations, aad.globals.AdditionalAnnotations())
+	maps.Copy(podAnnotations, aad.globals.AdditionalAnnotations())
+	maps.Copy(podAnnotations, annotations)
 
 	// Add pod options shared between all agents
 	podOpts := slices.Clone(aad.podOpts)
@@ -229,12 +234,27 @@ func (aad *AgentApplierDeleter) makeAgentDaemonSet(configChecksum string, opts A
 
 	podSpec := makePodSpec(aad.baseName, aad.image, podOpts, containerOpts)
 
+	selectorLabels := commonresources.MakeDefaultSelectorLabels(aad.baseName)
+	labels := commonresources.MakeDefaultLabels(aad.baseName, commonresources.LabelValueK8sComponentAgent)
+	defaultPodLabels := make(map[string]string)
+	maps.Copy(defaultPodLabels, labels)
+	maps.Copy(defaultPodLabels, aad.extraPodLabel)
+
+	// Create final labels for the DaemonSet and Pods with additional labels
+	resourceLabels := make(map[string]string)
+	podLabels := make(map[string]string)
+
+	maps.Copy(resourceLabels, aad.globals.AdditionalLabels())
+	maps.Copy(podLabels, aad.globals.AdditionalLabels())
+	maps.Copy(resourceLabels, labels)
+	maps.Copy(podLabels, defaultPodLabels)
+
 	return &appsv1.DaemonSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        aad.baseName,
 			Namespace:   aad.globals.TargetNamespace(),
-			Labels:      metadata.ResourceLabels,
-			Annotations: metadata.ResourceAnnotations,
+			Labels:      resourceLabels,
+			Annotations: resourceAnnotations,
 		},
 		Spec: appsv1.DaemonSetSpec{
 			Selector: &metav1.LabelSelector{
@@ -242,8 +262,8 @@ func (aad *AgentApplierDeleter) makeAgentDaemonSet(configChecksum string, opts A
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Labels:      metadata.PodLabels,
-					Annotations: metadata.PodAnnotations,
+					Labels:      podLabels,
+					Annotations: podAnnotations,
 				},
 				Spec: podSpec,
 			},
