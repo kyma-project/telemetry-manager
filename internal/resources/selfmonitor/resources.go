523848091a2f004fdd6430b99509a821db3e27e9
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2026-03-27 14:37:52 +0100

    add endpointslices RBAC to self-monitor role

--- internal/resources/selfmonitor/resources.go
+++ internal/resources/selfmonitor/resources.go
@@ -164,6 +164,11 @@ func (ad *ApplierDeleter) makeRole() *rbacv1.Role {
 				Resources: []string{"services", "endpoints", "pods"},
 				Verbs:     []string{"get", "list", "watch"},
 			},
+			{
+				APIGroups: []string{"discovery.k8s.io"},
+				Resources: []string{"endpointslices"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
 		},
 	}
 
