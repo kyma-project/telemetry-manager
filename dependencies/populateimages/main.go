46fc380363e500fd3b30239eabd27972d51730d4
Andreas Thaler <andreas.thaler01@sap.com>
2026-03-30 18:29:56 +0200

    chore: ignore fault-backend in sec scanners

--- dependencies/populateimages/main.go
+++ dependencies/populateimages/main.go
@@ -115,6 +115,7 @@ func generateSecScanConfig(data map[string]string) error {
 		"dependencies/populateimages/**",
 		"dependencies/sample-app/**",
 		"dependencies/stdout-log-generator/**",
+		"dependencies/fault-backend/**",
 		"internal/tools/**",
 		"apis/telemetry/v1alpha1/zz_generated.conversion.go",
 	}
