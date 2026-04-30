482aa7404c3bd868050d9441d97102a79c66edff
Stanislav Khalash <stanislav.khalash@sap.com>
2026-04-30 09:06:11 +0200

    make avalanche produce metrics below 20MB limit

--- test/testkit/mocks/prommetricgen/metric_producer.go
+++ test/testkit/mocks/prommetricgen/metric_producer.go
@@ -238,12 +238,13 @@ func (p *Pod) WithLabels(labels map[string]string) *Pod {
 	return p
 }
 
+// The total response size must stay below the metric agent's body_size_limit (20MB).
 func (p *Pod) WithAvalancheHighLoad() *Pod {
 	p.image = avalancheImage
 	p.args = []string{
-		"--gauge-metric-count=" + strconv.Itoa(int(160)),
-		"--counter-metric-count=" + strconv.Itoa(int(100)),
-		"--histogram-metric-count=" + strconv.Itoa(int(50)),
+		"--gauge-metric-count=" + strconv.Itoa(int(80)),
+		"--counter-metric-count=" + strconv.Itoa(int(50)),
+		"--histogram-metric-count=" + strconv.Itoa(int(25)),
 		"--port=" + strconv.Itoa(int(p.metricsPort)),
 	}
 
