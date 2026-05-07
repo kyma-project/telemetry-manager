72707a128dbe1b3b575c0e9f33aa5c627d4c4409
Mostafa Shorim <mostafa.shorim@sap.com>
2026-05-07 02:05:01 +0200

    adjust documentation for AdditionalMetrics

--- apis/telemetry/v1beta1/metricpipeline_types.go
+++ apis/telemetry/v1beta1/metricpipeline_types.go
@@ -102,9 +102,9 @@ type MetricPipelineRuntimeInput struct {
 	// Resources configures the Kubernetes resource types for which metrics are collected.
 	// +kubebuilder:validation:Optional
 	Resources *MetricPipelineRuntimeInputResources `json:"resources,omitempty"`
-	// AdditionalMetrics specifies optional upstream metric names to collect
+	// AdditionalMetrics specifies upstream metric names to collect
 	// in addition to the default curated set. Each entry must be a valid
-	// optional metric name.
+	// metric name.
 	// +kubebuilder:validation:Optional
 	AdditionalMetrics []string `json:"additionalMetrics,omitempty"`
 }
