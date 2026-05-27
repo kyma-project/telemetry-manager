975b36a03d43d0b7a9b1db441c17f74e5939bc8c
Hisar Balik <hisar.balik@sap.com>
2026-05-27 14:39:13 +0200

    update e2e tests

--- test/e2e/traces/reject_creation_test.go
+++ test/e2e/traces/reject_creation_test.go
@@ -258,7 +258,7 @@ func TestRejectTracePipelineCreation(t *testing.T) {
 					),
 				).
 				Build(),
-			errorMsg: "Invalid value: \"object\": 'tokenURL' must be a valid URL",
+			errorMsg: "Invalid value: 'tokenURL' must be a valid URL",
 			field:    "spec.output.otlp.authentication.oauth2.tokenURL",
 		},
 		{
